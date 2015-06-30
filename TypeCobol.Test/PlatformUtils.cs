f6ed0b5a3fd11e4912592c2cc91d1166c7460a85
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2015-06-30 21:44:06 +0200

    Fixed bug in relative path for TypeCobol.Test project

--- TypeCobol.Test/PlatformUtils.cs
+++ TypeCobol.Test/PlatformUtils.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 
 namespace TypeCobol.Test
 {
@@ -10,7 +9,7 @@ namespace TypeCobol.Test
         /// <summary>
         /// Relative path of the test project in VS solution
         /// </summary>
-        public const string RELATIVE_TEST_PROJECT_PATH = @"TypeCobol\TypeCobol.Test";
+        public const string RELATIVE_TEST_PROJECT_PATH = "TypeCobol.Test";
         
         static PlatformUtils()
         {
