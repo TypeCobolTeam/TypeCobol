9275f28e98be17011f2bc1b322b8435c954e6245
wiztigers <regis.witz@free.fr>
2015-07-07 10:49:07 +0200

    Use StringBuilder.

--- TypeCobol.Test/Compiler/CodeElements/ArithmeticStatementTester.cs
+++ TypeCobol.Test/Compiler/CodeElements/ArithmeticStatementTester.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using TypeCobol.Compiler.CodeElements;
 using TypeCobol.Compiler.CodeElements.Expressions;
 
@@ -10,29 +9,29 @@ namespace TypeCobol.Test.Compiler.CodeElements
     {
         public string Dump(AddStatement statement)
         {
-            string s = "";
+            StringBuilder s = new StringBuilder();
             foreach (var pair in statement.affectations)
             {
-                s += pair.Key.Text + " = " + statement.operations[pair.Value].ToString() + ", ";
+                s.AppendFormat("{0} = {1}, ", pair.Key.Text, statement.operations[pair.Value].ToString());
             }
-            if (statement.affectations.Count > 0) s = s.Remove(s.Length - 2);
-            return s;
+            if (statement.affectations.Count > 0) s.Length -= 2;
+            return s.ToString();
         }
 
         public void CompareWithRPN(TypeCobol.Compiler.Parser.SyntaxDocument tree, string[] rpn)
         {
             int c = 0;
-            string errors = "";
+            StringBuilder errors = new StringBuilder();
             foreach (TypeCobol.Compiler.CodeElements.CodeElement e in tree.CodeElements)
             {
                 if (e.GetType() != typeof(AddStatement)) continue;
                 AddStatement add = (TypeCobol.Compiler.CodeElements.AddStatement)e;
                 string dump = Dump(add);
-                if (c >= rpn.Length) errors += ("No "+c+"th rpn provided.");
-                else if (dump != rpn[c]) errors += (c + ": \""+dump+"\", expected \""+rpn[c]+"\"\n");
+                if (c >= rpn.Length) errors.AppendFormat("RPN number {} not provided.", c);
+                else if (dump != rpn[c]) errors.AppendFormat("{0}: \"{1}\", expected \"{2}\"\n", c, dump, rpn[c]);
                 c++;
             }
-            if (errors.Length > 0) throw new Exception(errors);
+            if (errors.Length > 0) throw new Exception(errors.ToString());
             if (c != rpn.Length) throw new Exception("Number of checks expected "+rpn.Length+" vs "+c);
         }
     }
