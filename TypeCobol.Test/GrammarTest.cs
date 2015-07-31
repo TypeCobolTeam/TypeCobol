ee6cd1f6779e51af7f29fcfc666a77872b2ebae7
wiztigers <regis.witz@externe.e-i.com>
2015-07-31 14:49:20 +0200

    issue #23: customize grammar tester.

--- TypeCobol.Test/GrammarTest.cs
+++ TypeCobol.Test/GrammarTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TypeCobol.Test.Compiler.Parser;
+using System.Diagnostics;
 
 namespace TypeCobol.Test
 {
@@ -16,12 +18,26 @@ namespace TypeCobol.Test
             string samples = @"Samples";
             string path = PlatformUtils.GetPathForProjectFile(samples);
             string[] files = System.IO.Directory.GetFiles(path, regex, System.IO.SearchOption.AllDirectories);
+            string[] ignored = { };
 
+            var str = new System.Text.StringBuilder();
             int tested = 0, errors = 0;
             foreach (var file in files)
             {
                 string filename = System.IO.Path.GetFileName(file);
+                str.Append(filename + ':');
+                if (ignored.Contains(filename)) {
+                    str.AppendLine(" ignored.");
+                    continue;
+                }
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
                 var unit = ParserUtils.ParseCobolFile(filename, null, samples);
+                watch.Stop();
+                TimeSpan elapsed = watch.Elapsed;
+                string formatted = String.Format("{0:00}m{1:00}s{2:000}ms", elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds);
+                str.AppendLine(" parsed in "+formatted);
+
                 tested++;
                 if(hasErrors(unit.SyntaxDocument)) {
                     Console.WriteLine(filename);
@@ -31,8 +47,9 @@ namespace TypeCobol.Test
                     if (errors >= STOP_AFTER_AS_MANY_ERRORS) break;
                 }
             }
-            string message = "Files tested="+tested+"/"+files.Length+", errors="+errors;
-            if (errors > 0) Assert.Fail("\n"+message);
+            string message = "Files tested=" + tested + "/" + files.Length + ", errors=" + errors;
+            System.IO.File.WriteAllText("CheckGrammarResults.txt", str.AppendLine(message).ToString());
+            if (errors > 0) Assert.Fail('\n'+message);
         }
 
         private bool hasErrors(TypeCobol.Compiler.Parser.SyntaxDocument document)
