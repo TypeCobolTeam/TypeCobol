4203d9bad087269be329b6222836a70c9cd41ff1
wiztigers <regis.witz@externe.e-i.com>
2015-08-17 14:23:40 +0200

    issue #63: oops

--- TypeCobol/Compiler/CodeElements/Statement/RewriteStatement.cs
+++ TypeCobol/Compiler/CodeElements/Statement/RewriteStatement.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using TypeCobol.Compiler.CodeElements.Expressions;
 
 namespace TypeCobol.Compiler.CodeElements
 {
+
     public class RewriteStatement : CodeElement
     {
-        public RewriteStatement() : base(CodeElementType.RewriteStatement)
-        { }
+        QualifiedDataName RecordName;
+        Identifier Identifier;
+
+        public RewriteStatement(QualifiedDataName recordname, Identifier identifier)
+            : base(CodeElementType.RewriteStatement)
+        {
+            this.RecordName = recordname;
+            this.Identifier = identifier;
+        }
     }
 }
