b057fd06e8b241668d71f4d4b38d1af75663e6a8
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2015-08-18 00:51:58 +0200

    Fixed issue #60 : in Token class, a ContinuationToken should always keep a channel value of CHANNEL_ContinuationTokens (to be filtered before the parsing stage), even if it is itself continued (because it is in the middle of a list of continuation lines)

--- TypeCobol.Test/Compiler/Parser/Samples/Statements/MOVE.cbl
+++ TypeCobol.Test/Compiler/Parser/Samples/Statements/MOVE.cbl
@@ -3,3 +3,8 @@ MOVE FUNCTION LENGTH (x) TO x.
 * special registers
 MOVE LENGTH OF x TO x.
 MOVE LENGTH x TO x.
+* continuations
+                 MOVE 'Lorem ipsum dolor sit amet, consectetur adi
+-               'piscing elit, sed do eiusmod tempor incididunt ut
+-               'labore et dolore magna aliqua                 '  
+                                         TO  SOMEWHERE           
