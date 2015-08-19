a5d01173c9b459e3f930de5405a404ee3b7185e3
wiztigers <regis.witz@externe.e-i.com>
2015-08-19 15:04:52 +0200

    issue #62: document CloseStatement.

--- TypeCobol/Compiler/CodeElements/Statement/CloseStatement.cs
+++ TypeCobol/Compiler/CodeElements/Statement/CloseStatement.cs
@@ -2,8 +2,29 @@
 
 namespace TypeCobol.Compiler.CodeElements
 {
+    /// <summary>
+    /// p313:
+    /// The CLOSE statement terminates the processing of volumes and files.
+    ///
+    /// p314:
+    /// A CLOSE statement can be executed only for a file in an open mode. After
+    /// successful execution of a CLOSE statement (without the REEL/UNIT phrase if
+    /// using format 1):
+    /// * The record area associated with the file-name is no longer available.
+    /// Unsuccessful execution of a CLOSE statement leaves availability of the record
+    /// data undefined.
+    /// * An OPEN statement for the file must be executed before any other input/output
+    /// statement can be executed for the file and before data is moved to a record
+    /// description entry associated with the file.
+    /// </summary>
     public class CloseStatement : CodeElement
     {
+        /// <summary>
+        /// p313:
+        /// Designates the file upon which the CLOSE statement is to operate. If more
+        /// than one file-name is specified, the files need not have the same
+        /// organization or access. [Each filename] must not be a sort or merge file.
+        /// </summary>
         IList<CloseFileName> FileNames;
 
         public CloseStatement(IList<CloseFileName> filenames)
@@ -15,11 +36,53 @@ namespace TypeCobol.Compiler.CodeElements
 
     public class CloseFileName
     {
+        /// <summary>
+        /// p313:
+        /// Designates the file upon which the CLOSE statement is to operate. If more
+        /// than one file-name is specified, the files need not have the same
+        /// organization or access. [FileName] must not be a sort or merge file.
+        /// </summary>
         SymbolReference<FileName> FileName;
+
+        /// <summary>
+        /// p314:
+        /// REEL and UNIT
+        /// You can specify these phrases only for QSAM multivolume or single
+        /// volume files. The terms REEL and UNIT are interchangeable.
+        /// p313:
+        /// The REEL, UNIT, and NO REWIND phrases are not valid for VSAM files.
+        /// </summary>
         bool IsReel;
+        /// <summary>
+        /// p314:
+        /// REEL and UNIT
+        /// You can specify these phrases only for QSAM multivolume or single
+        /// volume files. The terms REEL and UNIT are interchangeable.
+        /// p313:
+        /// The REEL, UNIT, and NO REWIND phrases are not valid for VSAM files.
+        /// </summary>
         bool IsUnit;
+        /// <summary>
+        /// p314:
+        /// WITH NO REWIND and FOR REMOVAL
+        /// These phrases apply only to QSAM tape files. If they are specified for
+        /// storage devices to which they do not apply, the close operation is
+        /// successful and a status key value is set to indicate the file was on a
+        /// non-reel medium.
+        /// </summary>
         bool IsForRemoval;
+        /// <summary>
+        /// p314:
+        /// WITH NO REWIND and FOR REMOVAL
+        /// These phrases apply only to QSAM tape files. If they are specified for
+        /// storage devices to which they do not apply, the close operation is
+        /// successful and a status key value is set to indicate the file was on a
+        /// non-reel medium.
+        /// </summary>
         bool IsNoRewind;
+        /// <summary>
+        /// TODO: Meaning not found in documentation.
+        /// </summary>
         bool IsLock;
 
         public CloseFileName(SymbolReference<FileName> filename, bool reel, bool unit, bool forremoval, bool norewind, bool lock_)
