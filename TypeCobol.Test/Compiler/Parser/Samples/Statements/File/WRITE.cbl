c91fd07ef91e9a9212a412d2696aa6b90182c940
wiztigers <regis.witz@externe.e-i.com>
2015-08-28 14:37:16 +0200

    closes #47: implement WRITE statement unit tests.

---
+++ TypeCobol.Test/Compiler/Parser/Samples/Statements/File/WRITE.cbl
@@ -0,0 +1,16 @@
+﻿WRITE x
+WRITE x FROM identifier
+WRITE x OF y OF z IN filename FROM identifier
+WRITE x FROM identifier BEFORE
+WRITE x FROM identifier BEFORE ADVANCING
+WRITE x FROM identifier AFTER
+WRITE x FROM identifier AFTER ADVANCING
+WRITE x FROM identifier BEFORE 1
+WRITE x FROM identifier BEFORE 1 LINE
+WRITE x FROM identifier AFTER 1 LINES
+WRITE x FROM identifier AFTER 2 LINE
+WRITE x FROM identifier BEFORE 2 LINES
+WRITE x FROM identifier BEFORE a
+WRITE x FROM identifier BEFORE a LINES
+WRITE x FROM identifier BEFORE PAGE
+WRITE x FROM identifier AFTER PAGE
