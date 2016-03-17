90f7c1b454fb9536290b4dd53f271ba61b02a4a9
wiztigers <regis.witz@externe.e-i.com>
2016-03-17 09:12:03 +0100

    issue #198: begin codegen parameters file implementation after @reydelpa spike

---
+++ Codegen/Config/ConfigParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using TypeCobol.Codegen.Skeletons;
+
+namespace TypeCobol.Codegen.Config {
+	/// <summary><see cref="Skeleton"/> parser interface.</summary>
+	interface ConfigParser {
+		List<Skeleton> Parse(string path);
+	}
+}
---
+++ Codegen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using TypeCobol.Codegen.Skeletons;
+
+namespace TypeCobol.Codegen {
+
+	class Program {
+		static void Main(string[] args) {
+			var files = new List<string>();
+			foreach(string arg in args) files.AddRange(GetParameterFiles(arg));
+			var skeletons = new List<Skeleton>();
+			foreach(var file in files) {
+				string extension = Path.GetExtension(file);
+				var parser = GetParser(extension);
+				skeletons.AddRange(parser.Parse(file));
+			}
+			System.Console.WriteLine(skeletons.Count+" skeleton"+(skeletons.Count>1?"s":"")+':');
+			foreach(var s in skeletons) System.Console.Write(s.ToString());
+		}
+
+		private static IEnumerable<string> GetParameterFiles(string path) {
+			var files = new List<string>();
+			if (File.Exists(path)) {
+				files.Add(path);
+			} else
+			if (Directory.Exists(path)) {
+				System.Console.WriteLine("\""+path+"\" is a directory.");
+			}
+			return files;
+		}
+
+		private static Config.ConfigParser GetParser(string extension) {
+			var map = new Dictionary<string,Config.ConfigParser>();
+			map.Add(".xml", new Config.XmlParser());
+			var defaultext = ".xml";
+			try { return map[extension.ToLower()]; }
+			catch(System.Exception) { return map[defaultext]; }
+		}
+	}
+
+}
