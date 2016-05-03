828342bb2e704820d89fc4b5656a0455c82478f0
wiztigers <regis.witz@externe.e-i.com>
2016-05-03 09:29:58 +0200

    issue #211 #186: fast-and-dumb variables replacer

---
+++ Codegen/src/Config/Cheetah.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TypeCobol.Codegen.Config {
+
+	public class Cheetah {
+
+		public static string Replace(string text, string variable, string value, char delimiter='%') {
+			return text.Replace(delimiter+variable, value);
+		}
+
+		public static string Replace(string text, Dictionary<string,string> variables, char delimiter='%') {
+			foreach(var x in variables) {
+				text = Replace(text, x.Key, x.Value, delimiter);
+			}
+			return text;
+		}
+
+	}
+}
