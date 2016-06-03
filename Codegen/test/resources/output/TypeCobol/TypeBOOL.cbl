63b8321cea87c050a1b50704a1ca283f6a67e895
wiztigers <regis.witz@externe.e-i.com>
2016-06-03 11:15:05 +0200

    issue #200 #211: fix TYPE BOOL codegen

--- Codegen/test/resources/output/TypeCobol/TypeBOOL.cbl
+++ Codegen/test/resources/output/TypeCobol/TypeBOOL.cbl
@@ -18,26 +18,30 @@
 
        01  x PIC X.
       * Booleans declaration
-      *01  Identifier TYPE BOOL.
-       01  Identifier-value PIC X VALUE LOW-VALUE.
-         88  Identifier       VALUE 'T'.
-         88  Identifier-false VALUE 'F'.
-      *01  AnotherOne TYPE BOOL.
-       01  AnotherOne-value PIC X VALUE LOW-VALUE.
-         88  AnotherOne       VALUE 'T'.
-         88  AnotherOne-false VALUE 'F'.
+      *01  Identifier TYPE BOOL.                                              
+       01  Identifier-value PIC X VALUE LOW-VALUE.                            
+         88  Identifier       VALUE 'T'.                                      
+         88  Identifier-false VALUE 'F'.                                      
+                                                                              
+      *01  AnotherOne TYPE BOOL.                                              
+       01  AnotherOne-value PIC X VALUE LOW-VALUE.                            
+         88  AnotherOne       VALUE 'T'.                                      
+         88  AnotherOne-false VALUE 'F'.                                      
+                                                                              
       * WARNING: initialization of a group containing booleans
        01  AGroup.
          05  a PIC X.
            10  c PIC X.
-      *    10  b TYPE BOOL.
-           10  b-value PIC X VALUE LOW-VALUE.
-             88  b       VALUE 'T'.
-             88  b-false VALUE 'F'.
-      *  05  d TYPE BOOL.
-         05  d-value PIC X VALUE LOW-VALUE.
-           88  d       VALUE 'T'.
-           88  d-false VALUE 'F'.
+      *    10  b TYPE BOOL.                                                   
+       10  b-value PIC X VALUE LOW-VALUE.                                     
+         88  b       VALUE 'T'.                                               
+         88  b-false VALUE 'F'.                                               
+                                                                              
+      *  05  d TYPE BOOL.                                                     
+       05  d-value PIC X VALUE LOW-VALUE.                                     
+         88  d       VALUE 'T'.                                               
+         88  d-false VALUE 'F'.                                               
+                                                                              
 
 
 
@@ -46,8 +50,9 @@
 
        TRAITEMENT.
            SET Identifier  TO TRUE
-      *    SET Identifier  TO FALSE
-       SET Identifier-false TO TRUE.
+      *    SET Identifier  TO FALSE                                           
+       SET Identifier-false TO TRUE.                                          
+                                                                              
       * OK
            MOVE TRUE         TO Identifier
            MOVE FALSE        TO Identifier
