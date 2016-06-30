TypeCobolTeam/TypeCobol

commit 8f35334dc05c9369399684af52f1c3bca1061fd9

renamed grammar rule prefixed with "tc" which was in fact cobol 2002 grammer rules Moved TypeCobol check from CobolElementCheckers to  TypeCobolProgramCheckers Moved Cobol 2002 tests to a Cobol2002 folder and TypeCobol tests to TypeCobol folder
Added check for External data not on level 01

Updated tests to reflect all theses changes

TypeCobol/Compiler/Diagnostics/TypeCobolChecker.cs
@@ -1,43 +1,0 @@
-﻿using Antlr4.Runtime;
-using System.Collections.Generic;
-using TypeCobol.Compiler.AntlrUtils;
-using TypeCobol.Compiler.CodeElements;
-using TypeCobol.Compiler.Parser;
-using TypeCobol.Compiler.Parser.Generated;
-
-namespace TypeCobol.Compiler.Diagnostics {
-
-	class TypeDefinitionChecker: CodeElementListener {
-
-		public IList<System.Type> GetCodeElements() {
-			return new List<System.Type>() { typeof(TypeDefinition), typeof(DataDescriptionEntry), };
-		}
-		public void OnCodeElement(CodeElement e, ParserRuleContext context) {
-			if (!(e is TypeDefinition)) return;
-			if (e is DataDescriptionEntry)
-				OnDataDescriptionEntry(e as DataDescriptionEntry, context as CodeElementsParser.DataDescriptionEntryContext);
-		}
-
-		private void OnDataDescriptionEntry(DataDescriptionEntry data, CodeElementsParser.DataDescriptionEntryContext context) {
-			if (!data.IsTypeDefinition) return;
-			if (data.LevelNumber != 1) {
-				string message = "TYPEDEF clause can only be specified for level 01 entries";
-				DiagnosticUtils.AddError(data, message, context.tcExtTypedefClause());
-			}
-			if (data.Picture != null && (data.DataType != null && data.DataType.IsStrong)) {
-				string message = "Elementary TYPEDEF cannot be STRONG";
-				string rulestack = new RuleStackBuilder().GetRuleStack(context.tcExtTypedefClause());
-				DiagnosticUtils.AddError(data, message, ParseTreeUtils.GetFirstToken(context.tcExtTypedefClause().STRONG()), rulestack);
-			}
-			if (data.RedefinesDataName != null) {
-				string message = "REDEFINES clause cannot be specified with TYPEDEF clause";
-				DiagnosticUtils.AddError(data, message, context.redefinesClause());
-			}
-			if (data.IsExternal) {
-				string message = "EXTERNAL clause cannot be specified with TYPEDEF clause";
-				foreach(var external in context.externalClause())
-					DiagnosticUtils.AddError(data, message, external);
-			}
-		}
-	}
-}

TypeCobol.Grammar/Grammars/Cobol/Cobol2002CodeElements.g4
@@ -10,7 +10,7 @@ cobolCodeElements: codeElement* EOF;
 dataDescriptionEntry:
 	( { CurrentToken.Text != "66" && CurrentToken.Text != "88" }? 
 
-		levelNumber (dataNameDefinition | FILLER)? redefinesClause? tcExtTypedefClause?
+		levelNumber (dataNameDefinition | FILLER)? redefinesClause? cob2002TypedefClause?
 		( pictureClause
 		| blankWhenZeroClause
 		| externalClause
@@ -22,12 +22,12 @@ dataDescriptionEntry:
 		| synchronizedClause
 		| usageClause
 		| valueClause
-		| tcExtTypeClause
+		| cob2002TypeClause
 		)* PeriodSeparator
 	)
 	| dataRenamesEntry
 	| dataConditionEntry;
 
-tcExtTypedefClause: TYPEDEF STRONG?;
+cob2002TypedefClause: TYPEDEF STRONG?;
 
-tcExtTypeClause:    TYPE UserDefinedWord;
+cob2002TypeClause:    TYPE UserDefinedWord;

TypeCobol.Test/Compiler/Parser/Samples/Programs/Cobol2002/Typedef.pgm
@@ -2,6 +2,8 @@
   PROGRAM-ID. TypeCobol.
 
 DATA DIVISION.
+
+
 WORKING-STORAGE SECTION.
 
 01 Feature TYPEDEF.
@@ -76,6 +78,11 @@ WORKING-STORAGE SECTION.
 
 
 PROCEDURE DIVISION.
+01 OutOfPerimeter2 TYPEDEF.
+  02 OutOfPerimeter2-var1 PIC X.
+
 MOVE X OF Coord2d OF Accel2d OF Movment2d OF Point TO X OF Coord2d OF Speed2d OF Movment2d OF Point.
 MOVE Y OF Coord2d OF Position2d TO Z OF Coord3d OF Position2d.
 MOVE X OF Coord3d TO Y.
+
+*------>Except a Type is not referenced on lines 34, 45, 49, 59

TypeCobol.Test/Compiler/Parser/Samples/Programs/TypeCobol/Type-Bool.pgm
@@ -1,0 +1,21 @@
+﻿IDENTIFICATION DIVISION.
+PROGRAM-ID.   TEST-BOOL.
+DATA DIVISION.
+**************
+WORKING-STORAGE SECTION.
+01 zoneTxt       pic X.
+01  check        TYPE BOOL.
+01  managed      TYPE BOOL.
+PROCEDURE DIVISION.
+TEST-BOOL.
+*** legal: from typed to typed
+move zoneTxt to zoneTxt
+MOVE check TO managed
+*** KO, check-value doesn't exist in TypeCobol (only in generated Code)
+MOVE check-value TO zoneTxt
+
+*** KO, check-false doesn't exist in TypeCobol (only in generated Code)
+if check-false
+    continue
+end-if
+.

TypeCobol.Test/Compiler/Parser/Samples/Programs/TypeCobol/Type-Date.pgm
@@ -8,16 +8,16 @@ DATA DIVISION.
 **************
 WORKING-STORAGE SECTION.
 
-01  TC-DATE        TYPEDEF STRONG.
-  10  YYYY         PIC 9(04).
-  10  MM           PIC 9(02).
-  10  DD           PIC 9(02).
+01 W-DATE TYPEDEF STRONG.
+    05 yyyy pic 9(04).
+    05 mm pic 9(02).
+    05 dd pic 9(02).
 
 01  olddate        PIC X(08).
-01  today          TYPE TC-DATE.
-01  tomorrow       TYPE TC-DATE.
+01  today          TYPE Date.
+01  tomorrow       TYPE Date.
 01 groupe.
-   05 date1        type TC-DATE.
+   05 date1        type Date.
 PROCEDURE DIVISION.
 *******************
 TEST-DATE.

TypeCobol.Test/TypeCobol.Test.csproj
@@ -115,8 +115,9 @@
     <Content Include="Compiler\Parser\ResultFiles\FD.txt" />
     <Content Include="Compiler\Parser\ResultFiles\Programs\MemoryMEM.txt" />
     <Content Include="Compiler\Parser\ResultFiles\Programs\TypeCobol\TablesPGM.txt" />
+    <Content Include="Compiler\Parser\ResultFiles\Programs\TypeCobol\Type-BoolPGM.txt" />
     <Content Include="Compiler\Parser\ResultFiles\Programs\TypeCobol\Type-DatePGM.txt" />
-    <Content Include="Compiler\Parser\ResultFiles\Programs\TypedefPGM.txt" />
+    <Content Include="Compiler\Parser\ResultFiles\Programs\Cobol2002\TypedefPGM.txt" />
     <Content Include="Compiler\Parser\ResultFiles\Programs\EmptyPGM.txt" />
     <Content Include="Compiler\Parser\ResultFiles\Programs\SimplePGM.txt" />
     <Content Include="Compiler\Parser\ResultFiles\Programs\Simple.0.txt" />
@@ -284,8 +285,9 @@
     <None Include="Compiler\Parser\Samples\FD.cbl" />
     <None Include="Compiler\Parser\Samples\Programs\Memory.pgm" />
     <None Include="Compiler\Parser\Samples\Programs\TypeCobol\Tables.pgm" />
+    <None Include="Compiler\Parser\Samples\Programs\TypeCobol\Type-Bool.pgm" />
     <None Include="Compiler\Parser\Samples\Programs\TypeCobol\Type-Date.pgm" />
-    <None Include="Compiler\Parser\Samples\Programs\Typedef.pgm" />
+    <None Include="Compiler\Parser\Samples\Programs\Cobol2002\Typedef.pgm" />
     <None Include="Compiler\Parser\Samples\Programs\TypeCobol\DoubleColon.pgm" />
     <None Include="Compiler\Pipeline\Samples\BigBatch.cbl" />
     <None Include="Compiler\Parser\Samples\Programs\Empty.pgm" />

TypeCobol/Compiler/Diagnostics/Cobol2002CodeElementChecker.cs
@@ -1,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Antlr4.Runtime;
+using TypeCobol.Compiler.AntlrUtils;
+using TypeCobol.Compiler.CodeElements;
+using TypeCobol.Compiler.CodeModel;
+using TypeCobol.Compiler.Parser;
+using TypeCobol.Compiler.Parser.Generated;
+
+namespace TypeCobol.Compiler.Diagnostics {
+
+	class Cobol2002TypeDefChecker : CodeElementListener {
+		public IList<Type> GetCodeElements() {
+			return new List<Type> { typeof(DataDescriptionEntry) };
+		}
+		public void OnCodeElement(CodeElement e, ParserRuleContext context) {
+			if (e is DataDescriptionEntry)
+				OnDataDescriptionEntry(e as DataDescriptionEntry, context as CodeElementsParser.DataDescriptionEntryContext);
+		}
+
+		private void OnDataDescriptionEntry(DataDescriptionEntry data, CodeElementsParser.DataDescriptionEntryContext context) {
+		    if (data.IsTypeDefinition)
+            { 
+		        if (data.LevelNumber != 1)
+		        {
+		            string message = "TYPEDEF clause can only be specified for level 01 entries";
+		            DiagnosticUtils.AddError(data, message, context.cob2002TypedefClause());
+		        }
+		        if (data.Picture != null && (data.DataType != null && data.DataType.IsStrong))
+		        {
+		            string message = "Elementary TYPEDEF cannot be STRONG";
+		            string rulestack = new RuleStackBuilder().GetRuleStack(context.cob2002TypedefClause());
+		            DiagnosticUtils.AddError(data, message,
+		                ParseTreeUtils.GetFirstToken(context.cob2002TypedefClause().STRONG()), rulestack);
+		        }
+		        if (data.RedefinesDataName != null)
+		        {
+		            string message = "REDEFINES clause cannot be specified with TYPEDEF clause";
+		            DiagnosticUtils.AddError(data, message, context.redefinesClause());
+		        }
+		        if (data.IsExternal)
+		        {
+		            string message = "EXTERNAL clause cannot be specified with TYPEDEF clause";
+		            foreach (var external in context.externalClause())
+		                DiagnosticUtils.AddError(data, message, external);
+		        }
+		    }
+        }
+	}
+}

TypeCobol/Compiler/Diagnostics/CodeElementCheckers.cs
@@ -12,7 +12,7 @@ namespace TypeCobol.Compiler.Diagnostics {
 	class DataDescriptionChecker: CodeElementListener {
 
 		public IList<Type> GetCodeElements() {
-			return new List<Type>() { typeof(DataDescriptionEntry), };
+			return new List<Type> { typeof(DataDescriptionEntry) };
 		}
 		public void OnCodeElement(CodeElement e, ParserRuleContext c) {
 			var data = e as DataDescriptionEntry;
@@ -35,12 +35,20 @@ namespace TypeCobol.Compiler.Diagnostics {
 			var occurs    = GetContext(data, context.occursClause());
 			var value     = GetContext(data, context.valueClause());
 
-			if (data.Name == null) {
-                if (data.IsExternal)
-                    DiagnosticUtils.AddError(data, "Data name must be specified for any entry containing the EXTERNAL clause", external);
-                if (data.IsGlobal)
-					DiagnosticUtils.AddError(data, "Data name must be specified for any entry containing the GLOBAL clause", global);
-			}
+		    if (data.Name == null)
+		    {
+		        if (data.IsExternal)
+		            DiagnosticUtils.AddError(data, "Data name must be specified for any entry containing the EXTERNAL clause",
+		                external);
+		        if (data.IsGlobal)
+		            DiagnosticUtils.AddError(data, "Data name must be specified for any entry containing the GLOBAL clause",
+		                global);
+		    } else { 
+		        if (data.IsExternal && data.LevelNumber != 01)
+                {
+                    DiagnosticUtils.AddError(data, "External is only allowed for level 01", external);
+                }
+            }
         }
         private void OnDataConditionEntry(DataDescriptionEntry data, CodeElementsParser.DataConditionEntryContext context) {
 			if (data.LevelNumber != 88)
@@ -294,7 +302,7 @@ namespace TypeCobol.Compiler.Diagnostics {
 				var receiving = lr[0];
 				if (receiving.DataType != sending && receiving.DataType.IsStrong) {
 					string message = "Writing "+sending+" to "+receiving.Name+":"+receiving.DataType+" is unsafe";
-					DiagnosticUtils.AddError(e, message, Diagnostics.MessageCode.SyntaxWarningInParser);
+					DiagnosticUtils.AddError(e, message);
 				}
 				CheckNesting(e, receiving);
 			}
@@ -310,71 +318,4 @@ namespace TypeCobol.Compiler.Diagnostics {
 		}
 	}
 
-	class ReadOnlyPropertiesChecker: ProgramListener {
-
-		private static string[] READONLY_DATATYPES = new string[] { "TC-DATE", };
-
-		public IList<Type> GetCodeElements() {
-			return new List<Type>() { typeof(TypeCobol.Compiler.CodeModel.SymbolWriter), };
-		}
-		public void OnCodeElement(CodeElement e, ParserRuleContext c, Program program) {
-			var element = e as TypeCobol.Compiler.CodeModel.SymbolWriter;
-			var table = program.SymbolTable;
-			foreach(var pair in element.Symbols) {
-				if (pair.Item2 == null) continue; // no receiving item
-				var lr = table.Get(pair.Item2);
-				if (lr.Count != 1) continue; // ambiguity or not referenced; not my job
-				var receiving = lr[0];
-				checkReadOnly(e, receiving);
-			}
-		}
-
-		private static void checkReadOnly(CodeElement e, DataDescriptionEntry receiving) {
-			if (receiving.TopLevel == null) return;
-			if (receiving.TopLevel.DataType == null) return;
-			foreach(var type in READONLY_DATATYPES) {
-				if (type.Equals(receiving.TopLevel.DataType.Name)) {
-					DiagnosticUtils.AddError(e, type+" properties are read-only");
-				}
-			}
-		}
-	}
-
-	class FunctionChecker: ProgramListener {
-		public IList<Type> GetCodeElements() {
-			return new List<Type>() { typeof(TypeCobol.Compiler.CodeModel.IdentifierUser), };
-		}
-		public void OnCodeElement(CodeElement e, ParserRuleContext context, Program program) {
-			var element = e as TypeCobol.Compiler.CodeModel.IdentifierUser;
-			foreach (var identifier in element.Identifiers) {
-				CheckIdentifier(e, program.SymbolTable, identifier);
-			}
-		}
-		private static void CheckIdentifier(CodeElement e, SymbolTable table, Identifier identifier) {
-			var fun = identifier as FunctionReference;
-			if (fun == null) return;// we only check functions
-			var def = table.GetFunction(fun.Name);
-			if (def == null) return;// ambiguity is not our job
-			if (fun.Parameters.Count > def.Parameters.Count)
-				DiagnosticUtils.AddError(e, "Function "+def.Name+" only takes "+def.Parameters.Count+" parameters");
-			for(int c=0; c<def.Parameters.Count; c++) {
-				var expected = def.Parameters[c];
-				if (c < fun.Parameters.Count) {
-					var actual = fun.Parameters[c].Value;
-					if (actual is Identifier) {
-						var found = table.Get(((Identifier)actual).Name);
-						if (found.Count != 1) continue;// ambiguity is not our job
-						var type = found[0].DataType;
-						if (!type.Equals(expected.Type))
-							DiagnosticUtils.AddError(e, "Function "+def.Name+" expected parameter "+(c+1)+" of type "+expected.Type+" (actual: "+type+')');
-						var length = found[0].MemoryArea.Length;
-						if (length > expected.Length)
-							DiagnosticUtils.AddError(e, "Function "+def.Name+" expected parameter "+(c+1)+" of max length "+expected.Length+" (actual: "+length+')');
-					}
-				} else {
-					DiagnosticUtils.AddError(e, "Function "+def.Name+" is missing parameter "+(c+1)+" of type "+expected.Type);
-				}
-			}
-		}
-	}
 }

commit bcd828443f7a74197e0d18b6b373eca3f8d3967b

issue #226: fix function parameters length check that was broken since 8f35334dc05c9369399684af52f1c3bca1061fd9.

TypeCobol/Compiler/Diagnostics/TypeCobolChecker.cs
@@ -11,105 +11,89 @@ using TypeCobol.Compiler.Parser.Generated;
 namespace TypeCobol.Compiler.Diagnostics {
 
 
-    class ReadOnlyPropertiesChecker : NodeListener
-    {
+	class ReadOnlyPropertiesChecker : NodeListener
+	{
 
-        private static string[] READONLY_DATATYPES = { "TC-DATE"};
+		private static string[] READONLY_DATATYPES = { "TC-DATE" };
 
-        public IList<Type> GetCodeElements()
-        {
-            return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.SymbolWriter), };
-        }
-        public void OnNode(Node node, ParserRuleContext c, Program program)
-        {
-            var element = node.CodeElement as TypeCobol.Compiler.CodeModel.SymbolWriter;
-            var table = program.SymbolTable;
-            foreach (var pair in element.Symbols)
-            {
-                if (pair.Item2 == null) continue; // no receiving item
-                var lr = table.Get(pair.Item2);
-                if (lr.Count != 1) continue; // ambiguity or not referenced; not my job
-                var receiving = lr[0];
-                checkReadOnly(node.CodeElement, receiving);
-            }
-        }
+		public IList<Type> GetCodeElements()
+		{
+			return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.SymbolWriter), };
+		}
+		public void OnNode(Node node, ParserRuleContext c, Program program)
+		{
+			var element = node.CodeElement as TypeCobol.Compiler.CodeModel.SymbolWriter;
+			var table = program.SymbolTable;
+			foreach (var pair in element.Symbols)
+			{
+				if (pair.Item2 == null) continue; // no receiving item
+				var lr = table.Get(pair.Item2);
+				if (lr.Count != 1) continue; // ambiguity or not referenced; not my job
+				var receiving = lr[0];
+				checkReadOnly(node.CodeElement, receiving);
+			}
+		}
 
-        private static void checkReadOnly(CodeElement e, DataDescriptionEntry receiving)
-        {
-            if (receiving.TopLevel == null) return;
-            if (receiving.TopLevel.DataType == null) return;
-            foreach (var type in READONLY_DATATYPES)
-            {
-                if (type.Equals(receiving.TopLevel.DataType.Name.ToLower()))
-                {
-                    DiagnosticUtils.AddError(e, type + " properties are read-only");
-                }
-            }
-        }
-    }
+		private static void checkReadOnly(CodeElement e, DataDescriptionEntry receiving)
+		{
+			if (receiving.TopLevel == null) return;
+			if (receiving.TopLevel.DataType == null) return;
+			foreach (var type in READONLY_DATATYPES)
+			{
+				if (type.Equals(receiving.TopLevel.DataType.Name.ToLower()))
+				{
+					DiagnosticUtils.AddError(e, type + " properties are read-only");
+				}
+			}
+		}
+	}
 
-    class FunctionChecker : NodeListener
-    {
-        public IList<Type> GetCodeElements()
-        {
-            return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.IdentifierUser), };
-        }
+	class FunctionChecker: NodeListener {
+		public IList<Type> GetCodeElements() {
+			return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.IdentifierUser), };
+		}
 
-        public void OnNode(Node node, ParserRuleContext context, Program program)
-        {
-            var element = node.CodeElement as TypeCobol.Compiler.CodeModel.IdentifierUser;
-            foreach (var identifier in element.Identifiers)
-            {
-                CheckIdentifier(node.CodeElement, program.SymbolTable, identifier);
-            }
-        }
+		public void OnNode(Node node, ParserRuleContext context, Program program) {
+			var element = node.CodeElement as TypeCobol.Compiler.CodeModel.IdentifierUser;
+			foreach (var identifier in element.Identifiers) {
+				CheckIdentifier(node.CodeElement, program.SymbolTable, identifier);
+			}
+		}
 
-        private static void CheckIdentifier(CodeElement e, SymbolTable table, Identifier identifier)
-        {
-            var fun = identifier as FunctionReference;
-            if (fun == null) return;// we only check functions
-            var def = table.GetFunction(fun.Name);
-            if (def == null) return;// ambiguity is not our job
-            if (fun.Parameters.Count > def.Parameters.Count)
-                DiagnosticUtils.AddError(e,
-                    "Function " + def.Name + " only takes " + def.Parameters.Count + " parameters");
-            for (int c = 0; c < def.Parameters.Count; c++)
-            {
-                var expected = def.Parameters[c];
-                if (c < fun.Parameters.Count)
-                {
-                    var actual = fun.Parameters[c].Value;
-                    if (actual is Identifier)
-                    {
-                        var found = table.Get(((Identifier)actual).Name);
-                        if (found.Count != 1) continue;// ambiguity is not our job
-                        var type = found[0].DataType;
-                        try
-                        {
-                            if (!type.Name.ToUpper().Equals(expected.Type.Name.ToUpper()))
-                            {
-                                //Console.WriteLine("param=" + expected);
-                                //Console.WriteLine("param.Definition=" + expected.Definition);
-                                DiagnosticUtils.AddError(e,
-                                    "Function " + def.Name + " expected parameter " + (c + 1) + " of type " +
-                                    expected.Type + " (actual: " + type + ')');
-                            }
-                        }
-                        catch (NullReferenceException)
-                        {
-                            //var length = found[0].MemoryArea.Length;
-                            //if (length > expected.Length)
-                            //	DiagnosticUtils.AddError(e, "Function "+def.Name+" expected parameter "+(c+1)+" of max length "+expected.Length+" (actual: "+length+')');
-                        }
-                    }
-                    else
-                    {
-                        DiagnosticUtils.AddError(e, "Function " + def.Name + " is missing parameter " + (c + 1) + " of type " + expected.Type);
-                    }
-                }
-            }
-        }
-    }
+		private static void CheckIdentifier(CodeElement e, SymbolTable table, Identifier identifier) {
+			var fun = identifier as FunctionReference;
+			if (fun == null) return;// we only check functions
+			var def = table.GetFunction(fun.Name);
+			if (def == null) return;// ambiguity is not our job
+			if (fun.Parameters.Count > def.Parameters.Count) {
+				var message = String.Format("Function {0} only takes {1} parameters", def.Name, def.Parameters.Count);
+				DiagnosticUtils.AddError(e, message);
+			}
+			for (int c = 0; c < def.Parameters.Count; c++) {
+				var expected = def.Parameters[c];
+				if (c < fun.Parameters.Count) {
+					var actual = fun.Parameters[c].Value;
+					if (actual is Identifier) {
+						var found = table.Get(((Identifier)actual).Name);
+						if (found.Count != 1) continue;// ambiguity is not our job
+						var type = found[0].DataType;
+						if (type != expected.Type) {
+							var message = String.Format("Function {0} expected parameter {1} of type {2} (actual: {3})", def.Name, c+1, expected.Type, type);
+							DiagnosticUtils.AddError(e, message);
+						}
+						var length = found[0].MemoryArea.Length;
+						if (length > expected.Length) {
+							var message = String.Format("Function {0} expected parameter {1} of max length {2} (actual: {3})", def.Name, c+1, expected.Length, length);
+							DiagnosticUtils.AddError(e, message);
+						}
+					} else {
+						var message = String.Format("Function {0} is missing parameter {1} of type {2}", def.Name, c+1, expected.Type);
+						DiagnosticUtils.AddError(e, message);
+					}
+				}
+			}
+		}
+	}
 
 
 }
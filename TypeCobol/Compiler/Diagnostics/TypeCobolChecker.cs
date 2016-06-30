bcd828443f7a74197e0d18b6b373eca3f8d3967b
wiztigers <regis.witz@externe.e-i.com>
2016-06-30 14:17:23 +0200

    issue #226: fix function parameters length check that was broken since 8f35334dc05c9369399684af52f1c3bca1061fd9.

--- TypeCobol/Compiler/Diagnostics/TypeCobolChecker.cs
+++ TypeCobol/Compiler/Diagnostics/TypeCobolChecker.cs
@@ -11,105 +11,89 @@ using TypeCobol.Compiler.Parser.Generated;
 namespace TypeCobol.Compiler.Diagnostics {
 
 
-    class ReadOnlyPropertiesChecker : NodeListener
-    {
+	class ReadOnlyPropertiesChecker : NodeListener
+	{
 
-        private static string[] READONLY_DATATYPES = { "TC-DATE"};
+		private static string[] READONLY_DATATYPES = { "TC-DATE" };
 
-        public IList<Type> GetCodeElements()
-        {
-            return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.SymbolWriter), };
-        }
-        public void OnNode(Node node, ParserRuleContext c, Program program)
-        {
-            var element = node.CodeElement as TypeCobol.Compiler.CodeModel.SymbolWriter;
-            var table = program.SymbolTable;
-            foreach (var pair in element.Symbols)
-            {
-                if (pair.Item2 == null) continue; // no receiving item
-                var lr = table.Get(pair.Item2);
-                if (lr.Count != 1) continue; // ambiguity or not referenced; not my job
-                var receiving = lr[0];
-                checkReadOnly(node.CodeElement, receiving);
-            }
-        }
+		public IList<Type> GetCodeElements()
+		{
+			return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.SymbolWriter), };
+		}
+		public void OnNode(Node node, ParserRuleContext c, Program program)
+		{
+			var element = node.CodeElement as TypeCobol.Compiler.CodeModel.SymbolWriter;
+			var table = program.SymbolTable;
+			foreach (var pair in element.Symbols)
+			{
+				if (pair.Item2 == null) continue; // no receiving item
+				var lr = table.Get(pair.Item2);
+				if (lr.Count != 1) continue; // ambiguity or not referenced; not my job
+				var receiving = lr[0];
+				checkReadOnly(node.CodeElement, receiving);
+			}
+		}
 
-        private static void checkReadOnly(CodeElement e, DataDescriptionEntry receiving)
-        {
-            if (receiving.TopLevel == null) return;
-            if (receiving.TopLevel.DataType == null) return;
-            foreach (var type in READONLY_DATATYPES)
-            {
-                if (type.Equals(receiving.TopLevel.DataType.Name.ToLower()))
-                {
-                    DiagnosticUtils.AddError(e, type + " properties are read-only");
-                }
-            }
-        }
-    }
+		private static void checkReadOnly(CodeElement e, DataDescriptionEntry receiving)
+		{
+			if (receiving.TopLevel == null) return;
+			if (receiving.TopLevel.DataType == null) return;
+			foreach (var type in READONLY_DATATYPES)
+			{
+				if (type.Equals(receiving.TopLevel.DataType.Name.ToLower()))
+				{
+					DiagnosticUtils.AddError(e, type + " properties are read-only");
+				}
+			}
+		}
+	}
 
-    class FunctionChecker : NodeListener
-    {
-        public IList<Type> GetCodeElements()
-        {
-            return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.IdentifierUser), };
-        }
+	class FunctionChecker: NodeListener {
+		public IList<Type> GetCodeElements() {
+			return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.IdentifierUser), };
+		}
 
-        public void OnNode(Node node, ParserRuleContext context, Program program)
-        {
-            var element = node.CodeElement as TypeCobol.Compiler.CodeModel.IdentifierUser;
-            foreach (var identifier in element.Identifiers)
-            {
-                CheckIdentifier(node.CodeElement, program.SymbolTable, identifier);
-            }
-        }
+		public void OnNode(Node node, ParserRuleContext context, Program program) {
+			var element = node.CodeElement as TypeCobol.Compiler.CodeModel.IdentifierUser;
+			foreach (var identifier in element.Identifiers) {
+				CheckIdentifier(node.CodeElement, program.SymbolTable, identifier);
+			}
+		}
 
-        private static void CheckIdentifier(CodeElement e, SymbolTable table, Identifier identifier)
-        {
-            var fun = identifier as FunctionReference;
-            if (fun == null) return;// we only check functions
-            var def = table.GetFunction(fun.Name);
-            if (def == null) return;// ambiguity is not our job
-            if (fun.Parameters.Count > def.Parameters.Count)
-                DiagnosticUtils.AddError(e,
-                    "Function " + def.Name + " only takes " + def.Parameters.Count + " parameters");
-            for (int c = 0; c < def.Parameters.Count; c++)
-            {
-                var expected = def.Parameters[c];
-                if (c < fun.Parameters.Count)
-                {
-                    var actual = fun.Parameters[c].Value;
-                    if (actual is Identifier)
-                    {
-                        var found = table.Get(((Identifier)actual).Name);
-                        if (found.Count != 1) continue;// ambiguity is not our job
-                        var type = found[0].DataType;
-                        try
-                        {
-                            if (!type.Name.ToUpper().Equals(expected.Type.Name.ToUpper()))
-                            {
-                                //Console.WriteLine("param=" + expected);
-                                //Console.WriteLine("param.Definition=" + expected.Definition);
-                                DiagnosticUtils.AddError(e,
-                                    "Function " + def.Name + " expected parameter " + (c + 1) + " of type " +
-                                    expected.Type + " (actual: " + type + ')');
-                            }
-                        }
-                        catch (NullReferenceException)
-                        {
-                            //var length = found[0].MemoryArea.Length;
-                            //if (length > expected.Length)
-                            //	DiagnosticUtils.AddError(e, "Function "+def.Name+" expected parameter "+(c+1)+" of max length "+expected.Length+" (actual: "+length+')');
-                        }
-                    }
-                    else
-                    {
-                        DiagnosticUtils.AddError(e, "Function " + def.Name + " is missing parameter " + (c + 1) + " of type " + expected.Type);
-                    }
-                }
-            }
-        }
-    }
+		private static void CheckIdentifier(CodeElement e, SymbolTable table, Identifier identifier) {
+			var fun = identifier as FunctionReference;
+			if (fun == null) return;// we only check functions
+			var def = table.GetFunction(fun.Name);
+			if (def == null) return;// ambiguity is not our job
+			if (fun.Parameters.Count > def.Parameters.Count) {
+				var message = String.Format("Function {0} only takes {1} parameters", def.Name, def.Parameters.Count);
+				DiagnosticUtils.AddError(e, message);
+			}
+			for (int c = 0; c < def.Parameters.Count; c++) {
+				var expected = def.Parameters[c];
+				if (c < fun.Parameters.Count) {
+					var actual = fun.Parameters[c].Value;
+					if (actual is Identifier) {
+						var found = table.Get(((Identifier)actual).Name);
+						if (found.Count != 1) continue;// ambiguity is not our job
+						var type = found[0].DataType;
+						if (type != expected.Type) {
+							var message = String.Format("Function {0} expected parameter {1} of type {2} (actual: {3})", def.Name, c+1, expected.Type, type);
+							DiagnosticUtils.AddError(e, message);
+						}
+						var length = found[0].MemoryArea.Length;
+						if (length > expected.Length) {
+							var message = String.Format("Function {0} expected parameter {1} of max length {2} (actual: {3})", def.Name, c+1, expected.Length, length);
+							DiagnosticUtils.AddError(e, message);
+						}
+					} else {
+						var message = String.Format("Function {0} is missing parameter {1} of type {2}", def.Name, c+1, expected.Type);
+						DiagnosticUtils.AddError(e, message);
+					}
+				}
+			}
+		}
+	}
 
 
 }
