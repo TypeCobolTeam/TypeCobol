TypeCobolTeam/TypeCobol

commit ba677ada14fca947194c82a46c702dd1a9c5ae0c

issue #230: change ProgramListener interface to take a Node instead of a CodeElement, and rename it to NodeListener

TypeCobol/Compiler/Parser/CodeElementDispatcher.cs
@@ -60,56 +60,56 @@ namespace TypeCobol.Compiler.Parser
 
 
 
-    public interface ProgramListener {
-        /// <summary>
-        /// Returns the type(s) of CodeElement the listener is interested in.
-        /// The listener will be notified about the creation of a CodeElement
-        /// only if the CodeElement type matches at least one of the Types
-        /// returned by this method.
-        /// </summary>
-        /// <returns>IList of CodeElement Types</returns>
-        IList<System.Type> GetCodeElements();
+	public interface NodeListener {
+		/// <summary>
+		/// Returns the type(s) of Node the listener is interested in.
+		/// The listener will be notified about the creation of a Node
+		/// only if the Node's CodeElement member type matches at least
+		/// one of the Types returned by this method.
+		/// </summary>
+		/// <returns>IList of CodeElement Types</returns>
+		IList<System.Type> GetCodeElements();
 
-        /// <summary>
-        /// Called when a CodeElement is created during ProgramClassParserStep,
-        /// if the CodeElement type equals one of those returned by GetCodeElements.
-        /// </summary>
-        /// <param name="ce">CodeElement created</param>
-        /// <param name="context">Context associated to ce's creation</param>
-        /// <param name="program">Current scope program</param>
-        void OnCodeElement(CodeElement ce, ParserRuleContext context, Program program);
-    }
+		/// <summary>
+		/// Called when a CodeElement is created during ProgramClassParserStep,
+		/// if the CodeElement type equals one of those returned by GetCodeElements.
+		/// </summary>
+		/// <param name="ce">CodeElement created</param>
+		/// <param name="context">Context associated to ce's creation</param>
+		/// <param name="program">Current scope program</param>
+		void OnNode(Node node, ParserRuleContext context, Program program);
+	}
 
-    public class ProgramDispatcher: ProgramListener {
-        public IList<System.Type> GetCodeElements() { return null; }
+	public class NodeDispatcher: NodeListener {
+		public IList<System.Type> GetCodeElements() { return null; }
 
-        /// <summary>Notifies listeners about the creation of a new CodeElement.</summary>
-        public void OnCodeElement(CodeElement e, ParserRuleContext context, Program program) {
-            foreach(var listener in listeners) {
-                var types = listener.GetCodeElements();
-                foreach (var expected in types) {
-                    if (TypeCobol.Tools.Reflection.IsTypeOf(e.GetType(), expected)) {
-                        listener.OnCodeElement(e, context, program);
-                        break; // only notify each listener once for a given CodeElement
-                    }
-                }
-            }
-        }
+		/// <summary>Notifies listeners about the creation of a new CodeElement.</summary>
+		public void OnNode(Node node, ParserRuleContext context, Program program) {
+			foreach(var listener in listeners) {
+				var types = listener.GetCodeElements();
+				foreach (var expected in types) {
+					if (TypeCobol.Tools.Reflection.IsTypeOf(node!=null? node.CodeElement.GetType():null, expected)) {
+						listener.OnNode(node, context, program);
+						break; // only notify each listener once for a given CodeElement
+					}
+				}
+			}
+		}
 
-        private IList<ProgramListener> listeners = new List<ProgramListener>();
+		private IList<NodeListener> listeners = new List<NodeListener>();
 
-        /// <summary>
-        /// Adds to listeners one instance of each type implementing CodeElementListener interface
-        /// and defined in namespace TypeCobol.Compiler.Diagnostics.
-        /// TODO: the list of namespace where CodeElementListeners are searched for should not be hard-coded
-        /// </summary>
-        internal void CreateListeners() {
-            var namespaces = new string[] { "TypeCobol.Compiler.Diagnostics", };
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            foreach (var names in namespaces) {
-                var instances = TypeCobol.Tools.Reflection.GetInstances<ProgramListener>(assembly, names);
-                foreach (var checker in instances) listeners.Add(checker);
-            }
-        }
-    }
+		/// <summary>
+		/// Adds to listeners one instance of each type implementing CodeElementListener interface
+		/// and defined in namespace TypeCobol.Compiler.Diagnostics.
+		/// TODO: the list of namespace where CodeElementListeners are searched for should not be hard-coded
+		/// </summary>
+		internal void CreateListeners() {
+			var namespaces = new string[] { "TypeCobol.Compiler.Diagnostics", };
+			var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			foreach (var names in namespaces) {
+				var instances = TypeCobol.Tools.Reflection.GetInstances<NodeListener>(assembly, names);
+				foreach (var checker in instances) listeners.Add(checker);
+			}
+		}
+	}
 }

TypeCobol/Compiler/Diagnostics/TypeCobolProgramCheckers.cs
@@ -11,7 +11,7 @@ using TypeCobol.Compiler.Parser.Generated;
 namespace TypeCobol.Compiler.Diagnostics {
 
 
-    class ReadOnlyPropertiesChecker : ProgramListener
+    class ReadOnlyPropertiesChecker : NodeListener
     {
 
         private static string[] READONLY_DATATYPES = { "TC-DATE"};
@@ -20,9 +20,9 @@ namespace TypeCobol.Compiler.Diagnostics {
         {
             return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.SymbolWriter), };
         }
-        public void OnCodeElement(CodeElement e, ParserRuleContext c, Program program)
+        public void OnNode(Node node, ParserRuleContext c, Program program)
         {
-            var element = e as TypeCobol.Compiler.CodeModel.SymbolWriter;
+            var element = node.CodeElement as TypeCobol.Compiler.CodeModel.SymbolWriter;
             var table = program.SymbolTable;
             foreach (var pair in element.Symbols)
             {
@@ -30,7 +30,7 @@ namespace TypeCobol.Compiler.Diagnostics {
                 var lr = table.Get(pair.Item2);
                 if (lr.Count != 1) continue; // ambiguity or not referenced; not my job
                 var receiving = lr[0];
-                checkReadOnly(e, receiving);
+                checkReadOnly(node.CodeElement, receiving);
             }
         }
 
@@ -48,19 +48,19 @@ namespace TypeCobol.Compiler.Diagnostics {
         }
     }
 
-    class FunctionChecker : ProgramListener
+    class FunctionChecker : NodeListener
     {
         public IList<Type> GetCodeElements()
         {
             return new List<Type> { typeof(TypeCobol.Compiler.CodeModel.IdentifierUser), };
         }
 
-        public void OnCodeElement(CodeElement e, ParserRuleContext context, Program program)
+        public void OnNode(Node node, ParserRuleContext context, Program program)
         {
-            var element = e as TypeCobol.Compiler.CodeModel.IdentifierUser;
+            var element = node.CodeElement as TypeCobol.Compiler.CodeModel.IdentifierUser;
             foreach (var identifier in element.Identifiers)
             {
-                CheckIdentifier(e, program.SymbolTable, identifier);
+                CheckIdentifier(node.CodeElement, program.SymbolTable, identifier);
             }
         }
 

TypeCobol/Compiler/Parser/ProgramClassBuilder.cs
@@ -53,15 +53,16 @@ namespace TypeCobol.Compiler.Parser
 			catch(ArgumentException ex) { table.RegisterCustomType(new CustomTypeDefinition(type)); }
 		}
 
-        public ProgramDispatcher Dispatcher { get; internal set; }
+        public NodeDispatcher Dispatcher { get; internal set; }
 
 		private void _add(Node node) {
 			node.SymbolTable = CurrentProgram.SymbolTable;
 			Program.SyntaxTree.Add(node);
 		}
 		private void _enter(CodeElement e, ParserRuleContext context) {
-			_enter(new Node(e));
-			if (e!=null) Dispatcher.OnCodeElement(e, context, CurrentProgram);
+			var node = new Node(e);
+			_enter(node);
+			if (e!=null) Dispatcher.OnNode(node, context, CurrentProgram);
 		}
 		private void _enter(Node node) {
 			node.SymbolTable = CurrentProgram.SymbolTable;

TypeCobol/Compiler/Parser/ProgramClassParserStep.cs
@@ -43,7 +43,7 @@ namespace TypeCobol.Compiler.Parser
             ParseTreeWalker walker = new ParseTreeWalker();
             ProgramClassBuilder programClassBuilder = new ProgramClassBuilder();
 			programClassBuilder.CustomSymbols = customSymbols;
-            programClassBuilder.Dispatcher = new ProgramDispatcher();
+            programClassBuilder.Dispatcher = new NodeDispatcher();
             programClassBuilder.Dispatcher.CreateListeners();
             walker.Walk(programClassBuilder, codeElementParseTree);
                         

commit 42792bce7b19308505e92b7fd08f1e719cce98b1

issue #230: fix potential NPE introduced by ba677ada14fca947194c82a46c702dd1a9c5ae0c

TypeCobol/Compiler/Parser/CodeElementDispatcher.cs
@@ -88,7 +88,7 @@ namespace TypeCobol.Compiler.Parser
 			foreach(var listener in listeners) {
 				var types = listener.GetCodeElements();
 				foreach (var expected in types) {
-					if (TypeCobol.Tools.Reflection.IsTypeOf(node!=null? node.CodeElement.GetType():null, expected)) {
+					if (TypeCobol.Tools.Reflection.IsTypeOf(node.CodeElement!=null? node.CodeElement.GetType():null, expected)) {
 						listener.OnNode(node, context, program);
 						break; // only notify each listener once for a given CodeElement
 					}