20ef4e489428864ece8e3b027bddcd2e34c8ff23
smedilol <olivier.smedile@e-i.com>
2016-07-08 16:41:28 +0200

    issue #243, new bug with replace

--- TypeCobol.Test/NewTestsToFix/Parser/Samples/Replace.cbl
+++ TypeCobol.Test/NewTestsToFix/Parser/Samples/Replace.cbl
@@ -1,10 +1,10 @@
 ﻿ IDENTIFICATION DIVISION.                                        
  PROGRAM-ID. PGMA.                                           
  DATA DIVISION.                                                  
- REPLACE ==UBININT-2== BY ==PIC 9(4)   COMP-4==.              
+ REPLACE ==UBININT-2== BY ==PIC 9==.              
  LOCAL-STORAGE SECTION.                                                                                 
- 01 var2                      UBININT-2.                  
- PROCEDURE DIVISION.                                                                                                                                               
+ 01 var UBININT-2.                
+ PROCEDURE DIVISION.                                                                                                                                            
      .                                                          
  REPLACE OFF.                                       
  END PROGRAM PGMA.                                                                                
