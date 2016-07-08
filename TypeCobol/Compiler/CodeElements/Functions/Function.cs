8e435e77de7ba1eae5c2c8f6fd52809ea6e92ecd
wiztigers <regis.witz@externe.e-i.com>
2016-07-08 12:40:57 +0200

    issue #245: functions declared parameters should be named

--- TypeCobol/Compiler/CodeElements/Functions/Function.cs
+++ TypeCobol/Compiler/CodeElements/Functions/Function.cs
@@ -41,11 +41,13 @@ namespace TypeCobol.Compiler.CodeElements.Functions {
 	}
 
 	public class Parameter {
+		public string Name;
 		public DataType Type;
 		public int Length;
 		public bool IsCustom;
 
-		public Parameter(bool isCustom, DataType type, int length=int.MaxValue) {
+		public Parameter(string name, bool isCustom, DataType type, int length=int.MaxValue) {
+			this.Name = name;
 			this.Type   = type;
 			this.Length = length;
 			if (length < 1) throw new System.ArgumentOutOfRangeException("Length must be >0 (actual: "+length+')');
@@ -64,14 +66,14 @@ namespace TypeCobol.Compiler.CodeElements.Functions {
 		}
 
 		public override string ToString() {
-			return (Type != null? Type.ToString() : "?") + (Length < int.MaxValue? '('+Length.ToString()+')' : "");
+			return (Name!=null? Name.ToString():"?")+ ' ' + (Type!=null? Type.ToString():"?") + (Length<int.MaxValue? '('+Length.ToString()+')':"");
 		}
 	}
 	public class CallParameter: Parameter {
 		public string Value { get; private set; }
 		public bool ByReference { get; private set; }
 		public CallParameter(string Value, bool ByReference = true)
-		  : base (false, null) {
+		  : base (null, false, null) {
 			this.Value = Value;
 			this.ByReference = ByReference;
 		}
@@ -90,15 +92,15 @@ namespace TypeCobol.Compiler.CodeElements.Functions {
 	public class SampleFactory {
 		public static Function Create(string name, string library = "TC-DEFAULT") {
 			return new Function(name, library,
-				new Parameter(false, DataType.Numeric, 8),
+				new Parameter(null, false, DataType.Numeric, 8),
 				new List<Parameter>() {
-					new Parameter(false, DataType.Numeric),
-					new Parameter(false, DataType.Numeric, 3),
+					new Parameter(null, false, DataType.Numeric),
+					new Parameter(null, false, DataType.Numeric, 3),
 				});
 		}
 		public static Function CreateCall(string name, string library = "TC-DEFAULT") {
 			return new Function(name, library,
-				new Parameter(false, DataType.Numeric, 8),
+				new Parameter(null, false, DataType.Numeric, 8),
 				new List<Parameter>() {
 					new CallParameter("param1"),
 					new CallParameter("'42'", false),
