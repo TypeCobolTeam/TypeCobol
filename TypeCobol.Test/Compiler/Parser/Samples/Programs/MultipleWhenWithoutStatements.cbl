8db83004cb8af1ce6b682d8cb42e1b7690d44ddf
smedilol <olivier.smedile@e-i.com>
2016-07-18 15:32:09 +0200

    Issue #243, oops test MultipleWhenWithoutStatements was in fact correct.

--- TypeCobol.Test/Compiler/Parser/Samples/Programs/MultipleWhenWithoutStatements.cbl
+++ TypeCobol.Test/Compiler/Parser/Samples/Programs/MultipleWhenWithoutStatements.cbl
@@ -1,5 +1,4 @@
-﻿*Don't except any errors
- IDENTIFICATION DIVISION.
+﻿ IDENTIFICATION DIVISION.
  PROGRAM-ID. MYPGM.
  ENVIRONMENT DIVISION.
  CONFIGURATION SECTION.
@@ -10,6 +9,7 @@
                add 1                            to NBR-ERR
              when TST-STT-OK
                add 1                            to NBR-OK
+*KO: when statement before a when other can't be empty			   
              when TST-STT-UNKNOWN
              when other
                 perform ABEND
