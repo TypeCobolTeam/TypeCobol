a2218a9057268e2205ba266ab25c2e87ed69db3f
wiztigers <regis.witz@externe.e-i.com>
2016-08-17 16:11:30 +0200

    fixes #265: inline types codegen.

--- Codegen/src/Nodes/TypedDataNode.cs
+++ Codegen/src/Nodes/TypedDataNode.cs
@@ -23,18 +23,25 @@ namespace TypeCobol.Codegen.Nodes {
 					int level = data.LevelNumber;
 					int generation = data.Generation;
 					bool isCustomType = type != null;
-					_cache.Add(CreateDataDefinition(data, level, generation, true));
+					_cache.Add(CreateDataDefinition(data, level, generation));
 					InsertChildren(data, level+1, generation+1);
 				}
 				return _cache;
 			}
 		}
 
-		internal ITextLine CreateDataDefinition(DataDescriptionEntry data, int level, int generation, bool isCustomType) {
+		internal ITextLine CreateDataDefinition(DataDescriptionEntry data, int level, int generation) {
 			var line = new System.Text.StringBuilder();
 			for(int c = 0; c < generation; c++) line.Append("  ");
 			line.Append(level.ToString("00")).Append(' ').Append(data.Name.Name);
-			if (!isCustomType) line.Append(" PIC ").Append(data.Picture);
+
+			string picture;
+			try {
+				var type = Node.SymbolTable.GetCustomType(data.DataType.Name);
+				if (type.Subordinates.Count > 0) picture = null;
+				else picture = ((DataDescriptionEntry)type).Picture;
+			} catch(System.ArgumentException) { picture = data.Picture; }
+			if (picture != null) line.Append(" PIC ").Append(picture);
 			line.Append('.');
 			return new TextLineSnapshot(-1, line.ToString(), null);
 		}
@@ -43,7 +50,7 @@ namespace TypeCobol.Codegen.Nodes {
 		private void InsertChildren(TypeDefinition type, int level, int generation) {
 			foreach(var child in type.Subordinates) {
 				bool isCustomTypeToo = !child.IsTypeDefinition && Node.SymbolTable.IsCustomType(child.DataType);
-				_cache.Add(CreateDataDefinition(child, level, generation, isCustomTypeToo));
+				_cache.Add(CreateDataDefinition(child, level, generation));
 				if (isCustomTypeToo) InsertChildren(Node.SymbolTable.GetCustomType(child.DataType.Name), level+1, generation+1);
 			}
 		}
