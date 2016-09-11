c9d09db3e2ccc60818dc71857e4373bd54d486b9
wiztigers <regis.witz@free.fr>
2016-09-11 15:21:36 +0200

    moar dumps

--- TypeCobol.Test/PlatformUtils.cs
+++ TypeCobol.Test/PlatformUtils.cs
@@ -11,20 +11,17 @@ namespace TypeCobol.Test
         /// </summary>
         public const string RELATIVE_TEST_PROJECT_PATH = "TypeCobol.Test";
         
-        static PlatformUtils()
-        {
-            // When tests are executed in Visual Studio test explorer
-            // the working directory is ....\TEST_PROJECT_PATH\bin\Debug\...
-            // => we can access all project files by extracting the start of 
-            // the project path on the local disk from the working directory        
+	static PlatformUtils() {
+		// When tests are executed in Visual Studio test explorer
+		// the working directory is ....\TEST_PROJECT_PATH\bin\Debug\...
+		// => we can access all project files by extracting the start of
+		// the project path on the local disk from the working directory
+		PWD = Directory.GetCurrentDirectory();
+		ABSOLUTE_TEST_PROJECT_PATH = pwd.Substring(0, workingDirectory.IndexOf(RELATIVE_TEST_PROJECT_PATH) + RELATIVE_TEST_PROJECT_PATH.Length);
+		}
 
-            string workingDirectory = Directory.GetCurrentDirectory();
-            ABSOLUTE_TEST_PROJECT_PATH = workingDirectory.Substring(0, workingDirectory.IndexOf(RELATIVE_TEST_PROJECT_PATH) + RELATIVE_TEST_PROJECT_PATH.Length);
-        }
-
-        /// <summary>
-        /// Absolute path of the test project on the local disk
-        /// </summary>
+		private static readonly string PWD;
+        /// <summary>Absolute path of the test project on the local disk</summary>
         private static readonly string ABSOLUTE_TEST_PROJECT_PATH;
 
 	/// <summary>
@@ -35,6 +32,8 @@ namespace TypeCobol.Test
 System.Console.WriteLine("relativeFilePath: \""+relativeFilePath+"\"");
             string pwd = Directory.GetCurrentDirectory();
 System.Console.WriteLine("GetCurrentDirectory(): \""+pwd+"\"");
+System.Console.WriteLine("PWD: \""+PWD+"\"");
+System.Console.WriteLine("Environment.CurrentDirectory: \""+Environment.CurrentDirectory+"\"");
 System.Console.WriteLine("ABSOLUTE_TEST_PROJECT_PATH: \""+ABSOLUTE_TEST_PROJECT_PATH+"\"");
 		return ABSOLUTE_TEST_PROJECT_PATH + Path.DirectorySeparatorChar + relativeFilePath;
 	}
