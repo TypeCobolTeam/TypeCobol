9e39cfa710cb2496d6b399693669c59eedd48d94
wiztigers <regis.witz@externe.e-i.com>
2016-09-22 17:31:34 +0200

    issue #249: fix CANCEL, INITIALIZE, DISPLAY, EVALUATE and (partially) CALL statements unit tests

--- TypeCobol/Compiler/CodeElements/Expressions/Variable.cs
+++ TypeCobol/Compiler/CodeElements/Expressions/Variable.cs
@@ -1,113 +1,94 @@
-﻿using System;
+﻿namespace TypeCobol.Compiler.CodeElements {
 
-namespace TypeCobol.Compiler.CodeElements
-{
-    /// <summary>
-    /// Compile-time value given by a syntax literal
-    ///  OR
-    /// Runtime value read in a storage area
-    /// </summary>
-    public abstract class VariableBase
-    {
-        public VariableBase(StorageDataType dataType, StorageArea storageArea)
-        {
-            DataType = dataType;
-            StorageArea = storageArea;
-        }
-
-        public StorageDataType DataType { get; private set; }
-
-        public StorageArea StorageArea { get; private set; }
-    }
-
-    public enum StorageDataType
-    {
-        Any,
-        Integer,
-        Numeric,
-        Character,
-        Alphanumeric,
-        ProgramName,
-        ProgramNameOrProgramEntry,
-        ProgramNameOrProgramEntryOrProcedurePointerOrFunctionPointer,
-        ClassNameOrObjectReference,
-        MethodName
-    }
-
-	public class IntegerVariable : VariableBase {
-		public IntegerVariable(StorageArea storageArea): base(StorageDataType.Integer, storageArea) { }
-		public IntegerVariable(IntegerValue value): base(StorageDataType.Integer, null) { Value = value; }
-
-		public IntegerValue Value { get; private set; }
+/// <summary>
+/// Compile-time value given by a syntax literal
+///  OR
+/// Runtime value read in a storage area
+/// </summary>
+public abstract class VariableBase {
+	public VariableBase(StorageDataType dataType, StorageArea storageArea) {
+		DataType = dataType;
+		StorageArea = storageArea;
 	}
 
-    public class NumericVariable : VariableBase
-    {
-        public NumericVariable(StorageArea storageArea) :
-            base(StorageDataType.Numeric, storageArea)
-        { }
+	public StorageDataType DataType { get; private set; }
+	public StorageArea StorageArea { get; private set; }
+}
 
-        public NumericVariable(NumericValue value) :
-            base(StorageDataType.Numeric, null)
-        {
-            Value = value;
-        }
+public enum StorageDataType {
+	Any,
+	Integer,
+	Numeric,
+	Character,
+	Alphanumeric,
+	ProgramName,
+	ProgramNameOrProgramEntry,
+	ProgramNameOrProgramEntryOrProcedurePointerOrFunctionPointer,
+	ClassNameOrObjectReference,
+	MethodName
+}
 
-        public NumericValue Value { get; private set; }
-    }
+public class IntegerVariable: VariableBase {
+	public IntegerVariable(StorageArea storageArea): base(StorageDataType.Integer, storageArea) { }
+	public IntegerVariable(IntegerValue value): base(StorageDataType.Integer, null) { Value = value; }
 
-    public class CharacterVariable : VariableBase
-    {
-        public CharacterVariable(StorageArea storageArea) :
-            base(StorageDataType.Character, storageArea)
-        { }
+	public IntegerValue Value { get; private set; }
+	public override string ToString() {
+		if (Value != null) return Value.ToString();
+		return base.ToString();
+	}
+}
 
-        public CharacterVariable(CharacterValue value) :
-            base(StorageDataType.Character, null)
-        {
-            Value = value;
-        }
+public class NumericVariable: VariableBase {
+	public NumericVariable(StorageArea storageArea): base(StorageDataType.Numeric, storageArea) { }
+	public NumericVariable(NumericValue value): base(StorageDataType.Numeric, null) { Value = value; }
 
-        public CharacterValue Value { get; private set; }
-    }
+	public NumericValue Value { get; private set; }
+	public override string ToString() {
+		if (Value != null) return Value.ToString();
+		return base.ToString();
+	}
+}
 
-    public class AlphanumericVariable : VariableBase
-    {
-        public AlphanumericVariable(StorageArea storageArea) :
-            base(StorageDataType.Alphanumeric, storageArea)
-        { }
+public class CharacterVariable: VariableBase {
+	public CharacterVariable(StorageArea storageArea): base(StorageDataType.Character, storageArea) { }
+	public CharacterVariable(CharacterValue value): base(StorageDataType.Character, null) { Value = value; }
 
-        public AlphanumericVariable(AlphanumericValue value) :
-            base(StorageDataType.Alphanumeric, null)
-        {
-            Value = value;
-        }
+	public CharacterValue Value { get; private set; }
+	public override string ToString() {
+		if (Value != null) return Value.ToString();
+		return base.ToString();
+	}
+}
 
-        public AlphanumericVariable(RepeatedCharacterValue repeadtedValue) :
-            base(StorageDataType.Alphanumeric, null)
-        {
-            RepeatedCharacterValue = repeadtedValue;
-        }
+public class AlphanumericVariable: VariableBase {
+	public AlphanumericVariable(StorageArea storageArea): base(StorageDataType.Alphanumeric, storageArea) { }
+	public AlphanumericVariable(AlphanumericValue value): base(StorageDataType.Alphanumeric, null) { Value = value; }
+	public AlphanumericVariable(RepeatedCharacterValue value): base(StorageDataType.Alphanumeric, null) {
+		RepeatedCharacterValue = value;
+	}
 
-        public AlphanumericValue Value { get; private set; }
+	public AlphanumericValue Value { get; private set; }
+	public RepeatedCharacterValue RepeatedCharacterValue { get; private set; }
 
-        public RepeatedCharacterValue RepeatedCharacterValue { get; private set; }
-    }
+	public override string ToString() {
+		if (RepeatedCharacterValue != null) return RepeatedCharacterValue.Value;
+		//the previous line will always return an exception. should be:
+		//return RepeatedCharacterValue.GetValueInContext(???);
+		if (Value != null) return Value.ToString();
+		return base.ToString();
+	}
+}
 
-    public class SymbolReferenceVariable : VariableBase
-    {
-        public SymbolReferenceVariable(StorageDataType symbolType, StorageArea storageArea) :
-            base(symbolType, storageArea)
-        { }
+public class SymbolReferenceVariable : VariableBase {
+	public SymbolReferenceVariable(StorageDataType symbolType, StorageArea storageArea): base(symbolType, storageArea) { }
+	public SymbolReferenceVariable(StorageDataType symbolType, SymbolReference symbolReference): base(symbolType, null) {
+		SymbolReference = symbolReference;
+	}
 
-        public SymbolReferenceVariable(StorageDataType symbolType, SymbolReference symbolReference) :
-            base(symbolType, null)
-        {
-            SymbolReference = symbolReference;
-        }
-
-        public SymbolReference SymbolReference { get; private set; }
-    }
+	public SymbolReference SymbolReference { get; private set; }
+	public override string ToString() { return SymbolReference.ToString(); }
+}
 
 
 
@@ -139,6 +120,17 @@ public class Variable: VariableBase, Named {
 			if (StorageArea is Named) return ((Named)StorageArea).QualifiedName;
 			return null;
 		}
+	}
+
+	public override string ToString() {
+		if (QualifiedName != null) return QualifiedName.ToString();
+		if (NumericValue != null) return NumericValue.Value.ToString();
+		try {
+			//these should be: return XXXValue.GetValueInContext(???);
+			if (AlphanumericValue != null) return AlphanumericValue.Token.SourceText;
+			if (RepeatedCharacterValue != null) return RepeatedCharacterValue.Token.SourceText;
+		} catch(System.InvalidOperationException) { }
+		return base.ToString();
 	}
 }
 
