12f2906e566448f7361f0436a4c06d8b23492fc3
wiztigers <regis.witz@externe.e-i.com>
2016-09-28 14:49:11 +0200

    close #267: copy debug lines "AS IS" in COBOL85 generated file

--- Codegen/src/TreeToCode.cs
+++ Codegen/src/TreeToCode.cs
@@ -85,6 +85,7 @@
 		/// <returns></returns>
 		private bool ShouldCopy(ICobolTextLine line) {
 			return line.Type == CobolTextLineType.Comment || line.Type == CobolTextLineType.Blank
+			   || line.Type == CobolTextLineType.Debug // #267: Debug lines are copied "AS IS", even if they are invalid in COBOL85!
 			   || (line.Type == CobolTextLineType.Source && line.SourceText.Trim().StartsWith("COPY"));
 		}
 		/// <summary>Write input lines up to the end.</summary>
