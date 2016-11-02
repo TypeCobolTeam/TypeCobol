TypeCobolTeam/TypeCobol

commit edb2779eafbde84faa587bce95c003e04d9eeb78

issue #278: TCRFUN_CODEGEN_ADAPTABLE_BEHAVIOUR: create sentence if PROCEDURE DIVISION does not have any.

Codegen/src/Nodes/ProcedureDivision.cs
@@ -3,6 +3,8 @@
 	using System.Collections.Generic;
 	using TypeCobol.Compiler.CodeElements;
 	using TypeCobol.Compiler.Text;
+	using System.Collections.Generic;
+	using TypeCobol.Compiler.Text;
 
 internal class ProcedureDivision: Compiler.Nodes.ProcedureDivision, Generated {
 
@@ -68,24 +70,51 @@ internal class ProcedureDivision: Compiler.Nodes.ProcedureDivision, Generated {
 	public bool IsLeaf { get { return false; } }
 }
 
+public class GeneratedParameter: CallTargetParameter {
+	public GeneratedParameter(SymbolDefinition symbol) {
+		this.StorageArea = CreateReceivingStorageArea(symbol);
+		var mode = TypeCobol.Compiler.CodeElements.ParameterSharingMode.ByReference;
+		this.SharingMode = new SyntaxProperty<ParameterSharingMode>(mode, null);
+	}
+
+	public static StorageArea CreateReceivingStorageArea(SymbolDefinition symbol) {
+		if (symbol == null) return null;
+		var storage = new DataOrConditionStorageArea(new SymbolReference(symbol));
+		return storage;
+	}
+}
 
 
-	public class GeneratedParameter: CallTargetParameter {
-//		public GeneratedParameter(ReceivingStorageArea storage): base(storage, null) {
-//			var mode = TypeCobol.Compiler.CodeElements.ReceivingMode.ByReference;
-//			this.ReceivingMode = new SyntaxProperty<ReceivingMode>(mode, null);
-//		}
 
-		public GeneratedParameter(SymbolDefinition symbol) {
-			this.StorageArea = CreateReceivingStorageArea(symbol);
-			var mode = TypeCobol.Compiler.CodeElements.ParameterSharingMode.ByReference;
-			this.SharingMode = new SyntaxProperty<ParameterSharingMode>(mode, null);
-		}
+internal class Sentence: Compiler.Nodes.Sentence, Generated {
+	public Sentence(): base() { }
 
-		public static StorageArea CreateReceivingStorageArea(SymbolDefinition symbol) {
-			if (symbol == null) return null;
-			var storage = new DataOrConditionStorageArea(new SymbolReference(symbol));
-			return storage;
+	private IList<ITextLine> _cache = null;
+	public override IEnumerable<ITextLine> Lines {
+		get {
+			if (_cache == null) {
+				_cache = new List<ITextLine>();
+				_cache.Add(new TextLineSnapshot(-1, "DATA DIVISION.", null));
+			}
+			return _cache;
 		}
 	}
+	public bool IsLeaf { get { return false; } }
+}
+internal class SentenceEnd: Compiler.Nodes.End, Generated {
+	public SentenceEnd(): base(null) { }
+
+	private IList<ITextLine> _cache = null;
+	public override IEnumerable<ITextLine> Lines {
+		get {
+			if (_cache == null) {
+				_cache = new List<ITextLine>();
+				_cache.Add(new TextLineSnapshot(-1, "    .", null));
+			}
+			return _cache;
+		}
+	}
+	public bool IsLeaf { get { return true; } }
+}
+
 }

Codegen/src/Nodes/Factory.cs
@@ -11,17 +11,38 @@ class Factory {
 			{ "local-storage", typeof(LocalStorageSection) },
 			{ "linkage", typeof(LinkageSection) },
 			{ "file", typeof(FileSection) },
+			{ "sentence", typeof(Sentence) },
+			{ "end", typeof(SentenceEnd) },
 		};
+	private Dictionary<string,string[]> GeneratedChildren = new Dictionary<string,string[]> {
+			{ "sentence", new string[] { "end", } },
+		};
+
+	private void GeneralizeName(ref string name) {
+		if (name.StartsWith("sentence-")) name = "sentence";
+	}
 	private Type GetGeneratedNode(string name) {
 		Type type;
 		GeneratedNodeTypes.TryGetValue(name, out type);
 		return type;
 	}
+	private string[] GetGeneratedChildren(string name) {
+		string[] names;
+		GeneratedChildren.TryGetValue(name, out names);
+		if (names == null) names = new string[0];
+		return names;
+	}
 
 	internal Compiler.Nodes.Node Create(string name) {
+		GeneralizeName(ref name);
 		var type = GetGeneratedNode(name);
 		if (type == null) return null; // cannot generate
-		return (Compiler.Nodes.Node)Activator.CreateInstance(type);
+		var node = (Compiler.Nodes.Node)Activator.CreateInstance(type);
+		foreach(var cname in GetGeneratedChildren(name)) {
+			var child = Create(cname);
+			if (child != null) node.Add(child);
+		}
+		return node;
 	}
 }
 

Codegen/test/resources/config/TypeCobol/ALL.xml
@@ -99,7 +99,7 @@
       10 LibFctPointer PROCEDURE-POINTER.
       </pattern>
       <pattern action="expand" location="program.end"></pattern>
-      <pattern location="program.procedure-division.begin" action="create" group="TCRFUN_CODEGEN_ADAPTABLE_BEHAVIOUR">
+      <pattern location="program.procedure-division.sentence-0.begin" action="create" group="TCRFUN_CODEGEN_ADAPTABLE_BEHAVIOUR">
     IF CallIsCopy
       PERFORM Copy-Process-Mode
     ELSE

Codegen/test/resources/output/TypeCobol/Library.cbl
@@ -91,6 +91,7 @@
            END-IF                                                             
                                                                               
            GOBACK                                                             
+           .                                                                  
       *=================================================================
       *DECLARE FUNCTION currentDate PUBLIC                                    
       *    RETURNING Result TYPE date.                                        

TypeCobol/Compiler/Nodes/Procedure.cs
@@ -41,6 +41,14 @@ public class Paragraph: Node, CodeElementHolder<ParagraphHeader> {
 
 public class Sentence: Node, CodeElementHolder<CodeElement> {
 	public Sentence(): base(null) { }
+	public override string ID {
+		get {
+			string id = "sentence-";
+			if (Parent == null) id += '0';
+			else id += new System.Collections.Generic.List<Node>(Parent.Children).IndexOf(this);
+			return id;
+		}
+	}
 }
 
 } // end of namespace TypeCobol.Compiler.Nodes

commit 3c47f2cddffd04b46cc3d91d0ac60c09b249c5c4

issue #278: fix "empty sentence codegen generates as DATA DIVISION" bug introduced by edb2779eafbde84faa587bce95c003e04d9eeb78.

Codegen/src/Nodes/ProcedureDivision.cs
@@ -89,14 +89,10 @@ public class GeneratedParameter: CallTargetParameter {
 internal class Sentence: Compiler.Nodes.Sentence, Generated {
 	public Sentence(): base() { }
 
-	private IList<ITextLine> _cache = null;
+	private IList<ITextLine> _nuthin = new List<ITextLine>();
 	public override IEnumerable<ITextLine> Lines {
 		get {
-			if (_cache == null) {
-				_cache = new List<ITextLine>();
-				_cache.Add(new TextLineSnapshot(-1, "DATA DIVISION.", null));
-			}
-			return _cache;
+			return _nuthin;
 		}
 	}
 	public bool IsLeaf { get { return false; } }