18121b9bc18917fbd5925719294adda7a2be2a31
wiztigers <regis.witz@externe.e-i.com>
2016-11-03 12:03:25 +0100

    issue #329: python delivery script

---
+++ build/package.py
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/python
+import os, zipfile, time
+
+files = [
+    'TypeCobol.CLI.exe',
+    'SimpleMsgPack.dll',
+    'Castle.Core.dll',
+    'TypeCobol.Codegen.exe',
+    'RazorEngine.dll',
+    'TypeCobol.exe',
+    'TypeCobol.Grammar.dll',
+    'Antlr4.Runtime.dll',
+
+    'System.Reactive.Core.dll',
+    'System.Reactive.Interfaces.dll',
+    'System.Reactive.Linq.dll',
+    'System.Reactive.PlatformServices.dll',
+    'System.Reactive.Windows.Threading.dll',
+    'System.Web.Razor.dll',
+
+    os.path.join('resources','config','TypeCobol','ALL.xml'),
+    ]
+
+if __name__ == '__main__':
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(current_directory, os.pardir, 'bin', 'Debug')
+    archive_name = 'TypeCobol.CLI-%s.zip' % time.strftime('%Y%m%d-%H%M%S')
+    archive = zipfile.ZipFile(archive_name, 'w')
+    for f in files:
+        archive.write(os.path.join(path, f), os.path.basename(f))
+    archive.close()
+
