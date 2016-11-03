9dc82f41938ca806b0795072ec681ed602c27e9d
smedilol <olivier.smedile@e-i.com>
2016-11-03 18:33:20 +0100

    Issue #325, Oops forgot to ignore GrammarTests again

--- TypeCobol.Test/GrammarTest.cs
+++ TypeCobol.Test/GrammarTest.cs
@@ -13,7 +13,7 @@ namespace TypeCobol.Test {
 		[TestMethod]
 		[TestCategory("Parsing")]
 		[TestProperty("Time","long")]
-		//[Ignore] // Ignored, as everybody does not have a Samples folder. Remove this if you do have one.
+		[Ignore] // Ignored, as everybody does not have a Samples folder. Remove this if you do have one.
 		public void CheckGrammarCorrectness() {
 
 			int STOP_AFTER_AS_MANY_ERRORS = 1000;
