3a00660006422c9883aec14548d041e9167d02e4
wiztigers <regis.witz@externe.e-i.com>
2016-11-17 10:28:38 +0100

    fixes #354: codegen: "erase" action now only erase whole words and ignores case

--- Codegen/test/resources/input/TypeCobol/unsafe.cbl
+++ Codegen/test/resources/input/TypeCobol/unsafe.cbl
@@ -17,6 +17,7 @@
            01 identifier-2 TYPE SmallGroup.
            01 identifier-3 TYPE Small.
            01 identifier-4 PIC 9(04).
+           01 myunsafeid PIC 9(04).
 
        PROCEDURE DIVISION.
        
@@ -30,17 +31,22 @@
       * OK: receiver is strongly-typed, with UNSAFE sender can be of a different TYPE
            MOVE UNSAFE CORRESPONDING identifier-2 TO identifier-1
            MOVE UNSAFE identifier-2 TO identifier-1
-           MOVE UNSAFE identifier-3 TO identifier-1
+           MOVE unsafe identifier-3 TO identifier-1
       * OK: receiver is strongly-typed, with UNSAFE sender can have no TYPE
            MOVE UNSAFE identifier-4 TO identifier-1
            MOVE UNSAFE '1337'       TO identifier-1
       * WARN: receiver is weakly-typed, UNSAFE is useless
            MOVE UNSAFE CORRESPONDING identifier-1 TO identifier-2
-           MOVE UNSAFE identifier-1 TO identifier-2
+           MOVE unsafe identifier-1 TO identifier-2
            MOVE UNSAFE identifier-4 TO identifier-3
            MOVE UNSAFE '1337'       TO identifier-3
            MOVE UNSAFE identifier-3 TO identifier-4
            MOVE UNSAFE '1337'       TO identifier-4
+      * OK: only exact matches to unsafe are deleted in output code
+           MOVE UNSAFE myunsafeid   TO identifier-4
+           MOVE UNSAFE identifier-4 TO myunsafeid
+           MOVE        myunsafeid   TO identifier-4
+           MOVE        myUNSAFEid   TO identifier-4
            .
 
        END PROGRAM Test-UNSAFE.
