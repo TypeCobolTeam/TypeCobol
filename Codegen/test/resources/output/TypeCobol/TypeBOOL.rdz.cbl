b9894aa5a05127eb55f445146fa3a882b9f8ae35
smedilol <olivier.smedile@e-i.com>
2017-01-11 17:05:38 +0100

    Issue #392 + Codegens issues : With a slight modification of what we expect. TypeBool test works again.

--- Codegen/test/resources/output/TypeCobol/TypeBOOL.rdz.cbl
+++ Codegen/test/resources/output/TypeCobol/TypeBOOL.rdz.cbl
@@ -8,24 +8,28 @@
        01  x PIC X.
       * Booleans declaration
       *01  Identifier TYPE BOOL.                                              
-       01  Identifier-value PIC X VALUE LOW-VALUE.                            
+       01  Identifier-value PIC X VALUE LOW-VALUE.
          88  Identifier       VALUE 'T'.                                      
          88  Identifier-false VALUE 'F'.                                      
+                                
       *01  AnotherOne TYPE BOOL.                                              
-       01  AnotherOne-value PIC X VALUE LOW-VALUE.                            
+       01  AnotherOne-value PIC X VALUE LOW-VALUE.
          88  AnotherOne       VALUE 'T'.                                      
          88  AnotherOne-false VALUE 'F'.                                      
+                                
        01  AGroup.
            05  a.
              10  c PIC X.
       *      10  b TYPE BOOL.                                                 
-       10  b-value PIC X VALUE LOW-VALUE.                                     
+             10  b-value PIC X VALUE LOW-VALUE.
          88  b       VALUE 'T'.                                               
          88  b-false VALUE 'F'.                                               
+                             
       *    05  d TYPE BOOL.                                                   
-       05  d-value PIC X VALUE LOW-VALUE.                                     
+           05  d-value PIC X VALUE LOW-VALUE.
          88  d       VALUE 'T'.                                               
          88  d-false VALUE 'F'.                                               
+                           
 
 
 
@@ -34,7 +38,8 @@
        TRAITEMENT.
            SET Identifier  TO TRUE
       *    SET Identifier  TO FALSE                                           
-       SET Identifier-false TO TRUE.                                          
+           SET Identifier-false TO TRUE.
+                                   
       * OK
            MOVE TRUE         TO Identifier
            MOVE FALSE        TO Identifier
