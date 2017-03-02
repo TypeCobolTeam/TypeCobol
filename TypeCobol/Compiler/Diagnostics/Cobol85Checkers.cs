dd5ffa38ff334a6a4fe2c94363c1a9c5e96b03d2
smedilol <olivier.smedile@e-i.com>
2017-03-02 09:36:05 +0100

    NullReferenceException Shield

--- TypeCobol/Compiler/Diagnostics/Cobol85Checkers.cs
+++ TypeCobol/Compiler/Diagnostics/Cobol85Checkers.cs
@@ -507,6 +507,9 @@ namespace TypeCobol.Compiler.Diagnostics {
                     entry = GetDataDescriptionEntry(table, redefines);
 			    }
 			} else throw new NotImplementedException(data.CodeElement.GetType().Name);
+		    if (entry == null) {
+		        return null;
+		    }
 			if (entry.UserDefinedDataType == null) return entry.DataType;//not a custom type
 		}
         ITypedNode typed = symbol as ITypedNode;
@@ -524,6 +527,9 @@ namespace TypeCobol.Compiler.Diagnostics {
         /// <returns></returns>
         private DataDescriptionEntry GetDataDescriptionEntry(SymbolTable table, DataRedefinesEntry dataRedefinesEntry) {
             var node = GetSymbol(table, dataRedefinesEntry.RedefinesDataName);
+            if (node == null) {
+                return null;
+            }
             if (node is DataDescription) {
                 return (DataDescriptionEntry)node.CodeElement;
             }
