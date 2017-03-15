13c65c68dd507e349a7e9deed5f5bf87ea0042f7
COLLARBE <benjamin.collard@externe.e-i.com>
2017-03-15 11:38:06 +0100

    Remove public RootDirectory

--- TypeCobol/Compiler/File/LocalDirectoryLibrary.cs
+++ TypeCobol/Compiler/File/LocalDirectoryLibrary.cs
@@ -12,7 +12,6 @@ namespace TypeCobol.Compiler.File
     /// </summary>
     public class LocalDirectoryLibrary : ICobolLibrary
     {
-        public DirectoryInfo RootDirectory { get { return rootDirectory; } }
 
         // Local directory properties
 
