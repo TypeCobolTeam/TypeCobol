6a313f1a3973f8bf9ad4f9a7ad6f310a0a14d9cd
COLLARBE <benjamin.collard@externe.e-i.com>
2017-05-15 11:29:05 +0200

    Issue #537, add log on Analytics crash.

--- Analytics/AnalyticsWrapper.cs
+++ Analytics/AnalyticsWrapper.cs
@@ -10,6 +10,7 @@ using System.DirectoryServices.AccountManagement;
 using System.Reflection;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
+using NLog;
 
 namespace Analytics
 {
@@ -21,18 +22,29 @@ namespace Analytics
         private static readonly Lazy<AnalyticsWrapper> _LazyAccess = new Lazy<AnalyticsWrapper>(() => new AnalyticsWrapper()); //Singleton pattern
         private static bool _DisableTelemetry = true; //By default telemetry needs to be disable. It will only be enable by the first caller.
         private static TelemetryClient _TelemetryClient;
-        private Configuration _AppConfig; 
+        private Configuration _AppConfig;
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         private AnalyticsWrapper()
         {
-            _AppConfig = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location); //Load custom app.config for this assembly
-            var appKey = _AppConfig.AppSettings.Settings["AppInsightKey"].Value;//Get API Key CLI project config file
+            try
+            {
+                _AppConfig = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location); //Load custom app.config for this assembly
+                var appKey = _AppConfig.AppSettings.Settings["AppInsightKey"].Value;//Get API Key CLI project config file
 
-            // ----- Initiliaze AppInsights Telemetry Client -------//
-            _TelemetryClient = new TelemetryClient(new TelemetryConfiguration(appKey));
-            _TelemetryClient.Context.User.Id = Environment.UserName;
-            _TelemetryClient.Context.Session.Id = Guid.NewGuid().ToString();
-            _TelemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
-            // --------------------------------------- //
+                // ----- Initiliaze AppInsights Telemetry Client -------//
+                _TelemetryClient = new TelemetryClient(new TelemetryConfiguration(appKey));
+                _TelemetryClient.Context.User.Id = Environment.UserName;
+                _TelemetryClient.Context.Session.Id = Guid.NewGuid().ToString();
+                _TelemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+                // --------------------------------------- //
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e);
+            }
+            
         }
 
         /// <summary>
@@ -51,8 +63,15 @@ namespace Analytics
         /// <param name="eventName">Event name to store</param>
         public void TrackEvent(string eventName)
         {
-            if (_DisableTelemetry) return;
-            _TelemetryClient.TrackEvent(eventName);
+            try
+            {
+                if (_DisableTelemetry) return;
+                _TelemetryClient.TrackEvent(eventName);
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e);
+            }
         }
 
         /// <summary>
@@ -61,8 +80,15 @@ namespace Analytics
         /// <param name="exception">Exception raised to store</param>
         public void TrackException(Exception exception)
         {
-            if (_DisableTelemetry) return;
-            _TelemetryClient.TrackException(exception);
+            try
+            {
+                if (_DisableTelemetry) return;
+                _TelemetryClient.TrackException(exception);
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e);
+            }
         }
 
         /// <summary>
@@ -70,75 +96,89 @@ namespace Analytics
         /// </summary>
         public void EndSession()
         {
-            _TelemetryClient.Flush();
+            try
+            {
+                _TelemetryClient.Flush();
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e);
+            }
         }
 
 
         public void SendMail(Exception exception, List<string> sourceFilePaths, List<string> CopyFolders, string config)
         {
-            if (_DisableTelemetry) return;
-
-            var currentUserMail = UserPrincipal.Current.EmailAddress;
-            var mail = new MailMessage();
-            var smtpClient = new SmtpClient();
-            var errorPagePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\Templates\ErrorMailTemplate.html";
-            string errorTemplate = "<h3>Oh.. template not found..</h3>";
-
-            smtpClient.Host = _AppConfig.AppSettings.Settings["SmtpServer"].Value;
-            smtpClient.Port = int.Parse(_AppConfig.AppSettings.Settings["SmtpPort"].Value);
-            smtpClient.UseDefaultCredentials = bool.Parse(_AppConfig.AppSettings.Settings["SmtpUseDefaultCredential"].Value);
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-
-            var receiver = _AppConfig.AppSettings.Settings["MailReceiver"].Value;
-            mail.To.Add(new MailAddress(receiver));
-            mail.From = new MailAddress(currentUserMail);
-            mail.Subject = _AppConfig.AppSettings.Settings["MailSubject"].Value;
-
-            if(File.Exists(errorPagePath))
+            try
             {
-                errorTemplate = File.ReadAllText(errorPagePath);
-                errorTemplate = errorTemplate.Replace("{DateTime}", DateTime.Now.ToString());
+                if (_DisableTelemetry) return;
 
-                errorTemplate = errorTemplate.Replace("{User}", currentUserMail.Split('@')[0].Replace('.', ' '));
-                errorTemplate = errorTemplate.Replace("{TypeCobolVersion}", _AppConfig.AppSettings.Settings["TypeCobolVersion"].Value);
-                errorTemplate = errorTemplate.Replace("{Source}", exception.Source);
+                var currentUserMail = UserPrincipal.Current.EmailAddress;
+                var mail = new MailMessage();
+                var smtpClient = new SmtpClient();
+                var errorPagePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\Templates\ErrorMailTemplate.html";
+                string errorTemplate = "<h3>Oh.. template not found..</h3>";
 
-                errorTemplate = errorTemplate.Replace("{Config}", config);
-                errorTemplate = errorTemplate.Replace("{Message}", exception.Message);
-                errorTemplate = errorTemplate.Replace("{StackTrace}", exception.StackTrace);
+                smtpClient.Host = _AppConfig.AppSettings.Settings["SmtpServer"].Value;
+                smtpClient.Port = int.Parse(_AppConfig.AppSettings.Settings["SmtpPort"].Value);
+                smtpClient.UseDefaultCredentials = bool.Parse(_AppConfig.AppSettings.Settings["SmtpUseDefaultCredential"].Value);
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
 
-                string copiesDump = null;
-     
-                if(CopyFolders != null)
-                    foreach (var folder in CopyFolders)
-                    {
-                        if (Directory.Exists(folder))
-                            foreach (var copy in Directory.EnumerateFiles(folder, "*", SearchOption.AllDirectories))
-                            {
-                                copiesDump += copy.Split('\\').Last() + " ";
-                                if (File.Exists(copy))
-                                    copiesDump += File.GetCreationTime(copy) + "\n";
-                                else
-                                    copiesDump += "Not found. \n";
-                            }
-                    }
-                
-                errorTemplate = errorTemplate.Replace("{CopiesDump}", copiesDump == null ? "No copy found" : copiesDump);
-            }
+                var receiver = _AppConfig.AppSettings.Settings["MailReceiver"].Value;
+                mail.To.Add(new MailAddress(receiver));
+                mail.From = new MailAddress(currentUserMail);
+                mail.Subject = _AppConfig.AppSettings.Settings["MailSubject"].Value;
 
-            mail.IsBodyHtml = true;
-            mail.Body = errorTemplate;
-
-            if(sourceFilePaths != null)
-                foreach (var sourceFilePath in sourceFilePaths)
+                if (File.Exists(errorPagePath))
                 {
-                    if (!string.IsNullOrEmpty(sourceFilePath) && File.Exists(sourceFilePath))
-                        mail.Attachments.Add(new Attachment(sourceFilePath));
+                    errorTemplate = File.ReadAllText(errorPagePath);
+                    errorTemplate = errorTemplate.Replace("{DateTime}", DateTime.Now.ToString());
+
+                    errorTemplate = errorTemplate.Replace("{User}", currentUserMail.Split('@')[0].Replace('.', ' '));
+                    errorTemplate = errorTemplate.Replace("{TypeCobolVersion}", _AppConfig.AppSettings.Settings["TypeCobolVersion"].Value);
+                    errorTemplate = errorTemplate.Replace("{Source}", exception.Source);
+
+                    errorTemplate = errorTemplate.Replace("{Config}", config);
+                    errorTemplate = errorTemplate.Replace("{Message}", exception.Message);
+                    errorTemplate = errorTemplate.Replace("{StackTrace}", exception.StackTrace);
+
+                    string copiesDump = null;
+
+                    if (CopyFolders != null)
+                        foreach (var folder in CopyFolders)
+                        {
+                            if (Directory.Exists(folder))
+                                foreach (var copy in Directory.EnumerateFiles(folder, "*", SearchOption.AllDirectories))
+                                {
+                                    copiesDump += copy.Split('\\').Last() + " ";
+                                    if (File.Exists(copy))
+                                        copiesDump += File.GetCreationTime(copy) + "\n";
+                                    else
+                                        copiesDump += "Not found. \n";
+                                }
+                        }
+
+                    errorTemplate = errorTemplate.Replace("{CopiesDump}", copiesDump == null ? "No copy found" : copiesDump);
                 }
+
+                mail.IsBodyHtml = true;
+                mail.Body = errorTemplate;
+
+                if (sourceFilePaths != null)
+                    foreach (var sourceFilePath in sourceFilePaths)
+                    {
+                        if (!string.IsNullOrEmpty(sourceFilePath) && File.Exists(sourceFilePath))
+                            mail.Attachments.Add(new Attachment(sourceFilePath));
+                    }
 
 #if !DEBUG
             smtpClient.Send(mail);
 #endif
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e);
+            } 
         }
     }
 }
