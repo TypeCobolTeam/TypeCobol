04d89484872901d28170786b7e8e779b4a9d9688
mayanje <jean.mayan@externe.e-i.com>
2017-11-14 09:07:58 +0100

    #706: Taking in account the characters that might be put in column 73-80, even in the reverted column 7 at the tail of the generated file.

--- TypeCobol.Transform/Decoder.cs
+++ TypeCobol.Transform/Decoder.cs
@@ -153,7 +153,11 @@ namespace TypeCobol.Transform
                     if (isInPart4) // If inside of part 4 append
                     {
                         if (line.Length >= 7)
-                            tcLinesCol7.Append(line.Substring(7).PadRight(LineLength - 1));
+                        {   //We must remove any character after column 72
+                            int len = (line.Length > 72 ? 72 : line.Length) - 7;
+                            string transcript = line.Substring(7, len);
+                            tcLinesCol7.Append(transcript.PadRight(LineLength - 1));
+                        }
                     }
                 }
 
