afe98079d2e02a2aa036a9f3e5d9128ef3fcbd40
COLLARBE <benjamin.collard@externe.e-i.com>
2018-02-27 16:52:28 +0100

    #842, fix textchange bug with server

--- TypeCobol.LanguageServer/TypeCobolServer.cs
+++ TypeCobol.LanguageServer/TypeCobolServer.cs
@@ -108,7 +108,9 @@ namespace TypeCobol.LanguageServer
                                                        contentChange.text[0] == '\n';
                     //Allow to know if a new line was added
                     //Split on \r \n to know the number of lines added
-                    lineUpdates = contentChange.text.Replace("\r", "").Split('\n').Where(s => !(string.IsNullOrEmpty(s) && replacementTextStartsWithNewLine)).ToList();
+                    lineUpdates = contentChange.text.Replace("\r", "").Split('\n').ToList();
+                    if(string.IsNullOrEmpty(lineUpdates.FirstOrDefault()) && replacementTextStartsWithNewLine)
+                        lineUpdates.RemoveAt(0);
                 }
 
                 // Document cleared
