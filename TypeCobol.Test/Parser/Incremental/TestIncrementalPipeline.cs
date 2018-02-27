f20cf38f4812e4f389436bfcccb5f1012df73ae3
COLLARBE <benjamin.collard@externe.e-i.com>
2018-02-27 16:42:05 +0100

    #849, create unit tests for UpdateTextLine incremental parser

--- TypeCobol.Test/Parser/Incremental/TestIncrementalPipeline.cs
+++ TypeCobol.Test/Parser/Incremental/TestIncrementalPipeline.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace TypeCobol.Test.Parser.Incremental {
 	public class TestIncrementalPipeline {
 
 		[TestMethod]
-		[TestCategory("Performance")]
+		[TestCategory("Incremental")]
 		[TestProperty("Time","long")]
 		public void CheckPerformance() {
             // Sample program properties
@@ -76,7 +77,7 @@ namespace TypeCobol.Test.Parser.Incremental {
         static readonly string Root = PlatformUtils.GetPathForProjectFile("Parser") + Path.DirectorySeparatorChar + "Programs" + Path.DirectorySeparatorChar + "Cobol85";
 
         [TestMethod]
-        [TestCategory("Performance")]
+        [TestCategory("Incremental")]
         [TestProperty("Time", "long")]
         public void Check_BeforeAfterInsertionBatched()
         {
@@ -99,7 +100,7 @@ namespace TypeCobol.Test.Parser.Incremental {
         }
 
         [TestMethod]
-        [TestCategory("Performance")]
+        [TestCategory("Incremental")]
         [TestProperty("Time", "long")]
         public void Check_BeforeAfterInsertion()
         {
@@ -142,12 +143,278 @@ namespace TypeCobol.Test.Parser.Incremental {
         }
 
 
-        private static TextChangedEvent UpdateLine(TextChangeType type, int line, string text, TextChangedEvent e = null)
+        public static TextChangedEvent UpdateLine(TextChangeType type, int line, string text, TextChangedEvent e = null)
         {
             if (e == null) e = new TextChangedEvent();
             ITextLine snapshot = new TextLineSnapshot(line, text, null);
             e.TextChanges.Add(new TextChange(type, line, snapshot));
             return e;
+        }
+    }
+
+    [TestClass]
+    public class IncrementalTextLineChanges
+    {
+        static readonly string Root = PlatformUtils.GetPathForProjectFile("Parser") + Path.DirectorySeparatorChar +
+                                      "Incremental" + Path.DirectorySeparatorChar + "TextLineChanges";
+
+        private Paths UnitTestPaths = null;
+        private TestUnit UnitTest = null;
+
+        /// <summary>
+        /// This method will initialize the paths and the unit. It will also parse the tcbl program once. 
+        /// </summary>
+        /// <param name="inputFileName">File name of the test</param>
+        private void InitUnitTest(string inputFileName)
+        {
+            UnitTestPaths = new Paths(Root, Root, Root + Path.DirectorySeparatorChar + inputFileName, new Multipass.IndexNames());
+            UnitTest = new TestUnit(new Multipass(UnitTestPaths));
+            UnitTest.Init(new[] { ".tcbl", ".cpy" });
+            //Parser file for the first time
+            UnitTest.Parse();
+        }
+
+        private void CompareTextDocuments()
+        {
+            //Get document's lines as string
+            var stringDocument = GetTextDocument(UnitTest.Compiler.CompilationResultsForProgram.CobolTextLines);
+            //Compare with result file
+            UnitTest.Compare(stringDocument);
+        }
+        private string GetTextDocument(IReadOnlyList<ICobolTextLine> cobolTextLines)
+        {
+            var sb = new StringBuilder();
+            foreach (var cobolTextLine in cobolTextLines)
+            {
+                sb.AppendLine(cobolTextLine.SourceText);
+            }
+            return sb.ToString();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_AddCharBeginLine()
+        {
+            InitUnitTest("AddCharBeginLine.tcbl");
+
+            var updateLine = TestChangeEvent.UpdateLine(TextChangeType.LineUpdated, 3, "       CDATA DIVISION.");
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(updateLine);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_AddCharMiddleLine()
+        {
+            InitUnitTest("AddCharMiddleLine.tcbl");
+
+            var updateLine = TestChangeEvent.UpdateLine(TextChangeType.LineUpdated, 3, "       DATA DIVCISION.");
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(updateLine);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_AddCharEndLine()
+        {
+            InitUnitTest("AddCharEndLine.tcbl");
+
+            var updateLine = TestChangeEvent.UpdateLine(TextChangeType.LineUpdated, 3, "       DATA DIVISION.C");
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(updateLine);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_AddLineJumpMiddleLine()
+        {
+            InitUnitTest("AddLineJumpMiddleLine.tcbl");
+
+            var lineJumpChanges = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+            lineJumpChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "       DATA        ").TextChanges[0]);
+            lineJumpChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 4, "       DIVISION.").TextChanges[0]);
+            
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(lineJumpChanges);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_DeleteCharBeginLine()
+        {
+            InitUnitTest("DeleteCharBeginLine.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "       ATA DIVISION.").TextChanges[0]);
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_DeleteCharMiddleLine()
+        {
+            InitUnitTest("DeleteCharMiddleLine.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "       DATA DIISION.").TextChanges[0]);
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_DeleteCharEndLine()
+        {
+            InitUnitTest("DeleteCharEndLine.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "       DATA DIVISION").TextChanges[0]);
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_SupprEndLineEmptyLine()
+        {
+            InitUnitTest("SupprEndLineEmptyLine.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 1, "");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 1, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 1, "       PROGRAM-ID. ProcCall.").TextChanges[0]);
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_SupprEndLineTextedLine()
+        {
+            InitUnitTest("SupprEndLineTextedLine.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "       DATA DIVISION.       WORKING-STORAGE SECTION.").TextChanges[0]);
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_AddMultipleLines()
+        {
+            InitUnitTest("AddMultipleLines.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 6, "       01 mytest     TYPE BOOL.");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 7, "       01 myzef TYPE DATE.").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 8, "       01 zfzef TYPE BOOL.").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 9, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 10, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 11, "       01 fzef TYPE CountryISO2.").TextChanges[0]);
+            
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Incremental_DeleteMultipleLines()
+        {
+            InitUnitTest("DeleteMultipleLines.tcbl");
+
+            var changes = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "");
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 5, "").TextChanges[0]);
+            changes.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 5, "       01 W-TechCTX TYPE TechnicalContext.").TextChanges[0]);
+
+            //Update text document
+            UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(changes);
+
+            CompareTextDocuments();
+        }
+
+        [TestMethod]
+        [TestCategory("Incremental")]
+        [TestProperty("Time", "fast")]
+        public void Check_TextLineChange()
+        {
+            InitUnitTest("TextLineIncremental.tcbl");
+
+            //Get existing result files
+            var names = UnitTest.Comparator.paths.Resultnames as Multipass.IndexNames;
+            if (names != null)
+            {
+                names.index = 0;
+                //Delete multiple lines 
+                var firstChanges = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+                firstChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "").TextChanges[0]);
+                firstChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "").TextChanges[0]);
+                firstChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "").TextChanges[0]);
+                firstChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "").TextChanges[0]);
+                firstChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "").TextChanges[0]);
+
+                //Update text document
+                UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(firstChanges);
+                //Compare
+                CompareTextDocuments();
+
+                //Simulate a Ctrl+Z
+                var secondChanges = TestChangeEvent.UpdateLine(TextChangeType.LineRemoved, 3, "");
+                secondChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 3, "       DATA DIVISION.").TextChanges[0]);
+                secondChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 4, "       WORKING-STORAGE SECTION.").TextChanges[0]);
+                secondChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 5, "       01 W-TechCTX TYPE TechnicalContext.").TextChanges[0]);
+                secondChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 6, "       01 mytest     TYPE BOOL.").TextChanges[0]);
+                secondChanges.TextChanges.Add(TestChangeEvent.UpdateLine(TextChangeType.LineInserted, 7, "       01 myzef TYPE DATE.").TextChanges[0]);
+
+                //Update text document
+                UnitTest.Compiler.CompilationResultsForProgram.UpdateTextLines(secondChanges);
+                //Compare
+                names.index++;
+                CompareTextDocuments();
+            }
         }
     }
 }
