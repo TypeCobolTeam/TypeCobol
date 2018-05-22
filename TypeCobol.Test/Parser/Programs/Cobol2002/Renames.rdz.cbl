ca82cd3169217cc3b4f52a2e848338c476efbc47
COLLARBE <benjamin.collard@externe.e-i.com>
2018-05-22 16:08:55 +0200

    Issue #950, add renames check on level 01

---
+++ TypeCobol.Test/Parser/Programs/Cobol2002/Renames.rdz.cbl
@@ -0,0 +1,16 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. RenamesPgm.
+
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       01 var1.
+         05 D1 PIC X(10).
+       66 var2 renames D1.
+       66 var3 renames var1.
+
+       PROCEDURE DIVISION.
+           .
+
+       END PROGRAM RenamesPgm.
