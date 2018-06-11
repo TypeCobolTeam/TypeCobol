c49e7e319fe37862de84aad63c064ccde2bd0e66
Cobarzan Cosmin <cosmin.cobarzan@externe.e-i.com>
2018-06-11 11:10:03 +0200

    Issue #979 Add elements to StorageAreaReads and StorageAreaWrites

--- TypeCobol/Compiler/Parser/CodeElementBuilder/CobolCodeElementBuilder.cs
+++ TypeCobol/Compiler/Parser/CodeElementBuilder/CobolCodeElementBuilder.cs
@@ -57,6 +57,17 @@ namespace TypeCobol.Compiler.Parser
                 }
                 if (CobolExpressionsBuilder.storageAreaGroupsCorrespondingImpact != null) {
                     CodeElement.StorageAreaGroupsCorrespondingImpact = CobolExpressionsBuilder.storageAreaGroupsCorrespondingImpact;
+                    if (CodeElement.StorageAreaWrites == null)
+                    {
+                        CodeElement.StorageAreaWrites=new List<ReceivingStorageArea>();
+                    }
+                    if (CodeElement.StorageAreaReads == null)
+                    {
+                        CodeElement.StorageAreaReads=new List<StorageArea>();
+                    }
+
+                    CodeElement.StorageAreaWrites.Add(new ReceivingStorageArea(StorageDataType.Any, CodeElement.StorageAreaGroupsCorrespondingImpact.SendingGroupItem));
+                    CodeElement.StorageAreaReads.Add(CodeElement.StorageAreaGroupsCorrespondingImpact.ReceivingGroupItem);
                 }
                 if (CobolExpressionsBuilder.callTarget != null) {
                     CodeElement.CallTarget = CobolExpressionsBuilder.callTarget;
