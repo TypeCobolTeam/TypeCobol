9397c68d83ff20089d8ea79f07c0af0786be6d4d
mayanje <jean.mayan@externe.e-i.com>
2018-10-09 13:02:45 +0200

    WI #1149 Specify the LL(1) attributed grammar of a skeleton file.

---
+++ TypeCobol.TemplateCore/SaxParser/SkeletonSaxParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCobol.TemplateCore.SaxParser
+{
+    /// <summary>
+    /// This the Sax Parser of a Skeleton file base on the pseudo LL(1) attributed grammar below
+    /// 
+    /// skeletons ::= SKELETONS_TAG skeleton_list END_SKELETONS_TAG
+    /// ;
+    /// skeleton_list ::= skeleton(name, var)
+    /// | skeleton_list skeleton(name, var)
+    /// ;
+    /// skeleton(name, var) ::= SKELETON_TAG conditions patterns END_SKELETON_TAG
+    /// ;
+    /// conditions ::= CONDITIONS_TAG condition_list END_CONDITIONS_TAG
+    /// ;
+    /// condition_list ::= condition(node, name, level, type, sender, receiver,unsafe,function,definitions,variables,typecobol,visibility,copyname,Usage,isPointerIncrementation,receiverUsage)
+    /// | condition_list condition(node, name, level, type, sender, receiver,unsafe,function,definitions,variables,typecobol,visibility,copyname,Usage,isPointerIncrementation,receiverUsage)
+    /// ;
+    /// condition(node, name, level, type, sender, receiver,unsafe,function,definitions,variables,typecobol,visibility,copyname,Usage,isPointerIncrementation,receiverUsage) :: CONDITION_TAG optionalEndConditionTag;	
+    /// ;
+    /// optionalEndConditionTag ::= /* empty */
+    /// | END_CONDITION_TAG
+    /// ;
+    /// patterns ::= PATTERNS_TAG pattern_list END_PATTERNS_TAG
+    /// ;
+    /// pattern_list ::= pattern(name, group, location, action, var, position, deprecated, newline, boolean_property)
+    /// | pattern_list pattern(name, group, location, action, var, position, deprecated, newline, boolean_property)
+    /// ;
+    /// pattern(name, group, location, action, var, position, deprecated, newline, boolean_property) ::= PATTERN_TAG optionalText optionalEndPatternTag
+    /// ;
+    /// optionalText ::= /* empty */
+    /// | Text
+    /// ;
+    /// optionalEndPatternTag ::= /* empty */
+    /// | END_PATTERN_TAG
+    /// ;
+    /// </summary>
+    public class SkeletonSaxParser
+    {
+    }
+}
