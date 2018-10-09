942a1971588192bdfd65bd33ce17fc111a07a452
mayanje <jean.mayan@externe.e-i.com>
2018-10-09 17:17:07 +0200

    WI #1150 Implement the Sax parser of a skeleton file.
    The Skeleton Sax Parser  must be tested using unit tests, and also against schema validation.
    
    WI #1150 Add missing code portion to parse patterns + correcting typos.
    
    WI #1150 Finalize Skeleton file Sax Parser implementation.
    
    This finalization includes unit tests that validate an Xml Skeleton file against its Schema.

---
+++ TypeCobol.TemplateCore/Model/Attribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCobol.TemplateCore.Model
+{
+    public class Attribute
+    {
+        /// <summary>
+        /// The attribute's System type.
+        /// </summary>
+        public System.Type Type
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The attribute's name
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The Attribute's value.
+        /// </summary>
+       public object Value
+        {
+            get;
+            set;
+        }
+    }
+}
