f1b92dc7e4a27d72b4cb7d91c134697370ad6535
mayanje <jean.mayan@externe.e-i.com>
2018-10-25 16:14:47 +0200

    WI #1155 Add missing files.

---
+++ Codegen/src/Actions/IActionsProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeCobol.Compiler.Nodes;
+
+namespace TypeCobol.Codegen.Actions
+{
+    /// <summary>
+    /// The Interface of an Action Provider clas.
+    /// </summary>
+    public interface IActionsProvider
+    {
+        /// <summary>
+        /// Metthod for getting actions associated toa given node.
+        /// </summary>
+        /// <param name="Self">The Given node</param>
+        /// <param name="SelfContext">The Generator Context</param>
+        /// <returns>The list of actions if any, null otherwise</returns>
+        List<TypeCobol.Codegen.Actions.Action> GetActions(TypeCobol.Compiler.Nodes.Node @Self, TypeCobol.Codegen.GeneratorActions @SelfContext);
+    }
+}
