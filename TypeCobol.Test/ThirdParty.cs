aa5c9493640f5a247c42d4d58aa17e3021178de8
smedilol <olivier.smedile@e-i.com>
2018-12-03 11:28:35 +0100

    WI TypeCobolTeam#1194 Add CNAF cobol source file for tests
    As CNAF source file are big, only a few are present.
    Full CNAF list of source avaiable here:
    https://github.com/Wenzel/CNAF

---
+++ TypeCobol.Test/ThirdParty.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TypeCobol.Test {
+
+	[TestClass]
+	public class ThirdParty
+    {
+        [TestMethod]
+		[TestCategory("Parsing")]
+		[TestProperty("Time","long")]
+		public void CNAF() {
+			string samples = @"ThirdParty" + Path.DirectorySeparatorChar + "CNAF" + Path.DirectorySeparatorChar + "Batch";
+			string root = PlatformUtils.GetPathForProjectFile(samples);
+            GrammarTest.CheckTests(root, @"CNAFTest", @"CNAF.txt", ignoreWarningDiag: true, regex: "*");
+
+        }
+
+        [TestMethod]
+        [TestCategory("Parsing")]
+        [TestProperty("Time", "long")]
+        public void NistTest()
+        {
+            string samples = @"ThirdParty" + Path.DirectorySeparatorChar + "Nist";
+            string root = PlatformUtils.GetPathForProjectFile(samples);
+            GrammarTest.CheckTests(root, @"NistTest", @"CheckNistResults.txt", ignoreWarningDiag: true);
+
+        }
+    }
+}
