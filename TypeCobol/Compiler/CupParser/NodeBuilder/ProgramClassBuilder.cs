9fb0f67b924515460bc1a3af6045a50754df2b82
BALLMA <BALLMA@PF11-25Q>
2018-12-21 15:10:14 +0100

    Issue #1209, generate exec functions as Nested

--- TypeCobol/Compiler/CupParser/NodeBuilder/ProgramClassBuilder.cs
+++ TypeCobol/Compiler/CupParser/NodeBuilder/ProgramClassBuilder.cs
@@ -750,6 +750,20 @@ namespace TypeCobol.Compiler.CupParser.NodeBuilder
 
         public virtual void EndExecStatement()
         {
+            //procedures containing EXEC statements should be generated as nested
+            if (_IsInsideProcedure)
+            {
+                Node parent = CurrentNode.Parent;
+                while (parent is Program == false)
+                {
+                    if (parent is FunctionDeclaration)
+                    {
+                        parent.SetFlag(Node.Flag.GenerateAsNested, true);
+                        break;
+                    }
+                    parent = parent.Parent;
+                }
+            }
             Exit();
         }
 
