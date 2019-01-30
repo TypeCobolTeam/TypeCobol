d8ad2df7735553ab8810c44d89ded54b9b1dfbcd
BALLMA <BALLMA@PF11-25Q>
2019-01-30 15:58:34 +0100

    Issue #1245, uncomment test category

--- TypeCobol.LanguageServer.Test/LSRTest.cs
+++ TypeCobol.LanguageServer.Test/LSRTest.cs
@@ -8,126 +8,126 @@ namespace TypeCobol.LanguageServer.Test
     {
         #region Incremental_Tests
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void Incremental_RemoveDataDivisionCtrlZ()
         {
             LSRTestHelper.Test("Incremental_RemoveDataDivisionCtrlZ", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void AddCharBeginLine()
         {
             LSRTestHelper.Test("AddCharBeginLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void AddCharEndLine()
         {
             LSRTestHelper.Test("AddCharEndLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void AddCharMiddleLine()
         {
             LSRTestHelper.Test("AddCharMiddleLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void AddLineJumpMiddleLine()
         {
             LSRTestHelper.Test("AddLineJumpMiddleLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void AddMultipleLines()
         {
             LSRTestHelper.Test("AddMultipleLines", LsrTestingOptions.LsrParsingPhaseTesting);
         }
         
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void AddMultipleLinesWithEmptyLineInside()
         {
             LSRTestHelper.Test("AddMultipleLinesWithEmptyLineInside", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteCharBeginLine()
         {
             LSRTestHelper.Test("DeleteCharBeginLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteCharEndLine()
         {
             LSRTestHelper.Test("DeleteCharEndLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteCharMiddleLine()
         {
             LSRTestHelper.Test("DeleteCharMiddleLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteMultipleLineButStopInMiddleLine()
         {
             LSRTestHelper.Test("DeleteMultipleLineButStopInMiddleLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteMultipleLines()
         {
             LSRTestHelper.Test("DeleteMultipleLines", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteMultipleLinesButStopAtEndLine()
         {
             LSRTestHelper.Test("DeleteMultipleLinesButStopAtEndLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void DeleteMultipleLineWithEmptyLineInside()
         {
             LSRTestHelper.Test("DeleteMultipleLineWithEmptyLineInside", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void SupprEndLineEmptyLine()
         {
             LSRTestHelper.Test("SupprEndLineEmptyLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void SupprEndLineTextedLine()
         {
             LSRTestHelper.Test("SupprEndLineTextedLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void SupprMultipleLinesFromMiddlePreviousLine()
         {
             LSRTestHelper.Test("SupprMultipleLinesFromMiddlePreviousLine", LsrTestingOptions.LsrParsingPhaseTesting);
         }
 
         [TestMethod]
-        //[TestCategory("Incremental")]
+        [TestCategory("Incremental")]
         public void MultiLineCommentIncScan()
         {
             LSRTestHelper.Test("MultiLineCommentIncScan", LsrTestingOptions.NoLsrTesting, false);
@@ -136,7 +136,7 @@ namespace TypeCobol.LanguageServer.Test
 
         #region Sementic_Tests
         [TestMethod]
-        //[TestCategory("Semantic")]
+        [TestCategory("Semantic")]
         public void RemoveDataDivisionCtrlZWithDiag()
         {
             LSRTestHelper.Test("RemoveDataDivisionCtrlZWithDiag", LsrTestingOptions.NoLsrTesting, true);
@@ -145,7 +145,7 @@ namespace TypeCobol.LanguageServer.Test
 
         #region Sementic_Tests
         [TestMethod]
-        //[TestCategory("Semantic")]
+        [TestCategory("Semantic")]
         public void MultiLineBlockDeleteUndo()
         {
             LSRTestHelper.Test("MultiLineBlockDeleteUndo", LsrTestingOptions.NoLsrTesting, false);
@@ -154,98 +154,98 @@ namespace TypeCobol.LanguageServer.Test
 
         #region Completion_Tests
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void TypeCompletion()
         {
             LSRTestHelper.Test("TypeCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void SimpleMoveToCompletion()
         {
             LSRTestHelper.Test("SimpleMoveToCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void VariableQualifiedNameCompletion()
         {
             LSRTestHelper.Test("VariableQualifiedNameCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void VariableQualifiedNameCompletion2()
         {
             LSRTestHelper.Test("VariableQualifiedNameCompletion2", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void ProcedureCompletion()
         {
             LSRTestHelper.Test("ProcedureCompletion", LsrTestingOptions.NoLsrTesting, true, null, null, "CustomDependencies");
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void PerformCompletion()
         {
             LSRTestHelper.Test("PerformCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void SetToAddressOfCompletion()
         {
             LSRTestHelper.Test("SetToAddressOfCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void IfCompletion()
         {
             LSRTestHelper.Test("IfCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void DisplayCompletion()
         {
             LSRTestHelper.Test("DisplayCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void NestedProgramCompletion()
         {
             LSRTestHelper.Test("CompletionWithNestedProgram", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void MoveZeroSpaceCompletion()
         {
             LSRTestHelper.Test("MoveZeroSpaceCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void OfCompletion()
         {
             LSRTestHelper.Test("OfCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void GlobalWithNestedProgramCompletion()
         {
             LSRTestHelper.Test("GlobalWithNestedProgramCompletion", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void QualifiedNameCompletionWithFiller()
         {
             LSRTestHelper.Test("QualifiedNameCompletionWithFiller", LsrTestingOptions.NoLsrTesting, true);
@@ -253,27 +253,27 @@ namespace TypeCobol.LanguageServer.Test
 
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void ProcedureCompletionCaseInsensitive()
         {
             LSRTestHelper.Test("ProcedureCompletionCaseInsensitive", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void ProcedureCompletionInputInoutOutput()
         {
             LSRTestHelper.Test("ProcedureCompletionInputInoutOutput", LsrTestingOptions.NoLsrTesting, true);
         }
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void ProcedureCompletionInputInoutOutput_NoContext()
         {
             LSRTestHelper.Test("ProcedureCompletionInputInoutOutput-NoContext", LsrTestingOptions.NoLsrTesting, true);
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void ProcedureCompletionPotentialChildrenWithNoName()
         {
             LSRTestHelper.Test("ProcedureCompletionPotentialChildrenWithNoName", LsrTestingOptions.NoLsrTesting, true);
@@ -281,14 +281,14 @@ namespace TypeCobol.LanguageServer.Test
 
 #if EUROINFO_RULES
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void CompletionUsingCopy()
         {
             LSRTestHelper.Test("CompletionUsingCopy", LsrTestingOptions.NoLsrTesting, true, "CopyFolder");
         }
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void ReplacingSyntaxOff()
         {
             LSRTestHelper.Test("replacingSyntaxOff", LsrTestingOptions.NoLsrTesting, true, "CopyFolder");
@@ -296,7 +296,7 @@ namespace TypeCobol.LanguageServer.Test
 #endif
 
         [TestMethod]
-        //[TestCategory("Completion")]
+        [TestCategory("Completion")]
         public void GlobalStorageCompletion()
         {
             LSRTestHelper.Test("GlobalStorageCompletion", LsrTestingOptions.NoLsrTesting, true);
