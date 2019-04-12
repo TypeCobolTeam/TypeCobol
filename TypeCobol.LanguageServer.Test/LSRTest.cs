ee98741d62c121d5c7777815618163a8a1dd8f5e
mayanje <jean.mayan@externe.e-i.com>
2019-04-12 12:47:40 +0200

    WI #1350 Add LSR tests

--- TypeCobol.LanguageServer.Test/LSRTest.cs
+++ TypeCobol.LanguageServer.Test/LSRTest.cs
@@ -140,6 +140,40 @@ namespace TypeCobol.LanguageServer.Test
             LSRTestHelper.Test("FormalizedCommentIncScan", LsrTestingOptions.NoLsrTesting, false, true);
         }
 
+        /// <summary>
+        /// This test is releated to issue:https://github.com/TypeCobolTeam/TypeCobol/issues/1350
+        /// The following steps were raising an exception, before the fix.
+        /// 
+        /// Inside TypeCobol Editor: 
+        /// - Go to the beginning of a line for instance in the middle of a document
+        /// - Ctrl+Shift+End
+        /// - Suppr
+        /// - Ctrl+S
+        /// - Ctrl+Z
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Incremental")]
+        public void SelectEndSuppressSaveUndo()
+        {
+            LSRTestHelper.Test("SelectEndSuppressSaveUndo", LsrTestingOptions.NoLsrTesting, false);
+        }
+
+        /// <summary>
+        /// This test is releated to issue:https://github.com/TypeCobolTeam/TypeCobol/issues/1350
+        /// The following steps were raising an exception, before the fix.
+        /// 
+        /// Inside TypeCobol Editor: 
+        /// - Goto the beginning of the first line of a document.
+        /// - Enter
+        /// - Enter
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Incremental")]
+        public void BeginTopLineEnterEnter()
+        {
+            LSRTestHelper.Test("BeginTopLineEnterEnter", LsrTestingOptions.NoLsrTesting, false);
+        }
+        
         #endregion
 
         #region SyntaxColoring
