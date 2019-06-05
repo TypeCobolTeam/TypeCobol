6a80c591b2daa033b0e03affa19ccc0565b42be5
MILLET Florent <florent.millet@e-i.com>
2019-06-05 10:20:17 +0200

    WI #1204 Remove unused server mode code

--- CLI/src/Daemon.cs
+++ CLI/src/Daemon.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.IO.Pipes; // NamedPipeServerStream, PipeDirection
 using Mono.Options;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
-using TypeCobol.Compiler;
-using TypeCobol.Compiler.CodeModel;
 using TypeCobol.Compiler.Diagnostics;
-using TypeCobol.Compiler.Text;
-using SimpleMsgPack;
-using TypeCobol.Server.Serialization;
 using Analytics;
 using Castle.Core.Internal;
-using TypeCobol.CustomExceptions;
-using TypeCobol.Tools;
 using TypeCobol.Tools.Options_Config;
-
 
 namespace TypeCobol.Server {
 
@@ -94,51 +84,10 @@ namespace TypeCobol.Server {
                 if (config.OutputFiles.Count == 0 && config.ExecToStep >= ExecutionStep.Generate)
                     config.ExecToStep = ExecutionStep.CrossCheck; //If there is no given output file, we can't run generation, fallback to CrossCheck
 
-                //"startClient" will be true when "-K" is passed as an argument in command line.
-                if (startClient != StartClient.No && once) {
-                    pipename= "TypeCobol.Server";
-                    using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream(pipename))
-                    {
-                        try {
-                            namedPipeClient.Connect(100);
-		                } catch
-                             {
-                            System.Diagnostics.Process process = new System.Diagnostics.Process();
-                            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-		                    if (startClient == StartClient.NormalWindow) {
-		                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
-		                    } else {
-                                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                            }
-		                    startInfo.FileName = "cmd.exe";
-                            startInfo.Arguments = @"/c " + folder + Path.DirectorySeparatorChar+ "TypeCobol.CLI.exe";
-                            process.StartInfo = startInfo;
-                            process.Start();
-                            
-                            namedPipeClient.Connect(1000);
-		                }
-
-                        namedPipeClient.WriteByte(68);
-
-                        ConfigSerializer configSerializer = new ConfigSerializer();
-                        var configBytes = configSerializer.Serialize(config);
-
-                        namedPipeClient.Write(configBytes, 0, configBytes.Length);
-                        //Wait for the response "job is done"
-                        var returnCode = namedPipeClient.ReadByte(); //Get running server ReturnCode
-                        return exit((ReturnCode)returnCode, "");
-                    }
-				}
-                
-                //option -1
-                else if (once) {
-                    var returnCode = CLI.runOnce(config);
-                    if (returnCode != ReturnCode.Success)
-                        return exit(returnCode, "Operation failled");
-                } else {
-                    runServer(pipename);
-                }
-			}
+                var returnCode = CLI.runOnce(config);
+                if (returnCode != ReturnCode.Success)
+                    return exit(returnCode, "Operation failed");
+            }
             catch (Exception e) {
                 AnalyticsWrapper.Telemetry.TrackException(e, null);
                 return exit(ReturnCode.FatalError, e.Message);
@@ -184,39 +133,6 @@ namespace TypeCobol.Server {
             writer.AddErrors(path, diagnostic);
             Console.WriteLine(diagnostic);
         }
-
-        private static void runServer(string pipename) {
-			var parser = new Parser();
-
-            var pipe = new NamedPipeServerStream(pipename, PipeDirection.InOut, 4, PipeTransmissionMode.Message);
-
-			Commands.Register(66, new Parse(parser, pipe, pipe));
-			Commands.Register(67, new Initialize(parser, pipe, pipe));
-            Commands.Register(68, new RunCommandLine(parser, pipe, pipe));
-			var decoder = new TypeCobol.Server.Serialization.IntegerSerializer();
-			System.Console.WriteLine("NamedPipeServerStream thread created. Wait for a client to connect on " + pipename);
-			while (true) {
-				pipe.WaitForConnection(); // blocking
-                //System.Console.WriteLine("Client connected.");
-			    int code = 0;
-                try {
-					code = decoder.Deserialize(pipe);
-					var command = Commands.Get(code);
-                    command.execute(pipe);
-				}
-				catch (IOException ex) { Console.WriteLine("Error: {0}", ex.Message); }
-                catch (System.Runtime.Serialization.SerializationException ex) { Console.WriteLine("Error: {0}", ex.Message); }
-                finally {
-                    pipe.Disconnect();
-                    //68 is a server that need to stay alive
-                    if(code.Equals(66) || code.Equals(67)) {
-                        pipe.Close();
-                    }
-                }
-			}
-		}
-        
-
 
 		private static readonly string PROGNAME = System.AppDomain.CurrentDomain.FriendlyName;
 		private static readonly string PROGVERSION = GetVersion();
