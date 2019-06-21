e6b4b7decb681b365ae5a4a9e6b13a2857827695
fm-117 <milletfl@e-i.com>
2019-06-21 17:13:06 +0200

    WI #1399 Add Codegen test

---
+++ Codegen/test/TestCobolV6Codegen.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TypeCobol.Codegen
+{
+    [TestClass]
+    public class TestCobolV6Codegen
+    {
+        [TestMethod]
+        [TestCategory("Codegen")]
+        [TestProperty("Time", "fast")]
+        public void TestDynamicAllocationStatements()
+        {
+            CodegenTestUtils.ParseGenerateCompare(Path.Combine("CobolV6", "DynamicAllocationStatements") + ".rdz.cbl");
+        }
+    }
+}
