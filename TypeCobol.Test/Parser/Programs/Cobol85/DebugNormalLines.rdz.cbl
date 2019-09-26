434469e8f0615623bebee738b7525cb6bc7de981
LANOY Dominique (PRESTA EXT) <dominique.lanoy@externe.e-i.com>
2019-09-26 15:29:16 +0200

    WI #1181, add Diagnostic error when a statement is across lines marked with debug and lines not marked debug

---
+++ TypeCobol.Test/Parser/Programs/Cobol85/DebugNormalLines.rdz.cbl
@@ -0,0 +1,33 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Pgm.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       DATA DIVISION.
+       Working-Storage Section.
+       01 xxxx pic XXXX value 'toto'.
+       01 yyyy pic XXXX.
+       PROCEDURE DIVISION.
+           MOVE xxxx TO yyyy
+      d    DISPLAY 'name = '
+                yyyy
+           MOVE '1234' TO yyyy
+           GOBACK.
+       END PROGRAM Pgm.
+      
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Pgm2.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER. IBM-370 WITH DEBUGGING MODE.
+       DATA DIVISION.
+       Working-Storage Section.
+       01 xxxx pic XXXX value 'toto'.
+       01 yyyy pic XXXX.
+       PROCEDURE DIVISION.
+           MOVE xxxx TO yyyy
+      d    DISPLAY 'name = '
+                yyyy
+           MOVE '1234' TO yyyy
+           GOBACK.
+       END PROGRAM Pgm2.
+      
--- TypeCobol/Compiler/Parser/CodeElementsParserStep.cs
+++ TypeCobol/Compiler/Parser/CodeElementsParserStep.cs
@@ -304,6 +304,21 @@ namespace TypeCobol.Compiler.Parser
                                     codeElement.ConsumedTokens = codeElement.ConsumedTokens.OrderBy(t => t.Line)
                                         .ThenBy(t => t.Column).ToList();
                                 }
+
+                                // #1181 : Detect CodeElement with a mix of Debug and "Normal" lines
+                                if (codeElementsLine.Type == CobolTextLineType.Debug && codeElementsLine.ScanState.WithDebuggingMode)
+                                {
+                                    bool isDebug = true;
+                                    for (int lineIndex = codeElement.ConsumedTokens[0].Line; lineIndex <= stopLine && lineIndex < documentLines.Count; lineIndex++)
+                                    {
+                                        isDebug = documentLines[lineIndex].Type == CobolTextLineType.Debug;
+                                        if (!isDebug) break;
+                                    }
+                                    if (!isDebug)
+                                    {
+                                        DiagnosticUtils.AddError(codeElement, "In debugging mode, a statement cannot be across lines marked with debug and lines not marked debug.");
+                                    }
+                                }
                             }
 
 
