0f7d75d4a6851a5c8ed0c76a579ef25e21a36857
fm-117 <51322634+fm-117@users.noreply.github.com>
2019-11-15 09:30:58 +0100

    WI #1385 Avoid NullReferenceException when completing a DEPENDING ON clause

--- TypeCobol.LanguageServer/Completion Factory/CompletionFactory.cs
+++ TypeCobol.LanguageServer/Completion Factory/CompletionFactory.cs
@@ -548,7 +548,7 @@ namespace TypeCobol.LanguageServer
             if (node == null)
                 return completionItems;
 
-            IEnumerable<DataDefinition> potentialVariables = null;
+            var potentialVariables = Enumerable.Empty<DataDefinition>();
             var userFilterText = userFilterToken == null ? string.Empty : userFilterToken.Text;
             Expression<Func<DataDefinition, bool>> variablePredicate =
                 da =>
