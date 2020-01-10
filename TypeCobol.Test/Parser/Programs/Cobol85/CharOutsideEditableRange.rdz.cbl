8f7d5cc92c9dfa654c2a6c3c7184cd7584ae3d97
LANOYDO <dominique.lanoy@externe.e-i.com>
2020-01-10 16:49:00 +0100

    WI #1594 Add new sample of code

--- TypeCobol.Test/Parser/Programs/Cobol85/CharOutsideEditableRange.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/CharOutsideEditableRange.rdz.cbl
@@ -2,57 +2,18 @@
        PROGRAM-ID. Pgm.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 var1 pic x(30) value "hi".
+       01 var1 pic x(100) value "hi".
        PROCEDURE DIVISION.
            display "max-ok_max-ok_max-ok_max-ok_max-ok_max-ok_max-ok_ma"
+
            display "3333333333333333333333333333222222222222222222222222
+
            move var1 to var1
+
            display "endxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx1234
            display "3333333333333333333333333333222222222222222222222222
+
            move "gggggggggggggggggggggggggggggggggggggggggggggssssssssss
                     to var1
-      
-      
-      *    OK
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            " er zer "
-           display 'endxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx1234xxxxxxx
-      -            ' gggg '
-           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            ' vvvv
-      -            ' er zer '
-           move "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -         " er zer " to var1
-      
-      
-      *    KO
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            ' er zer '
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            " er zer '
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            ' er zer "
-      
-           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            " er zer "
-           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            ' er zer "
-           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            " er zer '
-      
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            ' er zer
-      -            " er zer "
-           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -            ' er zer
-      -            " er zer '
-      
-      
-      *    KO
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -              er zer "
-           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      -             " er zer
-      
            GOBACK.
        END PROGRAM Pgm.
