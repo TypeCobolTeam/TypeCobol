00c1753548691501ef2ca1ebb3b7879b27e86f7d
LANOYDO <dominique.lanoy@externe.e-i.com>
2020-01-10 08:06:36 +0100

    WI #1594 Add error message for invalid continuation line

--- TypeCobol.Test/Parser/Programs/Cobol85/CharOutsideEditableRange.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/CharOutsideEditableRange.rdz.cbl
@@ -3,16 +3,56 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 var1 pic x(30) value "hi".
-      
        PROCEDURE DIVISION.
            display "max-ok_max-ok_max-ok_max-ok_max-ok_max-ok_max-ok_ma"
-           display "33333333333333333333333333332222222222222222222222222222
+           display "3333333333333333333333333333222222222222222222222222
            move var1 to var1
            display "endxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx1234
-           display "3333333333333333333333333333222222222222222222222222222
-           move "gggggggggggggggggggggggggggggggggggggggggggggsssssssssssss
-           to var1
-           display "endxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx1234xxxxxxxxxxxx1234
-      -            "gggg"
+           display "3333333333333333333333333333222222222222222222222222
+           move "gggggggggggggggggggggggggggggggggggggggggggggssssssssss
+                    to var1
+      
+      
+      *    OK
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            " er zer "
+           display 'endxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx1234xxxxxxx
+      -            ' gggg '
+           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            ' vvvv
+      -            ' er zer '
+           move "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -         " er zer " to var1
+      
+      
+      *    KO
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            ' er zer '
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            " er zer '
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            ' er zer "
+      
+           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            " er zer "
+           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            ' er zer "
+           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            " er zer '
+      
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            ' er zer
+      -            " er zer "
+           display 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -            ' er zer
+      -            " er zer '
+      
+      
+      *    KO
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -              er zer "
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      -             " er zer
+      
            GOBACK.
        END PROGRAM Pgm.
--- TypeCobol/Compiler/Scanner/Scanner.cs
+++ TypeCobol/Compiler/Scanner/Scanner.cs
@@ -271,8 +271,9 @@ namespace TypeCobol.Compiler.Scanner
 
             // All the following lines are continuation lines
             // => build a character string representing the complete continuation text along the way
-            for (int i = 1; i < continuationLinesGroup.Count; i++)
+            for (int i = 1, lastContinuationLinesIndex = continuationLinesGroup.Count - 1; i <= lastContinuationLinesIndex; i++)
             {
+                bool isLastLine = i == lastContinuationLinesIndex;
                 TokensLine continuationLine = continuationLinesGroup[i];
                 int startIndex = continuationLine.Source.StartIndex;
                 int lastIndex = continuationLine.Source.EndIndex;
@@ -334,6 +335,68 @@ namespace TypeCobol.Compiler.Scanner
                     // Check if the last token so far is an alphanumeric or national literal
                     if (lastTokenOfConcatenatedLineSoFar.TokenFamily == TokenFamily.AlphanumericLiteral)
                     {
+                        if (!lastTokenOfConcatenatedLineSoFar.HasClosingDelimiter)
+                        {
+                            // check delimiters
+                            char startDelimiter = line[startOfContinuationIndex];
+                            bool isBadStartDelimiter = startDelimiter != lastTokenOfConcatenatedLineSoFar.ExpectedClosingDelimiter;
+                            char endDelimiter = '\'';
+                            bool isBadEndDelimiter = false;
+                            if (isLastLine)
+                            {
+                                // check closing delimiter of the last continuation line
+                                string lastLine = continuationLine.SourceText.TrimEnd();
+                                int pos = lastLine.LastIndexOf(startDelimiter);
+                                if (pos > lastLine.IndexOf(startDelimiter))
+                                {
+                                    // delimiter is also present near the end
+                                    lastLine = lastLine.Substring(0, pos + 1);
+                                }
+                                else
+                                {
+                                    // . is present at the end
+                                    pos = lastLine.Length - 1;
+                                    if (lastLine[pos] == '.')
+                                    {
+                                        lastLine = lastLine.Substring(0, pos).TrimEnd();
+                                    }
+                                }
+
+                                endDelimiter = lastLine[lastLine.Length - 1];
+                                isBadEndDelimiter = endDelimiter != lastTokenOfConcatenatedLineSoFar.ExpectedClosingDelimiter;
+                            }
+
+                            if (isBadStartDelimiter || isBadEndDelimiter)
+                            {
+                                if (startDelimiter != '\'' && startDelimiter != '"')
+                                {
+                                    // no valid starting delimiter
+                                    continuationLine.AddDiagnostic(MessageCode.SyntaxErrorInParser,
+                                        startOfContinuationIndex, startOfContinuationIndex + 1, 
+                                        "Starting delimiter of the continuation line is missing.");
+                                    offsetForLiteralContinuation = 0;
+                                }
+                                else if (endDelimiter != '\'' && endDelimiter != '"')
+                                {
+                                    // no valid closing delimiter
+                                    continuationLine.AddDiagnostic(MessageCode.SyntaxErrorInParser,
+                                        startOfContinuationIndex, startOfContinuationIndex + 1,
+                                        "Closing delimiter of the continuation line is missing.");
+                                    offsetForLiteralContinuation = 0;
+
+                                }
+                                else
+                                {
+                                    // different delimiters
+                                    continuationLine.AddDiagnostic(MessageCode.InvalidDelimiterForContinuationLine,
+                                        startOfContinuationIndex, startOfContinuationIndex + 1,
+                                        lastTokenOfConcatenatedLineSoFar.ExpectedClosingDelimiter);
+                                    // Use the first quotation mark to avoid a complete mess while scanning the rest of the line
+                                    offsetForLiteralContinuation = 0;
+                                }
+                            }
+                        }
+
                         //// The continuation of the literal begins with the character immediately following the quotation mark.
                         if (line[startOfContinuationIndex] == lastTokenOfConcatenatedLineSoFar.ExpectedClosingDelimiter)
                         {
