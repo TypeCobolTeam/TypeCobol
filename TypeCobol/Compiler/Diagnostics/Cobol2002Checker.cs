27a7437fa5732cb6d81e6e232b35cc911580e7f8
fm-117 <milletfl@e-i.com>
2020-02-25 10:01:32 +0100

    WI #1679 Remove unused dependency Castle.Core

--- TypeCobol/Compiler/Diagnostics/Cobol2002Checker.cs
+++ TypeCobol/Compiler/Diagnostics/Cobol2002Checker.cs
@@ -1,14 +1,11 @@
 ﻿using System.Linq;
-using System;
 using System.Collections.Generic;
-using Antlr4.Runtime;
 using TypeCobol.Compiler.AntlrUtils;
 using TypeCobol.Compiler.CodeElements;
 using TypeCobol.Compiler.CodeElements.Expressions;
 using TypeCobol.Compiler.Parser;
 using TypeCobol.Compiler.Parser.Generated;
 using TypeCobol.Compiler.Nodes;
-using Castle.Core.Internal;
 using TypeCobol.Compiler.Scanner;
 
 namespace TypeCobol.Compiler.Diagnostics
@@ -131,8 +128,7 @@ namespace TypeCobol.Compiler.Diagnostics
             }
 
             // Add a warning if a parameters field is set inside the formalized comment
-            if (typeDefinition.CodeElement.FormalizedCommentDocumentation != null &&
-                !typeDefinition.CodeElement.FormalizedCommentDocumentation.Parameters.IsNullOrEmpty())
+            if (typeDefinition.CodeElement.FormalizedCommentDocumentation != null && typeDefinition.CodeElement.FormalizedCommentDocumentation.Parameters.Any())
             {
                 var token = typeDefinition.CodeElement.ConsumedTokens
                     .FirstOrDefault(t => t.TokenType == TokenType.FORMALIZED_COMMENTS_PARAMETERS);
