97173b57b2ab9b52dc95ba9913dedc941dd1d380
LANOYDO <55186270+LANOYDO@users.noreply.github.com>
2020-02-26 10:37:36 +0100

    WI #1616  Remove exception on call procedure defined in stacked program (#1650)
    * WI #1616  Remove exception on call procedure defined in stacked program
    
    * WI #1616 Manage programs with same name in SymbolTable and CLI.AddProgramsToRootTable
    
    * WI #1616 No longer fill the symbol table with programs stacked from another source file
    
    * WI #1616 Modify test to verify handling the case of 2 programs with the same name
    
    * WI #1616  Modify in order to a stacked program cannot be call by others programs
    
    * WI #1616 Modify comment about types of filtered out programs

---
+++ CLI/test/ressources/generate_stackedProc/output_expected/Pgm2Same.cbl
@@ -0,0 +1,77 @@
+      *TypeCobol_Version:0.1(alpha)
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PGM2.
+       DATA DIVISION.
+                                                         
+       WORKING-STORAGE SECTION.
+       01  TC-PGM2-FctList-Loaded PIC X(02).
+           88 TC-PGM2-FctList-IsLoaded      VALUE 'OK'.
+
+       01 TC-PGM2-PntTab.
+           05 TC-PGM2-PntNbr         PIC S9(04) COMP VALUE 1.
+      *To call program a4ee502d
+      *Which is generated code for PGM2.Proc1
+      *Declared in source file Pgm2Same.tcbl
+           05 TC-PGM2-a4ee502d-Idt   PIC X(08) VALUE 'a4ee502d'.
+           05 TC-PGM2-a4ee502d PROCEDURE-POINTER.
+
+       
+       LINKAGE SECTION.
+       01 PntTab-Pnt POINTER.
+       01 TC-A1 PIC X.
+
+      * same name PGM2
+       PROCEDURE DIVISION USING PntTab-Pnt.
+                          
+      *
+      *    IF CallIsCopy
+      *      PERFORM Copy-Process-Mode
+      *    ELSE
+           PERFORM FctList-Process-Mode
+           perform INIT-LIBRARY
+      *    END-IF
+
+           GOBACK.
+
+        FctList-Process-Mode.
+            IF NOT TC-PGM2-FctList-IsLoaded
+              SET TC-PGM2-a4ee502d   TO ENTRY 'a4ee502d'
+              SET TC-PGM2-FctList-IsLoaded TO TRUE
+            END-IF
+               .
+
+            set PntTab-Pnt TO ADDRESS OF TC-PGM2-PntTab
+
+           .
+                          
+       INIT-LIBRARY.
+           continue.
+           .
+       PA-ALL-ENTRIES.
+           ENTRY 'a4ee502d' USING TC-A1
+               CALL "a4ee502d" USING TC-A1
+               GOBACK.
+
+
+      *declare procedure Proc1 public
+      *    input t pic x.
+      
+      *
+      *declare procedure Proc1 public
+      *    input t pic x.
+      *_________________________________________________________________
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. a4ee502d IS COMMON.
+       DATA DIVISION.
+       LINKAGE SECTION.
+      *PGM2.Proc1 - Params :
+      *     input(t: pic x)
+       01 t pic x.
+       PROCEDURE DIVISION
+             USING BY REFERENCE t
+           .
+      *PGM2.Proc1 - Params :
+      *     input(t: pic x)
+           .
+       END PROGRAM a4ee502d.
+       END PROGRAM PGM2.
