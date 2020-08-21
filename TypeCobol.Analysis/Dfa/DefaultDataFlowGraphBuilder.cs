9a6f503d69efd861d5956d47647ce18d856710bd
fm-117 <milletfl@e-i.com>
2020-08-21 09:11:31 +0200

    WI #1750 Remove StorageArea visitor, use node dictionaries instead

--- TypeCobol.Analysis/Dfa/DefaultDataFlowGraphBuilder.cs
+++ TypeCobol.Analysis/Dfa/DefaultDataFlowGraphBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TypeCobol.Analysis.Graph;
 using TypeCobol.Compiler.CodeElements;
 using TypeCobol.Compiler.Nodes;
@@ -12,80 +11,28 @@ namespace TypeCobol.Analysis.Dfa
     /// </summary>
     public class DefaultDataFlowGraphBuilder : DataFlowGraphBuilder<Node, Symbol>
     {
-        /// <summary>
-        /// Visitor on StorageArea to collect them.
-        /// </summary>
-        internal class StorageAreaVisitor : TypeCobol.Compiler.CodeElements.AbstractAstVisitor
+        private static HashSet<Symbol> GetSymbols(Dictionary<StorageArea, VariableSymbol> symbolDictionary)
         {
-            /// <summary>
-            /// Kind of Storage Area Requested
-            /// </summary>
-            [Flags]
-            internal enum Kind : byte
+            var result = new HashSet<Symbol>();
+            if (symbolDictionary != null)
             {
-                Read = 1,
-                Write = 0x01 << 1
-            }
-
-            /// <summary>
-            /// All visited symbols.
-            /// </summary>
-            public HashSet<Symbol> Symbols
-            {
-                get;
-                internal set;
-            }
-
-            private readonly Kind _kind;
-            private Node _node;
-            private readonly IDictionary<StorageArea, VariableSymbol> _storageAreaReadsSymbol;
-            private readonly IDictionary<StorageArea, VariableSymbol> _storageAreaWritesSymbol;
-            /// <summary>
-            /// Constructor
-            /// </summary>
-            /// <param name="kind">The Kind of storage area requested</param>
-            /// <param name="node"></param>
-            public StorageAreaVisitor(Kind kind, Node node)
-            {
-                Symbols = new HashSet<Symbol>();
-                _kind = kind;
-                _node = node;
-                _storageAreaReadsSymbol = node.StorageAreaReadsSymbol;
-                _storageAreaWritesSymbol = node.StorageAreaWritesSymbol;
-
-        }
-        /// <summary>
-        /// Visitor on StorageArea
-        /// </summary>
-        /// <param name="storageArea"></param>
-        /// <returns></returns>
-        public override bool Visit(StorageArea storageArea)
-            {
-                SymbolReference symRef = storageArea.SymbolReference;
-                if (_storageAreaReadsSymbol != null  && storageArea.IsReadFrom && (_kind & Kind.Read) != 0)
+                foreach (var p in symbolDictionary)
                 {
-                    if (_storageAreaReadsSymbol.TryGetValue(storageArea, out var symbol))
-                    {
-                        Symbols.Add(symbol);
-                    }
+                    result.Add(p.Value);
                 }
-                if (_storageAreaWritesSymbol != null && storageArea.IsWrittenTo && (_kind & Kind.Write) != 0)
-                {
-                    if (_storageAreaWritesSymbol.TryGetValue(storageArea, out var symbol))
-                    {
-                        Symbols.Add(symbol);
-                    }
-                }
-                return true;
             }
+
+            return result;
         }
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="cfg"></param>
-        public DefaultDataFlowGraphBuilder(ControlFlowGraph<Node, DfaBasicBlockInfo<Symbol>> cfg) : base(cfg)
+        public DefaultDataFlowGraphBuilder(ControlFlowGraph<Node, DfaBasicBlockInfo<Symbol>> cfg)
+            : base(cfg)
         {
+
         }
 
         /// <summary>
@@ -96,9 +43,7 @@ namespace TypeCobol.Analysis.Dfa
         public override HashSet<Symbol> GetUseVariables(Node node)
         {
             System.Diagnostics.Debug.Assert(node != null);
-            StorageAreaVisitor visitor = new StorageAreaVisitor(StorageAreaVisitor.Kind.Read, node);
-            node.CodeElement?.AcceptASTVisitor(visitor);
-            return visitor.Symbols;
+            return GetSymbols(node.StorageAreaReadsSymbol);
         }
 
         /// <summary>
@@ -109,9 +54,7 @@ namespace TypeCobol.Analysis.Dfa
         public override HashSet<Symbol> GetDefVariables(Node node)
         {
             System.Diagnostics.Debug.Assert(node != null);
-            StorageAreaVisitor visitor = new StorageAreaVisitor(StorageAreaVisitor.Kind.Write, node);
-            node.CodeElement?.AcceptASTVisitor(visitor);
-            return visitor.Symbols;
+            return GetSymbols(node.StorageAreaWritesSymbol);
         }
     }
 }
