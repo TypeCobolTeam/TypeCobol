9c54b3aaf763f857ce9d0b87e3e62f3cd544ebac
fm-117 <milletfl@e-i.com>
2020-11-06 17:11:15 +0100

    WI #1671 Add unit test

---
+++ TypeCobol.Test/Parser/Programs/Cobol85/ContinuationLine/ContinuationPictureString.rdz.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DVZZMFT0.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 STR1.
+          05 data1                                              PIC    X
+      -    X.
+       01 STR2.
+          05 data2                                                 PIC X
+      -    X.
+       PROCEDURE DIVISION.
+           GOBACK
+           .
+       END PROGRAM DVZZMFT0.
