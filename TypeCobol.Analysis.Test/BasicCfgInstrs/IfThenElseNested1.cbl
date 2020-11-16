c4ee360fd1f1c6b98049c270ffe7b18f34f8a0e2
fm-117 <milletfl@e-i.com>
2020-11-16 11:12:13 +0100

    WI #1269 Add CFG tests

---
+++ TypeCobol.Analysis.Test/BasicCfgInstrs/IfThenElseNested1.cbl
@@ -0,0 +1,35 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. IFTHEN.
+      
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 A PIC 9(2) VALUE 10.
+       01 B PIC 9(2) VALUE 20.
+       01 C PIC 9(2) VALUE 30.
+       PROCEDURE DIVISION.
+           IF A = 10 THEN
+             DISPLAY "A = 10"
+             DISPLAY "RIGHT ?"
+                IF B = 20 THEN
+                    DISPLAY "B = 20"
+                    DISPLAY "RIGHT ?"
+                ELSE
+                    DISPLAY "B <> 20"
+                    DISPLAY "???"
+                END-IF
+             DISPLAY "AFTER NESTED B"
+           ELSE
+             DISPLAY "A <> 10"
+             DISPLAY "???"
+                IF C = 30 THEN
+                    DISPLAY "C = 30"
+                    DISPLAY "RIGHT ?"
+                ELSE
+                    DISPLAY "C <> 30"
+                    DISPLAY "???"
+                END-IF
+             DISPLAY "AFTER NESTED C"
+           END-IF.
+           DISPLAY "AFTER A".
+       END PROGRAM IFTHEN.
+      
---
+++ TypeCobol.Analysis.Test/BasicCfgInstrs/PerformUntil0.cbl
@@ -0,0 +1,15 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PERFORM0.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 action PIC 9 VALUE 0.
+          88 posibility VALUE 1, 2.
+       PROCEDURE DIVISION.
+           PERFORM UNTIL posibility
+           DISPLAY "action = " NO ADVANCING
+             ACCEPT action
+           END-PERFORM
+           DISPLAY "Right action !".
+           GOBACK.
+       END PROGRAM PERFORM0.
