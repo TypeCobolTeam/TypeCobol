1907cfb7601a44648486ef4301f47efb175ee6f8
fm-117 <milletfl@e-i.com>
2020-12-14 10:12:37 +0100

    WI #1192 #1724 Attach RENAMES to nearest level-01

---
+++ TypeCobol.Test/Parser/Programs/Cobol85/DataRenames.rdz.cbl
@@ -0,0 +1,16 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PGM1.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 TEST-GROUP1.
+          05 TEST-SGROUP1.
+             10 TEST-ELT1 PIC X(04).
+             10 TEST-ELT2 PIC X(350).
+          05 TEST-SGROUP2.
+             10 TEST-ELT3 PIC X(200).
+             10 TEST-ELT4 PIC X(1000).
+          05 TEST-SGROUP5.
+             10 TEST-ELT5 PIC X(100).
+          66 TEST-RENAMES RENAMES TEST-ELT2
+                          THRU    TEST-ELT4.
+       END PROGRAM PGM1.
