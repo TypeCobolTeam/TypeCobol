32a20e006db76f6c730184d564bf7321fc921a30
fm-117 <milletfl@e-i.com>
2020-12-22 14:04:16 +0100

    WI #1825 Add basic unit test for dynamic load of custom analyzers

---
+++ TypeCobol.Analysis.Test/CustomAnalyzers/TestAnalyzerProvider.cs
@@ -0,0 +1,21 @@
+﻿using TypeCobol.Compiler.Directives;
+using TypeCobol.Compiler.Text;
+
+namespace TypeCobol.Analysis.Test.CustomAnalyzers
+{
+    /// <summary>
+    /// This provider is loaded and called in CLI.Test, see TestCustomAnalyzers unit test.
+    /// </summary>
+    public class TestAnalyzerProvider : IAnalyzerProvider
+    {
+        public ISyntaxDrivenAnalyzer[] CreateSyntaxDrivenAnalyzers(TypeCobolOptions options, TextSourceInfo textSourceInfo)
+        {
+            return new ISyntaxDrivenAnalyzer[] { new DummySyntaxDrivenAnalyzer(nameof(DummySyntaxDrivenAnalyzer)) };
+        }
+
+        public IASTAnalyzer[] CreateASTAnalyzers(TypeCobolOptions options)
+        {
+            return new IASTAnalyzer[] { new DummyASTAnalyzer(nameof(DummyASTAnalyzer)) };
+        }
+    }
+}
