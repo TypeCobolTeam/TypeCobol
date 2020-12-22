aad21509a1be318362a785781affcd82f74fd35e
fm-117 <milletfl@e-i.com>
2020-12-22 16:24:11 +0100

    WI #1724 Add RENAMES for a whole group in test

--- TypeCobol.Test/Parser/Programs/Cobol85/DataRenames.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/DataRenames.rdz.cbl
@@ -13,4 +13,12 @@
              10 TEST-ELT5 PIC X(100).
           66 TEST-RENAMES RENAMES TEST-ELT2
                           THRU    TEST-ELT4.
+       01 TEST-GROUP2.
+          05 TEST-SGROUP6.
+             10 ELT1 PIC X.
+             10 ELT2 PIC X.
+          05 TEST-SGROUP7.
+             10 ELT3 PIC X.
+             10 ELT4 PIC X.
+          66 TEST-RENAMES-GROUP RENAMES TEST-SGROUP6.
        END PROGRAM PGM1.
