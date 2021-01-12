94fe5f9c7cf68865e25da23b764083e22d11655d
fm-117 <51322634+fm-117@users.noreply.github.com>
2021-01-12 08:44:46 +0100

    WI #1748 Check nature of separator after *CBL/*CONTROL keyword (#1840)

---
+++ TypeCobol.Test/Parser/Programs/Cobol85/CommentVsDirectives.rdz.cbl
@@ -0,0 +1,10 @@
+﻿      *Should be treated as comment
+      *CBL/OPTIMIZE(FULL),TEST(NONE,SYM),SSRANGE,DYNAM,FLAG(I,E)
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TestingPgm.
+      *Correct *CBL directive
+      *CBL NOSOURCE
+       PROCEDURE DIVISION.
+           GOBACK
+           .
+       END PROGRAM TestingPgm.
