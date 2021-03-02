aa0c220dccaacf00b2d9feb4cadce33a0801c706
DELEVOYE Valentin (PRESTA EXT) <valentin.delevoye@externe.e-i.com>
2021-03-02 17:24:50 +0100

    WI #1878 Split tests and ensure diagnostic is created

--- TypeCobol.Test/Misc/CodeElementDebugTest.cs
+++ TypeCobol.Test/Misc/CodeElementDebugTest.cs
@@ -17,7 +17,7 @@ namespace TypeCobol.Test.Misc
         [TestMethod]
         [TestCategory("Parsing")]
         [TestProperty("Time", "fast")]
-        public void CheckCodeElementDebugProperty()
+        public void CheckCodeElementDebugPropertyWithDebugging()
         {
             var expectedDebugMode = new List<CodeElement.DebugType>()
             {
@@ -44,32 +44,55 @@ namespace TypeCobol.Test.Misc
                 CodeElement.DebugType.Mix,  //20
                 CodeElement.DebugType.Mix,
                 CodeElement.DebugType.All,
-                CodeElement.DebugType.None, 
-                CodeElement.DebugType.None, 
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.Mix,
                 CodeElement.DebugType.None, //25
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None, //30
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.None,
-                CodeElement.DebugType.Mix,  //35
-                CodeElement.DebugType.All,
                 CodeElement.DebugType.None,
                 CodeElement.DebugType.Mix,
                 CodeElement.DebugType.None,
-                CodeElement.DebugType.None, //40
-                CodeElement.DebugType.None, 
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None  //30
             };
 
+            CompareDebugTypes(expectedDebugMode, "DebugLinesWithDebugging");
+        }
+
+        /// <summary>
+        /// Issue #1878 : check the debug mode of CodeElement
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Parsing")]
+        [TestProperty("Time", "fast")]
+        public void CheckCodeElementDebugPropertyNotDebugging()
+        {
+            var expectedDebugMode = new List<CodeElement.DebugType>()
+            {
+                CodeElement.DebugType.None, // CodeElement 0
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None, //5
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.Mix,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None, //10
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None,
+                CodeElement.DebugType.None
+            };
+
+            CompareDebugTypes(expectedDebugMode, "DebugLinesNotDebugging");
+        }
+
+        private static void CompareDebugTypes(IReadOnlyList<CodeElement.DebugType> expectedDebugMode, string fileName)
+        {
             var folder = Path.Combine("Parser", "Programs", "Cobol85");
-            var compilationUnit = ParserUtils.ParseCobolFile("DebugNormalLines", DocumentFormat.RDZReferenceFormat, folder);
+            var compilationUnit = ParserUtils.ParseCobolFile(fileName, DocumentFormat.RDZReferenceFormat, folder);
 
             var codeElements = compilationUnit.CodeElementsDocumentSnapshot.CodeElements.ToList();
-
             for (var i = 0; i < codeElements.Count; i++)
             {
                 Assert.AreEqual(expectedDebugMode[i], codeElements[i].DebugMode, $"CodeElement number: {i}");
--- TypeCobol.Test/Parser/Programs/Cobol85/DebugLinesWithDebugging.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/DebugLinesWithDebugging.rdz.cbl
@@ -47,27 +47,15 @@
 
       *Whole debug line
       d    DISPLAY 'name = ' yyyy
-      
-           GOBACK.
-       END PROGRAM Pgm.
-      
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. Pgm2.
-       ENVIRONMENT DIVISION.
-       CONFIGURATION SECTION.
-       DATA DIVISION.
-       Working-Storage Section.
-       01 xxxx pic XXXX value 'toto'.
-       01 yyyy pic XXXX.
-       PROCEDURE DIVISION.
-           MOVE xxxx TO yyyy
-      d    DISPLAY 'name = '
-                yyyy
-      d    DISPLAY 'name = ' yyyy
+
+      *Normal line then debug
            MOVE '1234' TO yyyy
       d    DISPLAY 
            'name = ' yyyy
+
+      *Normal line ended with dot followed by mix debug
+           MOVE xxxx TO yyyy.
+      d    DISPLAY 'name = '
+                yyyy
            GOBACK.
-       END PROGRAM Pgm2.
-      
-      
+       END PROGRAM Pgm.
