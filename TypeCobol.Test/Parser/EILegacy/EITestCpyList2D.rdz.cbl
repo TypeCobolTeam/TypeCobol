2a78574d6c26ae7fd293fcfe43448bc1a0d36d43
mayanje <jeanchrysostome.mayan@e-i.com>
2021-03-04 12:57:12 +0100

    WI #1732 Rename copy name in test

--- TypeCobol.Test/Parser/EILegacy/EITestCpyList2D.rdz.cbl
+++ TypeCobol.Test/Parser/EILegacy/EITestCpyList2D.rdz.cbl
@@ -2,10 +2,10 @@
        PROGRAM-ID. Pgm.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-      *Ok for declaration but suffixing YMYCOPY not working with placeholder
-       01 MYCOPY1. COPY YMYCOPY1 REPLACING ==:MYCOPY:== BY ==MYCOPY==.
+      *Ok for declaration but suffixing YMECOPY not working with placeholder
+       01 MECOPY1. COPY YMECOPY1 REPLACING ==:MECOPY:== BY ==MECOPY==.
        PROCEDURE DIVISION.
       *   Ko special CPY suffixing mechanism doesn't apply with placeholder
-           Move 'A'             to MYCOPY1-Data
+           Move 'A'             to MECOPY1-Data
            .
        END PROGRAM Pgm.
