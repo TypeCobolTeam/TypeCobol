2a15f577a82a2789de2bf0f6c41fd9d91bbfdf9a
fm-117 <milletfl@e-i.com>
2021-04-09 12:08:36 +0200

    WI #1892 Integrate direct copy parsing with LSP server

--- TypeCobol.LanguageServer/Workspace.cs
+++ TypeCobol.LanguageServer/Workspace.cs
@@ -210,11 +210,12 @@ namespace TypeCobol.LanguageServer
                 arrangedCustomSymbol.CopyAllPrograms(new List<List<Program>>() {prog.ToList()});
                 arrangedCustomSymbol.Programs.Remove(matchingPgm);
             }
+
             fileCompiler = new FileCompiler(initialTextDocumentLines, CompilationProject.SourceFileProvider,
                 CompilationProject, CompilationProject.CompilationOptions, arrangedCustomSymbol ?? _customSymbols,
-                false, CompilationProject);
+                docContext.IsCopy, CompilationProject);
 #else
-            fileCompiler = new FileCompiler(initialTextDocumentLines, CompilationProject.SourceFileProvider, CompilationProject, CompilationProject.CompilationOptions, _customSymbols, false, CompilationProject);
+            fileCompiler = new FileCompiler(initialTextDocumentLines, CompilationProject.SourceFileProvider, CompilationProject, CompilationProject.CompilationOptions, _customSymbols, docContext.IsCopy, CompilationProject);
 #endif
             //Set Any Language Server Connection Options.
             docContext.FileCompiler = fileCompiler;
