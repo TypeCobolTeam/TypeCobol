4e901f8f39dbbff8cd32706ca02ffd3c02a07c6b
fm-117 <51322634+fm-117@users.noreply.github.com>
2022-01-04 17:11:01 +0100

    WI #2039 Remove PERFORM recursivity check from CFG (#2100)

--- TypeCobol.Analysis/Resource.Designer.cs
+++ TypeCobol.Analysis/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace TypeCobol.Analysis {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resource {
@@ -84,15 +84,6 @@ namespace TypeCobol.Analysis {
         internal static string BasicBlockGroupGoesBeyondTheLimit {
             get {
                 return ResourceManager.GetString("BasicBlockGroupGoesBeyondTheLimit", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to A recursive loop has been encountered while analyzing &apos;PERFORM {0}&apos;, recursive instruction is &apos;{1}&apos;..
-        /// </summary>
-        internal static string RecursiveBlockOnPerformProcedure {
-            get {
-                return ResourceManager.GetString("RecursiveBlockOnPerformProcedure", resourceCulture);
             }
         }
     }
