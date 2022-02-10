db6674fbd3f2647fd2bf9e5f013a37e7fc8b1681
Mayan Jean <jean.mayan@externe.e-i.com>
2022-02-10 09:30:08 +0100

    WI #1874 Add diagnostic for invalid CONFIGURATION SECTION in nested programs (#2131)
    Co-authored-by: LAABIDHE <hend.laabidi@e-i.com>

--- TypeCobol/Compiler/Diagnostics/CrossChecker.cs
+++ TypeCobol/Compiler/Diagnostics/CrossChecker.cs
@@ -532,8 +532,20 @@ namespace TypeCobol.Compiler.Diagnostics
             CheckEndProgram(program);
             
             FormalizedCommentsChecker.CheckProgramComments(program);
+          
             return true;
         }
+
+        public override bool Visit(ConfigurationSection configurationSection)
+        {
+            Program program = configurationSection.GetProgramNode();
+            if (program != null && program.IsNested)
+            {
+                DiagnosticUtils.AddError(configurationSection, "A Nested Program cannot have a CONFIGURATION SECTION.");
+            }
+            return true;
+        }
+
 
         public override bool VisitVariableWriter(VariableWriter variableWriter)
         {
