8b8fc90849e475e5ea14306bc70ca053c34cb594
LAABIDHE <hend.laabidi@e-i.com>
2022-02-18 10:26:48 +0100

    WI #2088 Update serial search unit test

--- TypeCobol.Test/Parser/Programs/Cobol85/SerialSearch.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/SerialSearch.rdz.cbl
@@ -1,82 +1,33 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. SearchAll.
+       PROGRAM-ID. SerialSearch.
        data division.
        working-storage section.
        01 tab.
           05 var1 occurs 10 ascending key is Var2 indexed by idx .
             10 var2 pic X(1) .
       
-       PROCEDURE DIVISION.    
+       PROCEDURE DIVISION.
       
-      *-------- ONLY WHEN
-      *OK, when with instruction
-           search all var1
-              when var2(idx) = 'b'
-                display "test"
-           end-search
-      
-      *OK, when without instruction
-           search all var1
-              when var2(idx) = 'b'
-      
-           end-search
-      
-      *-------- AT END and WHEN
-      *OK
-           search all var1
-              at end
-                display "end"
-              when var2(idx) = 'b'
-                display "when"
-           end-search
-      
-      *OK
-           search all var1
-              at end
-                display "end"
-              when var2(idx) = 'b'
-           end-search
-      
-      
-      *-------- AT END at wrong position
-           search all var1
-              when var2(idx) = 'b'
-                display "when"
-      *KO at end must be before when statement
-              at end
-                display "end"
-           end-search
-      
-           search all var1
-              when var2(idx) = 'b'
-      *KO at end must be before when statement
-              at end
-                display "end"
-           end-search
-
-      *-------- 2 WHEN always KO
-           search all var1
+           search var1
               when var2(idx) = 'a'
-                display "when a"
+                 display "search"
               when var2(idx) = 'b'
-                display "when b"
+                 display "search"
            end-search
       
-           search all var1
-              at end
-                display "end"
-              when var2(idx) = 'a'
-                display "when a"
-              when var2(idx) = 'b'
-                display "when b"
-           end-search
       
-           search all var1
-              at end
-                display "end"
+           search var1
+      *KO when without instruction
               when var2(idx) = 'a'
               when var2(idx) = 'b'
-                display "when a and b"
+                 display "search"
            end-search
+      
+      
+           search var1
+      *KO when without instruction
+              when var2(idx) = 'b'
+           end-search
+      
            goback.
-       END PROGRAM SearchAll.
+       END PROGRAM SerialSearch.
