2a124d702868368b7e2adb2595dcf8e939c6aa26
fm-117 <51322634+fm-117@users.noreply.github.com>
2022-03-10 13:15:37 +0100

    WI #2124 #2126 #2127 Add unit test for unsupported REMARKS directive formats (#2149)

---
+++ TypeCobol.Test/Parser/EILegacy/UnsupportedRemarks/RemarksMixedWithAuthor.rdz.cbl
@@ -0,0 +1,9 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DVZF0OSM.
+       AUTHOR. Me.
+      *REMARKS.  COPY=YMSG113
+             .
+       procedure division.
+           goback
+           .
+       End Program DVZF0OSM.
