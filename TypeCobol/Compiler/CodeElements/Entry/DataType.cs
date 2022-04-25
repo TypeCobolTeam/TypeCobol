8f55b55a48119b14ea4a7187454cbdd1a49e30b9
Olivier SMEDILE <olivier.smedile@e-i.com>
2022-04-25 19:24:00 +0200

    WI #2221 Optimize DataType creation

--- TypeCobol/Compiler/CodeElements/Entry/DataType.cs
+++ TypeCobol/Compiler/CodeElements/Entry/DataType.cs
@@ -70,9 +70,10 @@ namespace TypeCobol.Compiler.CodeElements
             return new DataType(name, restrictionLevel, cobolLanguageLevel);
         }
 
-        public static DataType Create(string picture) {
-            var basic = new char[]{'.','Z','+','-','*','D'/*,'B'*/,'C'/*,'S'*/};
-            return doCreate(picture, basic);
+        public static DataType Create(string picture)
+        {
+            Init();
+            return DoCreate2(picture, EditingBaseAndBasic);
         }
 
         /// <summary>
@@ -89,73 +90,103 @@ namespace TypeCobol.Compiler.CodeElements
             }
             return DataType.Unknown;
         }
-        public static DataType Create(string picture, char[] currencies) {
-            var basic = new char[]{'.','Z','+','-','*','D'/*,'B'*/,'C'/*,'S'*/};
-            var all = new char[basic.Length + currencies.Length];
-            basic.CopyTo(all, 0);
-            currencies.CopyTo(all, basic.Length);
-            return doCreate(picture, all);
+        public static DataType Create(string picture, char[] currencies)
+        {
+            var all = new char[Basic.Length + currencies.Length];
+            Basic.CopyTo(all, 0);
+            currencies.CopyTo(all, Basic.Length);
+            return DoCreate(picture, all);
         }
-        private static DataType doCreate(string picture, char[] numericEditingSpecificChars) {
-            //if (picture.Length > 50) return DataType.Unknown;// it's not our job to detect this
-            char[] editingBase = new char[]{'B','0','/'};
-            char[] editingNumeric = new char[editingBase.Length + numericEditingSpecificChars.Length];
-            editingBase.CopyTo(editingNumeric, 0);
-            numericEditingSpecificChars.CopyTo(editingNumeric, editingBase.Length);
 
-            picture = remove(picture, '(',')');
-            char[] chars = distinct(picture.ToUpper());
-            if (contains(chars, 'E'))
-                return DataType.FloatingPoint;// ±?E±99
-            if (contains(chars, new char[]{'X'}))
-                if (contains(chars, editingBase))
-                    return DataType.AlphanumericEdited;
-                else return DataType.Alphanumeric;
-            if (contains(chars, new char[]{'A'}))
-                return DataType.Alphabetic;
-            if (contains(chars, new char[]{'G','N'}))
-                return DataType.DBCS;
-            if (contains(chars, editingNumeric))
-                return DataType.NumericEdited;
-            if (contains(chars, new char[]{'9','S','V','P'}))
-                return DataType.Numeric;
+        private static DataType DoCreate(string picture, char[] numericEditingSpecificChars)
+        {
+            char[] editingNumeric = new char[EditingBase.Length + numericEditingSpecificChars.Length];
+            EditingBase.CopyTo(editingNumeric, 0);
+            numericEditingSpecificChars.CopyTo(editingNumeric, EditingBase.Length);
+            return DoCreate2(picture, editingNumeric);
+        }
+
+
+        public static void Init()
+        {
+            if (EditingBaseAndBasic == null)
+            {
+                EditingBaseAndBasic = new char[EditingBase.Length + Basic.Length];
+                EditingBase.CopyTo(EditingBaseAndBasic, 0);
+                Basic.CopyTo(EditingBaseAndBasic, EditingBase.Length);
+            }
+        }
+
+        private static DataType DoCreate2(string picture, char[] editingNumeric)
+        {
+            bool insideParenthesis = false;
+            for (var i = 0; i < picture.Length; i++)
+            {
+                if (picture[i] == '(')
+                {
+                    insideParenthesis = true;
+                }
+                else if (picture[i] == ')')
+                {
+                    insideParenthesis = false;
+                }
+
+                if (insideParenthesis)
+                {
+                    continue;
+                }
+                if (Array.IndexOf(EChars, picture[i]) > -1)
+                {
+                    return DataType.FloatingPoint;// ±?E±99 
+                }
+                else if (Array.IndexOf(XChars, picture[i]) > -1)
+                {
+                    if (Array.IndexOf(EditingBase, picture[i]) > -1)
+                    {
+                        return DataType.AlphanumericEdited;
+                    }
+                    else
+                    {
+                        return DataType.Alphanumeric;
+                    }
+                }
+                else if (Array.IndexOf(AChars, picture[i]) > -1)
+                {
+                    return DataType.Alphabetic;
+                }
+                else if (Array.IndexOf(DbcsChars, picture[i]) > -1)
+                {
+                    return DataType.DBCS;
+                }
+                else if (Array.IndexOf(editingNumeric, picture[i]) > -1)
+                {
+                    return DataType.NumericEdited;
+                }
+                else if (Array.IndexOf(NumericChars, picture[i]) > -1)
+                {
+                    return DataType.Numeric;
+                }
+            }
             return DataType.Unknown;
         }
-        /// <summary>Remove from string expr all substrings between characters begin and end.</summary>
-        /// <param name="expr">string to be analyzed</param>
-        /// <param name="begin">first character of the the substring to be removed</param>
-        /// <param name="end">last character of the substrings to be removed</param>
-        /// <returns>Resulting string</returns>
-        private static string remove(string expr, char begin, char end) {
-            string regex = string.Format("\\{0}.*?\\{1}", begin, end);
-            return new System.Text.RegularExpressions.Regex(regex).Replace(expr, "");
-        }
-        // we don't want a dependency to Linq just for these
-        private static bool contains(char[] array, char c) {
-            return Array.IndexOf(array, c) > -1;
-        }
-        private static bool contains(char[] array, char[] chars) {
-            foreach(char c in chars)
-                if (contains(array, c)) return true;
-            return false;
-        }
-        /// <summary>
-        /// Return all distinct characters composing the input string.
-        /// Even if present more than once in the input string, each character is only present once in the result array.
-        /// A character not present in the input string won't be present in the result array.
-        /// </summary>
-        /// <param name="input">String to be analyzed</param>
-        /// <returns>Distinct characters composing input.</returns>
-        private static char[] distinct(string input) {
-            System.Collections.Generic.HashSet<char> set = new System.Collections.Generic.HashSet<char>(input);
-            char[] result = new char[set.Count];
-            set.CopyTo(result);
-            return result;
-        }
+
+
+        //Characters for alphanumeric
+        public static char[] XChars = { 'X', 'x' };
+        //Characters in FloatingPoint
+        public static char[] EChars = { 'E', 'e' };
+        //Characters for alphabetic
+        public static char[] AChars = { 'A', 'a' };
+        public static char[] DbcsChars = { 'G', 'g', 'N', 'n' };
+        //Characters for Numeric
+        public static char[] NumericChars = { '9', 'S', 'V', 'P', 's', 'v', 'p' };
+        public static char[] EditingBase = { 'B', 'b', '0', '/' };
+        public static char[] Basic = { '.', 'Z', 'z', '+', '-', '*', 'D', 'd'/*,'B'*/, 'C', 'c'/*,'S'*/};
+        public static char[] EditingBaseAndBasic;
 
 
 
-        public static readonly DataType Unknown            = new DataType("?");
+         public static readonly DataType Unknown            = new DataType("?");
         public static readonly DataType Omitted            = new DataType("Omitted");
         public static readonly DataType Alphabetic         = new DataType("Alphabetic");
         public static readonly DataType Numeric            = new DataType("Numeric");
