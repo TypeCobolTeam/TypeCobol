1968c536613d6351f4611cc5dfa02de14020e00b
fm-117 <milletfl@e-i.com>
2022-05-09 10:14:42 +0200

    WI #2183 Add Demo test for SQL Scanner [TO MODIFY]

---
+++ TypeCobol.Test/Parser/Scanner/TestSqlScanner.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace TypeCobol.Test.Parser.Scanner
+{
+    static class TestSqlScanner
+    {
+        private static void CheckWithResultFile(string actual, string testName) =>
+            ScannerUtils.CheckWithResultFile(actual, Path.Combine("SQL", testName));
+
+        public static void Demo_CheckSql()
+        {
+            string testName = "Demo_CheckSql";
+            string[] lines = { "COMMIT" };
+            var actual = ScannerUtils.ScanSqlLines(lines);
+            CheckWithResultFile(actual, testName);
+        }
+    }
+}
--- TypeCobol.Test/TestCollection.cs
+++ TypeCobol.Test/TestCollection.cs
@@ -83,6 +83,13 @@ namespace TypeCobol.Test {
 
         [TestMethod]
         [TestProperty("Time", "fast")]
+        public void CheckSqlScanner()
+        {
+            TestSqlScanner.Demo_CheckSql();
+        }
+
+        [TestMethod]
+        [TestProperty("Time", "fast")]
         public void CheckPreprocessor()
         {
             TestCompilerDirectiveBuilder.CheckBASIS();
