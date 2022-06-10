9206cbffbc9ae3f810b378f4df4178842c5ea37e
LAABIDHE <hend.laabidi@e-i.com>
2022-06-10 15:37:09 +0200

    WI #2243 Remove unused imports

--- TypeCobol/Compiler/Sql/CodeElements/SqlCodeElementCheckers.cs
+++ TypeCobol/Compiler/Sql/CodeElements/SqlCodeElementCheckers.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using TypeCobol.Compiler.CodeElements;
 using TypeCobol.Compiler.Parser;
 using TypeCobol.Compiler.Parser.Generated;
 using TypeCobol.Compiler.Sql.CodeElements.Statements;
 
 namespace TypeCobol.Compiler.Sql.CodeElements
 {
-    internal static class SavepointStatementChecker
+    public static class SavepointStatementChecker
     {
-        public static void OnCodeElement(SavepointStatement savepointStatement, CodeElementsParser.SavepointStatementContext context)
+        public static void OnCodeElement(SavepointStatement savepointStatement,
+            CodeElementsParser.SavepointStatementContext context)
         {
             var savePointName = savepointStatement.Name.ToString();
             if (savePointName.StartsWith("SYS", StringComparison.OrdinalIgnoreCase))
