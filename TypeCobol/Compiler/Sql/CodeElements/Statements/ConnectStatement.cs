5e17b9d64795c82c7845dca6f22c2072d500b0d8
LAABIDHE <hend.laabidi@e-i.com>
2022-06-23 15:11:53 +0200

    WI #2247 Put all the statement in the same line

--- TypeCobol/Compiler/Sql/CodeElements/Statements/ConnectStatement.cs
+++ TypeCobol/Compiler/Sql/CodeElements/Statements/ConnectStatement.cs
@@ -9,8 +9,7 @@ namespace TypeCobol.Compiler.Sql.CodeElements.Statements
         public SyntaxProperty<bool> Reset { get; }
         public ConnectionAuthorization Authorization { get; }
 
-        public ConnectStatement(ConnectionAuthorization authorization, SyntaxProperty<bool> reset, ConnectionTarget target) : base(
-            CodeElementType.ConnectStatement, StatementType.ConnectStatement)
+        public ConnectStatement(ConnectionAuthorization authorization, SyntaxProperty<bool> reset, ConnectionTarget target) : base(CodeElementType.ConnectStatement, StatementType.ConnectStatement)
         {
             Authorization = authorization;
             Reset = reset;
