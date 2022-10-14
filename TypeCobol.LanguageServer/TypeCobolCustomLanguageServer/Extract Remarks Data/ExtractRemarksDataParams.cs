9352e0c90818f5e8071c3b62c6d5ca378b1eb3b6
fm-117 <milletfl@e-i.com>
2022-10-14 17:29:13 +0200

    WI #2348 Define REMARKS data extraction request

---
+++ TypeCobol.LanguageServer/TypeCobolCustomLanguageServer/Extract Remarks Data/ExtractRemarksDataParams.cs
@@ -0,0 +1,13 @@
+﻿#if EUROINFO_RULES
+
+using TypeCobol.LanguageServer.VsCodeProtocol;
+
+namespace TypeCobol.LanguageServer.TypeCobolCustomLanguageServerProtocol
+{
+    public class ExtractRemarksDataParams
+    {
+        public TextDocumentIdentifier textDocument;
+    }
+}
+
+#endif
---
+++ TypeCobol.LanguageServer/TypeCobolCustomLanguageServer/Extract Remarks Data/ExtractRemarksDataRequest.cs
@@ -0,0 +1,13 @@
+﻿#if EUROINFO_RULES
+
+using TypeCobol.LanguageServer.JsonRPC;
+
+namespace TypeCobol.LanguageServer.TypeCobolCustomLanguageServerProtocol
+{
+    class ExtractRemarksDataRequest
+    {
+        public static readonly RequestType Type = new RequestType("typecobol/euroinformation/ExtractRemarksData", typeof(ExtractRemarksDataParams), typeof(RemarksData), typeof(ExtractRemarksDataError));
+    }
+}
+
+#endif
