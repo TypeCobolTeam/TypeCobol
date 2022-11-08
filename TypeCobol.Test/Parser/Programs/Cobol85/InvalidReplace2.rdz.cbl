3f1bf11968ef66db13a82b74d60b1de4323d9b7c
fm-117 <milletfl@e-i.com>
2022-11-08 11:56:08 +0100

    WI #2374 Extend unit test

--- TypeCobol.Test/Parser/Programs/Cobol85/InvalidReplace2.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/InvalidReplace2.rdz.cbl
@@ -25,6 +25,30 @@
        REPLACE ==:item-g:== BY ==G== ;==:item-h:== BY ==H==.
        01 DATA-:item-g: PIC X.
        01 DATA-:item-h: PIC X.
+      
+      *Ok
+       REPLACE ==:item-i:== BY ==I== , ==:item-j:== BY ==J==.
+       01 DATA-:item-i: PIC X.
+       01 DATA-:item-j: PIC X.
+      
+      *Warning
+      *A blank was missing before character"="in column 39.
+      *A blank was assumed.
+       REPLACE ==:item-k:== BY ==K==,==:item-l:== BY ==L==.
+       01 DATA-:item-k: PIC X.
+       01 DATA-:item-l: PIC X.
+      
+      *Ok
+       REPLACE ==:item-m:== BY ==M== ; ==:item-n:== BY ==N==.
+       01 DATA-:item-m: PIC X.
+       01 DATA-:item-n: PIC X.
+      
+      *Warning - accepted by IBM compiler but currently not supported
+      *A blank was missing before character"="in column 39.
+      *A blank was assumed.
+       REPLACE ==:item-o:== BY ==O==;==:item-p:== BY ==P==.
+       01 DATA-:item-o: PIC X.
+       01 DATA-:item-p: PIC X.
        PROCEDURE DIVISION.
            DISPLAY DATA-A
                    DATA-B
@@ -34,6 +58,14 @@
                    DATA-F
                    DATA-G
                    DATA-H
+                   DATA-I
+                   DATA-J
+                   DATA-K
+                   DATA-L
+                   DATA-M
+                   DATA-N
+                   DATA-O
+                   DATA-P
            GOBACK
            .
        END PROGRAM TCOMFL06.
