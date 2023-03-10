5eeafd8fd304396a418c296cc5feb501a426563e
fm-117 <51322634+fm-117@users.noreply.github.com>
2023-03-10 08:40:35 +0100

    WI #2457 Improve incremental parsing for compiler directives (#2468)

---
+++ TypeCobol.Test/Parser/Incremental/AdvancedEdits/ClearDocumentThenRewriteLineByLine/ReplaceMultilineWithComment.rdz.cbl
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCOMFL06.
+      *Insert here a multiline REPLACE directive
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 :item: PIC X.
+       END PROGRAM TCOMFL06.
