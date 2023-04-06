be36f8d5f8471a315c94644e4db0340cd8f22ec2
fm-117 <milletfl@e-i.com>
2023-04-06 17:03:57 +0200

    WI #2488 [WIP] use dotnet test, rewrite release script

--- ReleaseScript.bat
+++ ReleaseScript.bat
@@ -1,6 +1,9 @@
 IF EXIST .\TypeCobol.LanguageServer\bin\EI_Release (
 IF EXIST .\CLI\src\bin\EI_Release (
-IF NOT EXIST .\EI_TypeCobol_Release (
+IF EXIST .\EI_TypeCobol_Release (
+RMDIR .\EI_TypeCobol_Release /S /Q
+)
+MKDIR .\EI_TypeCobol_Release
 ECHO F | xcopy .\TypeCobol.LanguageServer\bin\EI_Release\*.exe .\EI_TypeCobol_Release /Y
 ECHO F | xcopy .\TypeCobol.LanguageServer\bin\EI_Release\*.dll .\EI_TypeCobol_Release /Y
 ECHO F | xcopy .\TypeCobol.LanguageServer\bin\EI_Release\*.runtimeconfig.json .\EI_TypeCobol_Release /Y
@@ -8,14 +11,17 @@ ECHO D | xcopy .\CLI\src\bin\EI_Release\DefaultCopies .\EI_TypeCobol_Release\Def
 ECHO F | xcopy .\CLI\src\bin\EI_Release\*.exe .\EI_TypeCobol_Release /Y
 ECHO F | xcopy .\CLI\src\bin\EI_Release\*.dll .\EI_TypeCobol_Release /Y
 ECHO F | xcopy .\CLI\src\bin\EI_Release\*.runtimeconfig.json .\EI_TypeCobol_Release /Y
-)) ELSE (
+) ELSE (
 ECHO "CLI EI_Release folder not found" )) ELSE (
 ECHO "LanguageServer EI_Release folder not found" )
 
 
 IF EXIST .\TypeCobol.LanguageServer\bin\Release (
 IF EXIST .\CLI\src\bin\Release (
-IF NOT EXIST .\TypeCobol_Release (
+IF EXIST .\TypeCobol_Release (
+RMDIR .\TypeCobol_Release /S /Q
+)
+MKDIR .\TypeCobol_Release
 ECHO F | xcopy .\TypeCobol.LanguageServer\bin\Release\*.exe .\TypeCobol_Release /Y
 ECHO F | xcopy .\TypeCobol.LanguageServer\bin\Release\*.dll .\TypeCobol_Release /Y
 ECHO F | xcopy .\TypeCobol.LanguageServer\bin\Release\*.runtimeconfig.json .\TypeCobol_Release /Y
@@ -23,6 +29,6 @@ ECHO D | xcopy .\CLI\src\bin\Release\DefaultCopies .\TypeCobol_Release\DefaultCo
 ECHO F | xcopy .\CLI\src\bin\Release\*.exe .\TypeCobol_Release /Y
 ECHO F | xcopy .\CLI\src\bin\Release\*.dll .\TypeCobol_Release /Y
 ECHO F | xcopy .\CLI\src\bin\Release\*.runtimeconfig.json .\TypeCobol_Release /Y
-)) ELSE (
+) ELSE (
 ECHO "CLI Release folder not found" )) ELSE (
 ECHO "LanguageServer Release folder not found" )
