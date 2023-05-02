977c06c1c7701c30d3290371cde60a7b882e384b
fm-117 <milletfl@e-i.com>
2023-05-02 11:03:25 +0200

    WI #2319 Allow missing sentence ending in grammar, check later

--- TypeCobol/Compiler/Nodes/Node.cs
+++ TypeCobol/Compiler/Nodes/Node.cs
@@ -920,6 +920,23 @@ namespace TypeCobol.Compiler.Nodes {
             CloneChildren(cloned);
             return cloned;
         }
+
+        /// <summary>
+        /// Return the node that immediately follows this one in document order.
+        /// </summary>
+        /// <returns>Non-null instance of Node.</returns>
+        public virtual Node GetNextNode()
+        {
+            System.Diagnostics.Debug.Assert(Parent != null);
+
+            int myIndex = Parent.IndexOf(this);
+            if (myIndex + 1 < Parent.ChildrenCount)
+            {
+                return Parent.Children[myIndex + 1];
+            }
+
+            return Parent.GetNextNode();
+        }
     }
 
     /// <summary>
@@ -1038,16 +1055,7 @@ namespace TypeCobol.Compiler.Nodes {
             }
         }
 
-
-
-
-
-
-
-
-
-
-
+        public override Node GetNextNode() => this;
     }
 
     public class LibraryCopy : GenericNode<LibraryCopyCodeElement>, Child<Program> {
