eecfae5c02b50bcf1c52ad2ef13ca0dc9f81af5a
fm-117 <milletfl@e-i.com>
2023-08-01 14:10:44 +0200

    WI #2555 Detect COPY instructions from Debug lines when extracting REMARKS data

--- TypeCobol.LanguageServer/Workspace.cs
+++ TypeCobol.LanguageServer/Workspace.cs
@@ -17,6 +17,7 @@ using TypeCobol.Logging;
 using TypeCobol.Tools;
 using System.Collections.Concurrent;
 #if EUROINFO_RULES
+using System.Text.RegularExpressions;
 using TypeCobol.Compiler.CodeElements;
 using TypeCobol.Compiler.Preprocessor;
 #endif
@@ -949,8 +950,16 @@ namespace TypeCobol.LanguageServer
 #if EUROINFO_RULES
         public (string[], int) GetRemarksData(CompilationUnit compilationUnit)
         {
-            // Get used CPYs
-            var usedCPYs = compilationUnit.CopyTextNamesVariations
+            // Get used CPYs from parsed code
+            var usedCPYs = compilationUnit.CopyTextNamesVariations;
+
+            // Check for COPY instructions in Debug lines
+            var debugUsedCPYs = compilationUnit.CobolTextLines
+                .Where(line => line.Type == CobolTextLineType.Debug)
+                .SelectMany(FindTextNameVariations);
+
+            // Concat and filter
+            var allUsedCPYs = usedCPYs.Concat(debugUsedCPYs)
                 .Where(v => this.Configuration.IsCpyCopy(v.TextName))
                 .Select(v => v.TextNameWithSuffix.ToUpperInvariant())
                 .Distinct()
@@ -964,7 +973,20 @@ namespace TypeCobol.LanguageServer
                 insertionLine = programIdentification.LineEnd + 1;
             }
 
-            return (usedCPYs, insertionLine);
+            return (allUsedCPYs, insertionLine);
+
+            IEnumerable<RemarksDirective.TextNameVariation> FindTextNameVariations(ICobolTextLine debugLine)
+            {
+                const string textNameCaptureRegex = @"COPY\s+(\w+)";
+                var matches = Regex.Matches(debugLine.SourceText, textNameCaptureRegex, RegexOptions.IgnoreCase);
+                foreach (Match match in matches)
+                {
+                    if (match.Groups.Count > 1)
+                    {
+                        yield return new RemarksDirective.TextNameVariation(match.Groups[1].Value);
+                    }
+                }
+            }
         }
 #endif
     }
