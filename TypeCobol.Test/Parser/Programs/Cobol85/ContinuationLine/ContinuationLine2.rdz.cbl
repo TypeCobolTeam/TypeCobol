5c942d9567ce1617a2792b5994aa9104f3ec6a8d
efr15 <eric.francois@e-i.com>
2023-09-01 17:09:01 +0200

    WI #2187 Revert useless alignment to avoid conflict

--- TypeCobol.Test/Parser/Programs/Cobol85/ContinuationLine/ContinuationLine2.rdz.cbl
+++ TypeCobol.Test/Parser/Programs/Cobol85/ContinuationLine/ContinuationLine2.rdz.cbl
@@ -2,7 +2,7 @@
        PROGRAM-ID. MyPGM2.
        procedure division.
       * KO
-               display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+           display "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
       -          ' er zer '
            .
        END PROGRAM MyPGM2.
