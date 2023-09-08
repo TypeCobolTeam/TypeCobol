704b65410118c40647b33b2d8b142a2c72dc9cd5
efr15 <129841296+efr15@users.noreply.github.com>
2023-09-08 10:31:35 +0200

    WI #2548 Create a new LSP custom request for DataLayout view (#2563)
    * WI #2548 Create a new custom LSP request for DataLayout view
    
    * WI #2548 Add a suffix for data definition under an occurs
    
    * WI #2548 Add a LSR test

---
+++ TypeCobol.LanguageServer/TypeCobolCustomLanguageServer/Get Data Layout Request/GetDataLayoutRequest.cs
@@ -0,0 +1,13 @@
+﻿using TypeCobol.LanguageServer.JsonRPC;
+
+namespace TypeCobol.LanguageServer.TypeCobolCustomLanguageServerProtocol
+{
+    /// <summary>
+    /// The GetDataLayout request is sent by the client to retrieve the elements declared in a program or a copy
+    /// and some additional information (especially their memory size).
+    /// </summary>
+    class GetDataLayoutRequest
+    {
+        public static readonly RequestType Type = new RequestType("typecobol/getDataLayoutRequest", typeof(GetDataLayoutParams), typeof(GetDataLayoutResult), null);
+    }
+}
