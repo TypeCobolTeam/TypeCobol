547f1a2e1587b954500a8d5a57004f735a26bf11
fm-117 <milletfl@e-i.com>
2024-07-23 17:10:29 +0200

    WI #2634 Add WorkspaceApplyEdit request

---
+++ TypeCobol.LanguageServer/VsCodeProtocol/Workspace Changes/ApplyWorkspaceEditResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace TypeCobol.LanguageServer.VsCodeProtocol
+{
+    /// <summary>
+    /// Class representing the response sent for a workspace/applyEdit request.
+    /// </summary>
+    public class ApplyWorkspaceEditResponse
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether edits were applied or not.
+        /// </summary>
+        public bool applied { get; set; }
+
+        /// <summary>
+        /// Gets or sets a string with textual description for why the edit was not applied.
+        /// </summary>
+        public string failureReason { get; set; }
+    }
+}
