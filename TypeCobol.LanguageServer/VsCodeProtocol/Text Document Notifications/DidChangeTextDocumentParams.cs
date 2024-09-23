20315ab27ed1919524ddb8a096f1e2d19fedd45b
efr15 <efr15@e-i.com>
2024-09-23 17:26:31 +0200

    WI #2659 Fix LSP incompatibilities

--- TypeCobol.LanguageServer/VsCodeProtocol/Text Document Notifications/DidChangeTextDocumentParams.cs
+++ TypeCobol.LanguageServer/VsCodeProtocol/Text Document Notifications/DidChangeTextDocumentParams.cs
@@ -8,10 +8,21 @@ namespace TypeCobol.LanguageServer.VsCodeProtocol
     /// <summary>
     /// The change text document notification's parameters.
     /// </summary>
-    public class DidChangeTextDocumentParams : TextDocumentIdentifier
+    public class DidChangeTextDocumentParams
     {
+        public VersionedTextDocumentIdentifier textDocument { get; set; }
+
         public TextDocumentContentChangeEvent[] contentChanges { get; set; }
 
-        public DidChangeTextDocumentParams(string uri) : base(uri) { }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="textDocument">The document that did change.</param>
+        /// <param name="contentChanges">The actual content changes.</param>
+        public DidChangeTextDocumentParams(VersionedTextDocumentIdentifier textDocument, TextDocumentContentChangeEvent[] contentChanges)
+        {
+            this.textDocument = textDocument;
+            this.contentChanges = contentChanges;
+        }
     }
 }
