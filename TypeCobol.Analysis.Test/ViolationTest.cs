e1274a4ebfb3125ea57c41d0ca68b7b017bc212b
efr15 <129841296+efr15@users.noreply.github.com>
2024-10-11 18:13:17 +0200

    WI #2666 Fix FormatException when creating violation from analyzer (#2667)

---
+++ TypeCobol.Analysis.Test/ViolationTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TypeCobol.Test;
+
+namespace TypeCobol.Analysis.Test
+{
+    /// <summary>
+    /// These are basic tests testing Violation creation.
+    /// </summary>
+    [TestClass]
+    public class ViolationTest
+    {
+        private static void TestCreation([CallerMemberName] string testName = null)
+        {
+            // Define input file
+            var fileName = nameof(TestCreation);
+            var folder = PlatformUtils.GetPathForProjectFile(testName, "TypeCobol.Analysis.Test");
+            var inputFileName = $"{fileName}-input.txt";
+
+            // Read error messages in input file and append corresponding violations in result
+            var result = new StringBuilder();
+            using (StreamReader reader = new StreamReader(Path.Combine(folder, inputFileName)))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var violation = new Violation("ruleID", Compiler.Diagnostics.Severity.Error, Compiler.Diagnostics.Diagnostic.Position.Default, line);
+                    result.AppendLine($"{violation}");
+                }
+            }
+
+            // Compare to expected
+            var expectedPath = Path.Combine(folder, $"{fileName}-expected.txt");
+            var expected = File.ReadAllText(expectedPath);
+            TestUtils.CompareLines(testName, result.ToString(), expected, expectedPath);
+        }
+
+        [TestMethod]
+        public void Violation()
+        {
+            TestCreation();
+        }
+    }
+}
