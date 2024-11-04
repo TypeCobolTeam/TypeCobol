abd9877abb16d52cb1996be0db7fbafd8474bca6
fm-117 <51322634+fm-117@users.noreply.github.com>
2024-11-04 11:52:02 +0100

    WI #2670 Use telemetry/event to send server exceptions to client (#2673)

---
+++ TypeCobol.LanguageServer/VsCodeProtocol/Telemetry Notification/TelemetryNotification.cs
@@ -0,0 +1,17 @@
+﻿using TypeCobol.LanguageServer.JsonRPC;
+
+namespace TypeCobol.LanguageServer.VsCodeProtocol.Telemetry_Notification
+{
+    /// <summary>
+    /// The telemetry notification is sent from the server to the client to ask the client
+    /// to log a telemetry event. The protocol doesn't specify the payload since no interpretation
+    /// of the data happens in the protocol.
+    /// 
+    /// Most clients even don’t handle the event directly but forward them to the extensions
+    /// owing the corresponding server issuing the event.
+    /// </summary>
+    class TelemetryNotification
+    {
+        public static readonly NotificationType Type = new NotificationType("telemetry/event", typeof(object));
+    }
+}
