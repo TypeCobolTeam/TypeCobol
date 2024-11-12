d50e14da0eb0bfca6e1bb6232d58a425852d3507
fm-117 <milletfl@e-i.com>
2024-11-12 16:57:09 +0100

    [POC] CobolStringBuilder again

--- TypeCobol.LanguageServer/Commands/Refactor/CobolStringBuilder.cs
+++ TypeCobol.LanguageServer/Commands/Refactor/CobolStringBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using TypeCobol.Compiler.Text;
 
 namespace TypeCobol.LanguageServer.Commands.Refactor
@@ -6,63 +7,82 @@ namespace TypeCobol.LanguageServer.Commands.Refactor
     internal class CobolStringBuilder
     {
         private const int MAX_LENGTH = (int)CobolFormatAreas.End_B; // 72
-        private const string SIX_SPACES = "      ";
         private const char ONE_SPACE = ' ';
+        private const char DEBUG_INDICATOR = 'D';
+
+        private static readonly string _SequenceNumber = new string(ONE_SPACE, CobolFormatAreas.EndNumber - CobolFormatAreas.BeginNumber + 1); // 6 spaces
 
         private readonly char _indicator;
         private readonly StringBuilder _text;
         private readonly StringBuilder _currentLine;
+        private int _previousLineIndentLength;
         private bool _currentLineIsEmpty;
 
-        public CobolStringBuilder(char indicator = ONE_SPACE)
+        public CobolStringBuilder(bool debug = false)
         {
-            _indicator = indicator;
+            _indicator = debug ? DEBUG_INDICATOR : ONE_SPACE;
             _text = new StringBuilder();
             _currentLine = new StringBuilder();
-            InitCurrentLine();
+            _previousLineIndentLength = -1;
+            InitCurrentLine(false);
+        }
+
+        private void InitCurrentLine(bool useIndentFromPreviousLine)
+        {
+            _currentLine.Append(_SequenceNumber);
+            _currentLine.Append(_indicator);
+            if (useIndentFromPreviousLine)
+            {
+                Debug.Assert(_previousLineIndentLength >= 0);
+                _currentLine.Append(new string(ONE_SPACE, _previousLineIndentLength));
+            }
+            
+            _currentLineIsEmpty = true;
         }
 
         private void FlushCurrentLine()
         {
-            _text.Append(_currentLine.ToString());
+            string currentLine = _currentLine.ToString();
+            _previousLineIndentLength = currentLine.Skip(_SequenceNumber.Length + 1).TakeWhile(c => c == ONE_SPACE).Count();
+            _text.Append(currentLine);
             _currentLine.Clear();
+        }
+
+        public void AppendIndent(int length)
+        {
+            string indent = new string(ONE_SPACE, length);
+            AppendText(indent);
         }
 
         public void AppendWord(string word)
         {
-            int wordLength = word.Length;
-            if (wordLength > CobolFormatAreas.End_B - CobolFormatAreas.Begin_B + 1) // 61
-            {
-                throw new NotSupportedException("This build does not support word splitting !");
-            }
+            AppendText(word);
+            _currentLineIsEmpty = false;
+        }
 
+        private void AppendText(string text)
+        {
             bool addSeparator = !_currentLineIsEmpty;
             int separatorLength = addSeparator ? 1 : 0;
-            if (_currentLine.Length + separatorLength + wordLength > MAX_LENGTH)
+            if (_currentLine.Length + separatorLength + text.Length > MAX_LENGTH)
             {
-                AppendLine();
+                AppendLine(true);
             }
             else if (addSeparator)
             {
                 _currentLine.Append(ONE_SPACE);
             }
 
-            _currentLine.Append(word);
-            _currentLineIsEmpty = false;
+            _currentLine.Append(text);
         }
 
-        public void AppendLine()
+        public void AppendLine() => AppendLine(false);
+
+        private void AppendLine(bool useIndentFromPreviousLine)
         {
             FlushCurrentLine();
             _text.AppendLine();
-            InitCurrentLine();
-        }
-
-        private void InitCurrentLine()
-        {
-            _currentLine.Append(SIX_SPACES);
-            _currentLine.Append(_indicator);
-            _currentLineIsEmpty = true;
+            InitCurrentLine(useIndentFromPreviousLine);
         }
 
         public override string ToString()
