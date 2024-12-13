d409e3c761f2a22b66328d8f045ecc03c10aab3c
efr15 <efr15@github.com>
2024-12-13 17:45:21 +0100

    WI #2688 Code review (tests)

--- TypeCobol.LanguageServer.Test/ProcessorTests/DataLayoutProcessorCSVTest.cs
+++ TypeCobol.LanguageServer.Test/ProcessorTests/DataLayoutProcessorCSVTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
 using TypeCobol.Compiler;
+using TypeCobol.LanguageServer.Test.Utilities;
 using TypeCobol.LanguageServer.VsCodeProtocol;
 using TypeCobol.Test;
 using TypeCobol.Test.Utils;
@@ -19,7 +20,7 @@ namespace TypeCobol.LanguageServer.Test.ProcessorTests
         private const string ROOT_PATH = "ProcessorTests";
         private readonly DataLayoutProcessor _processor = new();
 
-        private static void DoTestProcessor(string sourceFileName, string testDataFileName, Func<CompilationUnit, Position, (string Header, string[] Rows)> executeProcessor, bool isCopy = false, [CallerMemberName] string testName = null)
+        private void DoTestProcessor(string sourceFileName, string testDataFileName, bool isCopy = false, [CallerMemberName] string testName = null)
         {
             // Parse source file
             var folder = PlatformUtils.GetPathForProjectFile(RELATIVE_PATH, Path.GetFullPath(ROOT_PATH));
@@ -29,15 +30,11 @@ namespace TypeCobol.LanguageServer.Test.ProcessorTests
             var testDataFilePath = $"{Path.Combine(folder, testDataFileName)}.txt";
             var testData = ParseContent(testDataFilePath);
 
-            // Retrieve arguments
-            var arguments = JArray.Parse(testData.Arguments).Cast<object>().ToArray(); // Array of JToken, explicit cast to object is to avoid warning on conversion when calling the constructor
-            if (arguments == null || arguments.Length == 0 || arguments[0] is not JObject jObject)
-            {
-                throw new ArgumentException($"Invalid arguments for test {testDataFileName}");
-            }
+            // Retrieve unique argument = position
+            var position = JToken.Parse(testData.Argument).ToObject<Position>();
 
             // Execute processor
-            var processorResult = executeProcessor(compilationUnit, jObject.ToObject<Position>());
+            var processorResult = ExecuteProcessor(compilationUnit, position);
 
             // Build actual result
             var result = new StringBuilder();
@@ -52,64 +49,31 @@ namespace TypeCobol.LanguageServer.Test.ProcessorTests
             TestUtils.CompareLines(testName, result.ToString(), expected, null);
         }
 
-        private static (string Arguments, string Expected) ParseContent(string testDataFilePath)
-        {
-            string arguments = null;
-
-            int state = 0; // Track current part of the document
-            var builder = new StringBuilder();
-            using (var reader = File.OpenText(testDataFilePath))
-            {
-                while (reader.ReadLine() is { } line) // Non-null pattern + variable definition
-                {
-                    if (line.All(c => c == '-'))
-                    {
-                        // The line is a separator, flush content read so far into correct variable
-                        string currentPart = builder.ToString()[..^2]; // Remove trailing line break
-                        builder.Clear();
-
-                        switch (state)
-                        {
-                            case 0:
-                                // Array of arguments for the processor
-                                arguments = currentPart;
-                                break;
-                        }
-
-                        // Discard separator line but move on to next state
-                        state++;
-                        continue;
-                    }
-
-                    // Accumulate line
-                    builder.AppendLine(line);
-                }
-            }
-
-            // Last part is the expected
-            string expected = builder.ToString(); // Keep trailing line break as we'll end up with one added in the actual result too
-
-            return (arguments, expected);
-        }
-
-        [TestMethod]
-        public void TestCopy() => DoTestProcessor("copy", "CSV-copy", ExecuteProcessor, true);
-
-        [TestMethod]
-        public void TestSimplePgm() => DoTestProcessor("simplePgm", "CSV-simplePgm", ExecuteProcessor);
-
-        [TestMethod]
-        public void TestMainPgm() => DoTestProcessor("stackedAndNestedPgm", "CSV-mainPgm", ExecuteProcessor);
-
-        [TestMethod]
-        public void TestStackedPgm() => DoTestProcessor("stackedAndNestedPgm", "CSV-stackedPgm", ExecuteProcessor);
-
-        [TestMethod]
-        public void TestNestedPgm() => DoTestProcessor("stackedAndNestedPgm", "CSV-nestedPgm", ExecuteProcessor);
-
         private (string Header, string[] Rows) ExecuteProcessor(CompilationUnit compilationUnit, Position position)
         {
             return _processor.GetDataLayoutAsCSV(compilationUnit, position, ";");
         }
+
+        private static (string Argument, string Expected) ParseContent(string testDataFilePath)
+        {
+            var parts = LanguageServerTestUtils.ParseMultiplePartsContent(testDataFilePath);
+
+            return (parts[0], parts[1]);
+        }
+
+        [TestMethod]
+        public void TestCopy() => DoTestProcessor("copy", "CSV-copy", true);
+
+        [TestMethod]
+        public void TestSimplePgm() => DoTestProcessor("simplePgm", "CSV-simplePgm");
+
+        [TestMethod]
+        public void TestMainPgm() => DoTestProcessor("stackedAndNestedPgm", "CSV-mainPgm");
+
+        [TestMethod]
+        public void TestStackedPgm() => DoTestProcessor("stackedAndNestedPgm", "CSV-stackedPgm");
+
+        [TestMethod]
+        public void TestNestedPgm() => DoTestProcessor("stackedAndNestedPgm", "CSV-nestedPgm");
     }
 }
