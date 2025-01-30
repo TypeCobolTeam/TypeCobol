9b2f771f6633dd6888daad8e1240180c81e3db4f
fm-117 <milletfl@e-i.com>
2025-01-30 17:27:41 +0100

    WI #2699 Always use PIC 9(4) COMP-5 for generated indices

--- TypeCobol.LanguageServer.Test/RefactoringTests/InsertVariableDisplay/Occurs/FillersInOccursNoDependingOn.cbl
+++ TypeCobol.LanguageServer.Test/RefactoringTests/InsertVariableDisplay/Occurs/FillersInOccursNoDependingOn.cbl
@@ -61,9 +61,9 @@ refactoring.source=
                    20 FILLER PIC X(20).
                    20 var2 PIC X.
       *<DBG>InsertVariableDisplay 1959/09/18 11:09 TESTUSER
-      D77 Idx-d4df4249-1 PIC 9(4) COMP.
-      D77 Idx-d4df4249-2 PIC 9(3) COMP.
-      D77 Idx-d4df4249-3 PIC 9(2) COMP.
+      D77 Idx-d4df4249-1 PIC 9(4) COMP-5.
+      D77 Idx-d4df4249-2 PIC 9(4) COMP-5.
+      D77 Idx-d4df4249-3 PIC 9(4) COMP-5.
       *</DBG>
 
        PROCEDURE DIVISION.
