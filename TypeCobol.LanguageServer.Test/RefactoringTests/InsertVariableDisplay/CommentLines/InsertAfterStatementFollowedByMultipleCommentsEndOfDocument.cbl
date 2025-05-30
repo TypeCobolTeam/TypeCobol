2df7dcae1a08f860a96d07cc36486f4c5a7738fb
fm-117 <milletfl@e-i.com>
2025-05-30 14:12:47 +0200

    WI #2754 In InsertVariableDisplay look for comment lines before inserting

---
+++ TypeCobol.LanguageServer.Test/RefactoringTests/InsertVariableDisplay/CommentLines/InsertAfterStatementFollowedByMultipleCommentsEndOfDocument.cbl
@@ -0,0 +1,44 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCOMFL06.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 var1 PIC X.
+       PROCEDURE DIVISION.
+           DISPLAY "This statement is followed by comments"
+      * First line of comments
+      * Second line of comments
+      * Third line of comments
+-------------------------------------------------------------------------------------------------
+TypeCobol.LanguageServer.Commands.InsertVariableDisplay.InsertVariableDisplayRefactoringProcessor
+-------------------------------------------------------------------------------------------------
+[
+    {
+        "textDocument": { "uri": "file:/test.expected.cbl" },
+        "position": { "line": 6, "character": 18 }
+    },
+    false,
+    {
+        "vm": 1, "idx": 0, "ch": [
+            {
+                "vm": 0, "name": "var1"
+            }
+        ]
+    }
+]
+-------------------------------------------------------------------------------------------------
+refactoring.label=Debug instructions successfully generated.
+refactoring.source=
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCOMFL06.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 var1 PIC X.
+       PROCEDURE DIVISION.
+           DISPLAY "This statement is followed by comments"
+      * First line of comments
+      * Second line of comments
+      * Third line of comments
+      *<DBG>InsertVariableDisplay 1959/09/18 11:09 TESTUSER
+      D    DISPLAY 'var1 <' var1 '>'
+      *</DBG>
+
