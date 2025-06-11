b4a06a58c37154898166fda8a4e403cd88251932
fm-117 <51322634+fm-117@users.noreply.github.com>
2025-06-11 12:12:14 +0200

    WI #2762 In InsertVariableDisplay, avoid inserting inside statements having a body (#2763)

---
+++ TypeCobol.LanguageServer.Test/RefactoringTests/InsertVariableDisplay/StatementsWithBody/AfterIfWithoutEndIf.cbl
@@ -0,0 +1,65 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCOMFL06.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 group1.
+          05 var1 PIC X.
+          05 var2 PIC X(20).
+          05 var3 PIC X(20).
+       PROCEDURE DIVISION.
+      * This IF statement is not properly ended
+      * Generated statements go after last node of whole IF
+           IF var1 = "A"
+              DISPLAY var2
+           ELSE
+              DISPLAY var3
+           DISPLAY "misleading indent"
+           GOBACK
+           .
+       END PROGRAM TCOMFL06.
+-------------------------------------------------------------------------------------------------
+TypeCobol.LanguageServer.Commands.InsertVariableDisplay.InsertVariableDisplayRefactoringProcessor
+-------------------------------------------------------------------------------------------------
+[
+    {
+        "textDocument": { "uri": "file:/test.expected.cbl" },
+        "position": { "line": 11, "character": 18 }
+    },
+    false,
+    {
+        "vm": 1, "idx": 0, "ch": [
+            {
+                "vm": 2, "name": "group1"
+            }
+        ]
+    }
+]
+-------------------------------------------------------------------------------------------------
+refactoring.label=Debug instructions successfully generated.
+refactoring.source=
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCOMFL06.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 group1.
+          05 var1 PIC X.
+          05 var2 PIC X(20).
+          05 var3 PIC X(20).
+       PROCEDURE DIVISION.
+      * This IF statement is not properly ended
+      * Generated statements go after last node of whole IF
+           IF var1 = "A"
+              DISPLAY var2
+           ELSE
+              DISPLAY var3
+           DISPLAY "misleading indent"
+           GOBACK
+      *<DBG>InsertVariableDisplay 1959/09/18 11:09 TESTUSER
+      D    DISPLAY 'group1'
+      D    DISPLAY '  var1 <' var1 '>'
+      D    DISPLAY '  var2 <' var2 '>'
+      D    DISPLAY '  var3 <' var3 '>'
+      *</DBG>
+
+           .
+       END PROGRAM TCOMFL06.
