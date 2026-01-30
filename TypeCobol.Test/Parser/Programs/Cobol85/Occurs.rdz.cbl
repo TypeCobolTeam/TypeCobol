7b431878ec6846e11d47245cb683409d05b54a27
CAVAN Stéphane <stephane.cavan@e-i.com>
2026-01-30 11:13:30 +0100

    WI #2818 Check level on data having OCCURS clause

---
+++ TypeCobol.Test/Parser/Programs/Cobol85/Occurs.rdz.cbl
@@ -0,0 +1,17 @@
+﻿       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCOCCLVL.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      * KO: OCCURS cannot be specified on level 01
+        01 GRP01 OCCURS 2.
+           05 VAR01 PIC X.
+      * KO: OCCURS cannot be specified on level 77
+        77 GRP77 PIC X(10) OCCURS 2.
+      * OK
+        01 GRP0K.
+           05 VAROK PIC X OCCURS 2.
+
+       PROCEDURE DIVISION.
+           GOBACK
+           .
+       END PROGRAM TCOCCLVL.
